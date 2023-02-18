028ef7bdf96165b5869ce5099e33c6d8a1dd4fce
ijzm <ignacio.zevallos@hotmail.com>
2023-02-18 12:02:31 -0500

    Updated UI for Main Menu, Login & Matchmaking

---
+++ client/src/components/link-container/link-container.tsx
@@ -0,0 +1,27 @@
+import css from './link-container.module.css'
+
+function LinkContainer() {
+	return (
+		<div className={css.linkContainer}>
+			<a
+				href="https://www.reddit.com/r/HermitCraft/comments/10wksaw/hctcg_online/"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<img src="/images/icons/reddit.svg" height="26" />
+			</a>
+			<a
+				href="https://github.com/martinkadlec0/hc-tcg"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<img src="/images/icons/github.svg" height="26" />
+			</a>
+			<a href="https://discord.gg/AjGbqNfcQX" target="_blank" rel="noreferrer">
+				<img src="/images/icons/discord.svg" height="26" />
+			</a>
+		</div>
+	)
+}
+
+export default LinkContainer
