83bf1f7675fae069e8b3c0083118a3e3255af1c1
Sense101 <personal.sense101@gmail.com>
2023-02-18 20:41:55 +0000

    Fix gems ability to include all effect cards

--- server/cards/card-plugins/hermits/geminitay-rare.js
+++ server/cards/card-plugins/hermits/geminitay-rare.js
@@ -30,13 +30,7 @@ class GeminiTayRareHermitCard extends HermitCard {
 
 	register(game) {
 		game.hooks.attack.tap(this.id, (target, turnAction, derivedState) => {
-			const {
-				attackerHermitCard,
-				attackerHermitInfo,
-				typeAction,
-				currentPlayer,
-				attackerActiveRow,
-			} = derivedState
+			const {attackerHermitCard, typeAction, currentPlayer} = derivedState
 
 			if (attackerHermitCard.cardId !== this.id) return target
 			if (typeAction !== 'SECONDARY_ATTACK') return target
@@ -46,47 +40,79 @@ class GeminiTayRareHermitCard extends HermitCard {
 			currentPlayer.coinFlips[this.id] = coinFlip
 			if (coinFlip[0] === 'tails') return target
 
-			// We are using number to make sure extra single_use card use can happen only once
+			// special ability is active, set custom value to 1
+			// 1 means extra effect is available, 2 means used
 			currentPlayer.custom[this.id] = 1
 
 			return target
 		})
 
-		/*
-		Discarding single use cards needs to be delay, bceause some cards are not sued until later, e.g. chorus fruit
-		*/
+		// disable gems ability if it's active and an effect card is played
+		game.hooks.playCard
+			.for('effect')
+			.tap(this.id, (turnAction, derivedState) => {
+				this.tryDisableAbility(derivedState)
+			})
+		game.hooks.playCard
+			.for('single_use')
+			.tap(this.id, (turnAction, derivedState) => {
+				this.tryDisableAbility(derivedState)
+			})
+
+		// discarding single use cards can only occur after single use card is used up
 		game.hooks.actionEnd.tap(this.id, (turnAction, derivedState) => {
 			const {currentPlayer} = derivedState
-			const usedPower = currentPlayer.custom[this.id]
-			const suUsed = currentPlayer.board.singleUseCardUsed
-			if (usedPower === 1 && suUsed && !currentPlayer.followUp) {
-				discardSingleUse(game, currentPlayer)
-				currentPlayer.custom[this.id] = 2
-			}
-		})
 
-		game.hooks.turnEnd.tap(this.id, (derivedState) => {
-			const {currentPlayer} = derivedState
-			delete currentPlayer.custom[this.id]
+			// if gems ability is active and a single use effect card is used up, remove it
+			const abilityActive = currentPlayer.custom[this.id] === 1
+			const suUsed = currentPlayer.board.singleUseCardUsed
+
+			if (abilityActive && suUsed && !currentPlayer.followUp) {
+				discardSingleUse(game, currentPlayer)
+			}
 		})
 
 		game.hooks.availableActions.tap(
 			this.id,
 			(availableActions, derivedState) => {
-				const {pastTurnActions, currentPlayer} = derivedState
-				const usedPower = currentPlayer.custom[this.id]
+				const {currentPlayer} = derivedState
 
-				if (
-					usedPower &&
-					availableActions.includes('END_TURN') &&
-					!availableActions.includes('PLAY_SINGLE_USE_CARD') &&
-					!currentPlayer.board.singleUseCard
-				) {
-					availableActions.push('PLAY_SINGLE_USE_CARD')
+				const abilityActive = currentPlayer.custom[this.id] === 1
+
+				if (abilityActive) {
+					// make sure both effect cards can be played
+
+					if (
+						!currentPlayer.board.singleUseCard &&
+						!availableActions.includes('PLAY_SINGLE_USE_CARD')
+					) {
+						availableActions.push('PLAY_SINGLE_USE_CARD')
+					}
+
+					if (!availableActions.includes('PLAY_EFFECT_CARD')) {
+						availableActions.push('PLAY_EFFECT_CARD')
+					}
 				}
+
 				return availableActions
 			}
 		)
+
+		// at the end of the turn, delete custom gem data
+		game.hooks.turnEnd.tap(this.id, (derivedState) => {
+			const {currentPlayer} = derivedState
+			delete currentPlayer.custom[this.id]
+		})
+	}
+
+	// disables gems ability if its available
+	tryDisableAbility(derivedState) {
+		const {currentPlayer} = derivedState
+		const abilityActive = currentPlayer.custom[this.id] === 1
+		if (abilityActive) {
+			// disable ability
+			currentPlayer.custom[this.id] = 2
+		}
 	}
 }
 
