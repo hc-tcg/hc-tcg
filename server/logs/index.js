26d21edb7ec0205571523cb82c7747ba152f47f0
Andy Novocin <andy@novocin.com>
2023-02-19 01:59:00 -0500

    added anonymized log summary

--- server/logs/index.js
+++ server/logs/index.js
@@ -25,8 +25,12 @@ class FirebaseLogs {
 			game.hooks.gameStart.tap(this.id, () => {
 				const playerStates = Object.values(game.state.players)
 				const getHand = (pState) => pState.hand.map((card) => card.cardId)
+        game.startHand1 = getHand(playerStates[0]);
+        game.startHand2 = getHand(playerStates[1]);
+        game.startTimestamp = new Date().getTime();
 				console.log('Player 1 initial hand: ', getHand(playerStates[0]))
 				console.log('Player 2 initial hand: ', getHand(playerStates[1]))
+
         let pid0 = playerStates[0].id;
         server.allPlayers[pid0].socket.emit("andytest", {"yo":"yes"});
         let pid1 = playerStates[1].id;
@@ -37,10 +41,26 @@ class FirebaseLogs {
 			game.hooks.gameEnd.tap(this.id, () => {
 				console.log('game took turns: ', game.state.turn)
         const playerStates = Object.values(game.state.players)
+        let summaryObj = {
+          startHand1: game.startHand1,
+          startHand2: game.startHand2,
+          startTimestamp: game.startTimestamp,
+          endTimestamp: new Date().getTime(),
+          turns: game.state.turn
+        };
         let pid0 = playerStates[0].id;
         server.allPlayers[pid0].socket.emit("gameoverstat", game.deadPlayerId === pid0 ? 'you_lost' : 'you_won');
+        summaryObj.deck1 = server.allPlayers[pid0].playerDeck;
+
         let pid1 = playerStates[1].id;
         server.allPlayers[pid1].socket.emit("gameoverstat", game.deadPlayerId === pid1 ? 'you_lost' : 'you_won');
+        summaryObj.deck2 = server.allPlayers[pid1].playerDeck;
+        if (game.deadPlayerId === pid1){
+          summaryObj.outcome = "deck1win";
+        } else {
+          summaryObj.outcome = "deck2win";
+        }
+        this.db.ref("/logs").push(summaryObj);
         //"gameoverstat";
         let numGames = Object.values(server.games).filter(gameRecord => !!gameRecord.task).length;
         this.gamecountref.set(numGames-1);
