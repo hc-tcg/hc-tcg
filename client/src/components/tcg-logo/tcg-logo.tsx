68588a195824cdbced268f7778abe311d5bbabdb
Pieter Maes <maescool@gmail.com>
2023-03-01 20:38:09 +0100

    Make the Logo's subtext configured and update css name. The logo's subtext can be now changed from config. Updated the css name from notOfficial to logoSubText

--- client/src/components/tcg-logo/tcg-logo.tsx
+++ client/src/components/tcg-logo/tcg-logo.tsx
@@ -1,12 +1,14 @@
 import css from './tcg-logo.module.css'
+// @ts-ignore
+import {CONFIG} from '../../../../config'
 
 function TcgLogo() {
 	return (
 		/* Logo Container */
 		<div className={css.logo}>
 			<img draggable={false} width={'100%'} src="/images/tcg-logo.png"></img>
-			<div className={css.notOfficial}>
-				<p>Not Official!</p>
+			<div className={css.logoSubText}>
+				<p>{CONFIG.logoSubText}</p>
 			</div>
 		</div>
 	)
