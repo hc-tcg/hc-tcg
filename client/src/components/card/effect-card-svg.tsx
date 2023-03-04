67bf31114c061c3e05d4b7fd9b1c80f3b5a9a931
Jimmy474 <vashijaimin474@gmail.com>
2023-03-05 00:39:13 +0530

    removed useless console log

--- client/src/components/card/effect-card-svg.tsx
+++ client/src/components/card/effect-card-svg.tsx
@@ -6,7 +6,6 @@ export type EffectCardProps = {
 }
 
 const HermitCard = ({card}: EffectCardProps) => {
-	console.log(document.title);
 	return (
 		<svg className={css.card} width="100%" height="100%" viewBox="0 0 400 400">
 			<rect
