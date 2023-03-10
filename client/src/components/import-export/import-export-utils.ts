551926f668e53687546325584cd7d64fd2cbce2e
Martin Kadlec <harou2@gmail.com>
2023-03-11 00:55:09 +0100

    Dream card

--- client/src/components/import-export/import-export-utils.ts
+++ client/src/components/import-export/import-export-utils.ts
@@ -20,7 +20,9 @@ export const getDeckFromHash = (hash: string): Array<CardT> => {
 export const getHashFromDeck = (pickedCards: Array<CardT>): string => {
 	const indicies = []
 	for (let i = 0; i < pickedCards.length; i++) {
-		indicies.push(universe.indexOf(String(pickedCards[i].cardId)))
+		const cardId = String(pickedCards[i].cardId)
+		const index = universe.indexOf(cardId)
+		if (index >= 0) indicies.push(index)
 	}
 	const b64cards = btoa(String.fromCharCode.apply(null, indicies))
 	return b64cards
