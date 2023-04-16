dd16d1e178bcf4bf2b18bbad3b2662e28fe76ae8
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-16 00:05:14 -0500

    Update and rename hypnotizd-common.js to iceCreamDealer.js

---
+++ server/cards/card-plugins/hermits/iceCreamDealer.js
@@ -0,0 +1,29 @@
+import CharacterCard from './_character-card'
+
+class IceCreamDealerCharacterCard extends CharacterCard {
+	constructor() {
+		super({
+			id: 'iceCreamDealer',
+			name: 'Ice Cream Dealer',
+			rarity: 'common',
+			characterType: 'iceCream',
+			health: 300,
+			primary: {
+				name: 'Shady Salesman',
+				cost: ['iceCream','any'],
+				damage: 60,
+				power: null,
+			},
+			secondary: {
+				name: 'Alleyway Deals',
+				cost: ['iceCream', 'iceCream', 'any'],
+				damage: 90,
+				power: null,
+			},
+		})
+	}
+
+	register(game) {}
+}
+
+export default IceCreamDealerCharacterCard
