46169d1984a81628a55198a0139d5a711bf7dafe
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-15 20:59:16 -0500

    Replace Bdubs C to Banana Gun

---
+++ server/cards/card-plugins/hermits/banana-gun.js
@@ -0,0 +1,29 @@
+import CharacterCard from './_character-card'
+
+class BananaGunCharacterCard extends CharacterCard {
+	constructor() {
+		super({
+			id: 'banana_gun',
+			name: 'Banana Gun',
+			rarity: 'common',
+			characterType: 'cat',
+			health: 290,
+			primary: {
+				name: 'Banana Blast',
+				cost: ['cat'],
+				damage: 50,
+				power: null,
+			},
+			secondary: {
+				name: 'School Shooting',
+				cost: ['cat', 'cat','any'],
+				damage: 90,
+				power: null,
+			},
+		})
+	}
+
+	register(game) {}
+}
+
+export default BananaGunCharacterCard
