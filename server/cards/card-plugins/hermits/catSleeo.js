97a4ba32ee5341d0e05d281707fb0d02f2eb6fc8
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-15 22:38:11 -0500

    Update and rename grian-common.js to catSleeo.js

---
+++ server/cards/card-plugins/hermits/catSleeo.js
@@ -0,0 +1,29 @@
+import CharacterCard from './_character-card'
+
+class CatSleeoCharacterCard extends CharacterCard {
+	constructor() {
+		super({
+			id: 'catSleeo',
+			name: 'Cat Sleeo',
+			rarity: 'common',
+			characterType: 'cat',
+			health: 270,
+			primary: {
+				name: 'Eat Ramen',
+				cost: ['cat'],
+				damage: 30,
+				power: null,
+			},
+			secondary: {
+				name: 'Scream Lyrics',
+				cost: ['cat', 'any'],
+				damage: 80,
+				power: null,
+			},
+		})
+	}
+
+	register(game) {}
+}
+
+export default CatSleeoCharacterCard
