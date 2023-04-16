99508593313c775a4e6f91ad9b46e5926580e9db
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-15 22:04:09 -0500

    Update and rename ijevin-rare.js to hoglin.js

---
+++ server/cards/card-plugins/hermits/hoglin.js
@@ -0,0 +1,55 @@
+import CharacterCard from './_character-card'
+import {flipCoin} from '../../../utils'
+
+/**
+ * @typedef {import('models/game-model').GameModel} GameModel
+ */
+
+class IJevinRareHermitCard extends CharacterCard {
+	constructor() {
+		super({
+			id: 'hoglin',
+			name: 'Hoglin',
+			rarity: 'rare',
+			hermitType: 'speedrunner',
+			health: 290,
+			primary: {
+				name: 'Charge',
+				cost: ['bacon'],
+				damage: 50,
+				power: null,
+			},
+			secondary: {
+				name: 'Fling',
+				cost: ['bacon', 'bacon'],
+				damage: 80,
+				power:
+					'After attack, opponent is forced to replace active Character with Benched Character.\n\nIf there are no Benched Characters, active Character remains in battle.',
+			},
+		})
+	}
+
+	/**
+	 * @param {GameModel} game
+	 */
+	register(game) {
+		game.hooks.attack.tap(this.id, (target, turnAction, attackState) => {
+			const {opponentPlayer, opponentActiveRow} = game.ds
+			const {attackerCharacterCard, typeAction} = attackState
+
+			if (typeAction !== 'SECONDARY_ATTACK') return target
+			if (!target.isActive) return target
+			if (attackerCharacterCard.cardId !== this.id) return target
+
+			const hasOtherCharacters =
+				opponentPlayer.board.rows.filter((row) => !!row.characterCard).length > 1
+			if (!hasOtherCharacters || !opponentActiveRow) return target
+			opponentActiveRow.ailments.push({id: 'knockedout', duration: 1})
+			opponentPlayer.board.activeRow = null
+
+			return target
+		})
+	}
+}
+
+export default HoglinCharacterCard
