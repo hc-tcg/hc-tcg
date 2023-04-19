b0dfd1eca5dd7398ddf9cd09b447d20802417527
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-18 20:15:23 -0500

    Update and rename mumbojumbo-common.js to frog.js

---
+++ server/cards/card-plugins/hermits/frog.js
@@ -0,0 +1,29 @@
+import CharacterCard from './_character-card'
+
+class FrogCharacterCard extends CharacterCard {
+	constructor() {
+		super({
+			id: 'frog',
+			name: 'Frog',
+			rarity: 'common',
+			hermitType: 'minecraft',
+			health: 280,
+			primary: {
+				name: 'Lick',
+				cost: ['any'],
+				damage: 30,
+				power: null,
+			},
+			secondary: {
+				name: 'Pounce',
+				cost: ['any', 'any'],
+				damage: 60,
+				power: null,
+			},
+		})
+	}
+
+	register(game) {}
+}
+
+export default FrogCharacterCard
