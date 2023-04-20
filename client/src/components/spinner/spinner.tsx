5e8b19570dd4dcb6d203eea7a742fd8611fe68f2
Joseph Lyman <jmlyman424@gmail.com>
2023-04-19 23:39:15 -0400

    created spinner & updated login connecting message

---
+++ client/src/components/spinner/spinner.tsx
@@ -0,0 +1,26 @@
+import css from './spinner.module.scss'
+
+const Spinner = () => {
+	return (
+		<div className={css.pixelSpinner}>
+			<div className={`${css.pixel} ${css.pixel1}`}></div>
+			<div className={`${css.pixel} ${css.pixel2}`}></div>
+			<div className={`${css.pixel} ${css.pixel3}`}></div>
+			<div className={`${css.pixel} ${css.pixel4}`}></div>
+			<div className={`${css.pixel} ${css.pixel5}`}></div>
+			<div className={`${css.pixel} ${css.pixel6}`}></div>
+			<div className={`${css.pixel} ${css.pixel7}`}></div>
+			<div className={`${css.pixel} ${css.pixel8}`}></div>
+			<div className={`${css.pixel} ${css.pixel9}`}></div>
+			<div className={`${css.pixel} ${css.pixel10}`}></div>
+			<div className={`${css.pixel} ${css.pixel11}`}></div>
+			<div className={`${css.pixel} ${css.pixel12}`}></div>
+			<div className={`${css.pixel} ${css.pixel13}`}></div>
+			<div className={`${css.pixel} ${css.pixel14}`}></div>
+			<div className={`${css.pixel} ${css.pixel15}`}></div>
+			<div className={`${css.pixel} ${css.pixel16}`}></div>
+		</div>
+	)
+}
+
+export default Spinner
