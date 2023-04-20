7785ac7783cc73f2a973f187a5f8cb01de67b2b9
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-19 20:21:26 -0500

    All cards in

--- server/cards/card-plugins/hermits/index.js
+++ server/cards/card-plugins/hermits/index.js
@@ -3,115 +3,57 @@ import BananaGunCharacterCard from './bananaGun'
 import BunnyBoyOkuCharacterCard from './bunnyBoyOku'
 import BurgerCharacterCard from './burger'
 import ChickenPizzaPanCharacterCard from './chickenPizzaPan'
-import FalseSymmetryCommonHermitCard from './falsesymmetry-common'
-import GeminiTayCommonHermitCard from './geminitay-common'
-import GrianCommonHermitCard from './grian-common'
-import GoodTimesWithScarCommonHermitCard from './goodtimeswithscar-common'
-import HypnotizdCommonHermitCard from './hypnotizd-common'
-import IJevinCommonHermitCard from './ijevin-common'
-import ImpulseSVCommonHermitCard from './impulsesv-common'
-import Iskall85CommonHermitCard from './iskall85-common'
-import JoeHillsCommonHermitCard from './joehills-common'
-import KeralisCommonHermitCard from './keralis-common'
-import MumboJumboCommonHermitCard from './mumbojumbo-common'
-import PearlescentMoonCommonHermitCard from './pearlescentmoon-common'
-import RendogCommonHermitCard from './rendog-common'
-import StressMonster101CommonHermitCard from './stressmonster101-common'
-import TangoTekCommonHermitCard from './tangotek-common'
-import TinFoilChefCommonHermitCard from './tinfoilchef-common'
-import VintageBeefCommonHermitCard from './vintagebeef-common'
-import WelsknightCommonHermitCard from './welsknight-common'
-import XBCraftedCommonHermitCard from './xbcrafted-common'
-import XisumavoidCommonHermitCard from './xisumavoid-common'
-import ZedaphPlaysCommonHermitCard from './zedaphplays-common'
-import ZombieCleoCommonHermitCard from './zombiecleo-common'
+import MEE6CharacterCard from './MEE6'
+import DankMemerCharacterCard from './dankMemer'
+import CarlBotCharacterCard from './carlBot'
+import CatSleeoCharacterCard from './catSleeo'
+import NyanCatCharacterCard from './nyanCat'
+import QuackittyCharacterCard from './quackitty'
+import IceCreamDealerCharacterCard from './iceCreamDealer'
+import Harison24CharacterCard from './harison24'
+import FrogCharacterCard from './frog'
+import ChildCharacterCard from './child'
+import ConfusedMemeCharacterCard from './confusedMeme'
 // rare & ultra rare cards
-import BdoubleO100RareHermitCard from './bdoubleo100-rare'
-import Cubfan135RareHermitCard from './cubfan135-rare'
-import Docm77RareHermitCard from './docm77-rare'
-import EthosLabRareHermitCard from './ethoslab-rare'
-import EthosLabUltraRareHermitCard from './ethoslab-ultra-rare'
-import FalseSymmetryRareHermitCard from './falsesymmetry-rare'
-import GeminiTayRareHermitCard from './geminitay-rare'
-import GrianRareHermitCard from './grian-rare'
-import GoodTimesWithScarRareHermitCard from './goodtimeswithscar-rare'
-import HypnotizdRareHermitCard from './hypnotizd-rare'
-import IJevinRareHermitCard from './ijevin-rare'
-import ImpulseSVRareHermitCard from './impulsesv-rare'
-import Iskall85RareHermitCard from './iskall85-rare'
-import JoeHillsRareHermitCard from './joehills-rare'
-import KeralisRareHermitCard from './keralis-rare'
-import MumboJumboRareHermitCard from './mumbojumbo-rare'
-import PearlescentMoonRareHermitCard from './pearlescentmoon-rare'
-import RendogRareHermitCard from './rendog-rare'
-import StressMonster101RareHermitCard from './stressmonster101-rare'
-import TangoTekRareHermitCard from './tangotek-rare'
-import TinFoilChefRareHermitCard from './tinfoilchef-rare'
-import TinFoilChefUltraRareHermitCard from './tinfoilchef-ultra-rare'
-import VintageBeefRareHermitCard from './vintagebeef-rare'
-import VintageBeefUltraRareHermitCard from './vintagebeef-ultra-rare'
-import WelsknightRareHermitCard from './welsknight-rare'
-import XBCraftedRareHermitCard from './xbcrafted-rare'
-import XisumavoidRareHermitCard from './xisumavoid-rare'
-import ZedaphPlaysRareHermitCard from './zedaphplays-rare'
-import ZombieCleoRareHermitCard from './zombiecleo-rare'
+import Harison25CharacterCard from './harison25'
+import SleepyCharacterCard from './sleepy'
+import ChubbyCharacterCard from './chubby'
+import HoglinCharacterCard from './hoglin'
+import DonutCharacterCard from './donut'
+import KarutaCharacterCard from './karuta'
+import CrackKittyCharacterCard from './crackKitty'
+import MrCatCharacterCard from './mrCat'
+import ZekeCharacterCard from './zeke'
+import CheeseCharacterCard from './cheese'
 
 const HERMIT_CARDS = [
-	new BdoubleO100CommonHermitCard(),
-	new BdoubleO100RareHermitCard(),
-	new Cubfan135CommonHermitCard(),
-	new Cubfan135RareHermitCard(),
-	new Docm77CommonHermitCard(),
-	new Docm77RareHermitCard(),
-	new EthosLabCommonHermitCard(),
-	new EthosLabRareHermitCard(),
-	new EthosLabUltraRareHermitCard(),
-	new FalseSymmetryCommonHermitCard(),
-	new FalseSymmetryRareHermitCard(),
-	new GeminiTayCommonHermitCard(),
-	new GeminiTayRareHermitCard(),
-	new GrianCommonHermitCard(),
-	new GrianRareHermitCard(),
-	new GoodTimesWithScarCommonHermitCard(),
-	new GoodTimesWithScarRareHermitCard(),
-	new HypnotizdCommonHermitCard(),
-	new HypnotizdRareHermitCard(),
-	new IJevinCommonHermitCard(),
-	new IJevinRareHermitCard(),
-	new ImpulseSVCommonHermitCard(),
-	new ImpulseSVRareHermitCard(),
-	new Iskall85CommonHermitCard(),
-	new Iskall85RareHermitCard(),
-	new JoeHillsCommonHermitCard(),
-	new JoeHillsRareHermitCard(),
-	new KeralisCommonHermitCard(),
-	new KeralisRareHermitCard(),
-	new MumboJumboCommonHermitCard(),
-	new MumboJumboRareHermitCard(),
-	new PearlescentMoonCommonHermitCard(),
-	new PearlescentMoonRareHermitCard(),
-	new RendogCommonHermitCard(),
-	// new RendogRareHermitCard(),
-	new StressMonster101CommonHermitCard(),
-	new StressMonster101RareHermitCard(),
-	new TangoTekCommonHermitCard(),
-	new TangoTekRareHermitCard(),
-	new TinFoilChefCommonHermitCard(),
-	new TinFoilChefRareHermitCard(),
-	new TinFoilChefUltraRareHermitCard(),
-	new VintageBeefCommonHermitCard(),
-	new VintageBeefRareHermitCard(),
-	new VintageBeefUltraRareHermitCard(),
-	new WelsknightCommonHermitCard(),
-	new WelsknightRareHermitCard(),
-	new XBCraftedCommonHermitCard(),
-	new XBCraftedRareHermitCard(),
-	new XisumavoidCommonHermitCard(),
-	new XisumavoidRareHermitCard(),
-	new ZedaphPlaysCommonHermitCard(),
-	new ZedaphPlaysRareHermitCard(),
-	new ZombieCleoCommonHermitCard(),
-	// new ZombieCleoRareHermitCard(),
+	new BananaGunCharacterCard(),
+	new BunnyBoyOkuCharacterCard(),
+	new BurgerCharacterCard(),
+	new ChickenPizzaPanCharacterCard(),
+	new MEE6CharacterCard(),
+	new DankMemerCharacterCard(),
+	new CarlBotCharacterCard(),
+	new CatSleeoCharacterCard(),
+	// new NyanCatCharacterCard(),
+	// new QuackittyCharacterCard(),
+	new IceCreamDealerCharacterCard(),
+	new Harison24CharacterCard(),
+	new FrogCharacterCard(),
+	new ChildCharacterCard(),
+	new ConfusedMemeCharacterCard(),
+// rare cards
+	new Harison25CharacterCard (),
+	new HoglinCharacterCard(),
+	new DonutCharacterCard(),
+	new KarutaCharacterCard(),
+	new CrackKittyCharacterCard(),
+	new MrCatCharacterCard(),
+	//new ZekeCharacterCard(),
+	//new CheeseCharacterCard(),
+// ultra rare cards
+	new SleepyCharacterCard(),
+	new ChubbyCharacterCard(),
 ]
 
-export default HERMIT_CARDS
+export default CHARACTER_CARDS
