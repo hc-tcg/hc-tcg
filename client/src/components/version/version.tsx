786a22613af1cc63ec78eec9efbe85846cabad90
Joseph Lyman <jmlyman424@gmail.com>
2023-04-21 12:22:56 -0400

    removed Version component

--- client/src/components/version/version.tsx
+++
@@ -1,8 +0,0 @@
-import {CONFIG} from '../../../../config'
-import css from './version.module.scss'
-
-const Version = () => {
-	return <div className={css.version}>v{CONFIG.version}</div>
-}
-
-export default Version
