966bb8e6f736a67ec4826fda76b3beb3c8e26b43
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-20 20:15:22 -0500

    Should Work

--- server/cards/card-plugins/hermits/index.js
+++ server/cards/card-plugins/hermits/index.js
@@ -1,32 +1,32 @@
 // common cards
-import BananaGunCharacterCard from './bananaGun'
-import BunnyBoyOkuCharacterCard from './bunnyBoyOku'
+import BananaGunCharacterCard from './andrew-BananaGun'
+import BunnyBoyOkuCharacterCard from './oku-BunnyBoyOku'
 import BurgerCharacterCard from './burger'
-import ChickenPizzaPanCharacterCard from './chickenPizzaPan'
+import ChickenPizzaPanCharacterCard from './will-ChickenPizzaPan'
 import MEE6CharacterCard from './MEE6'
 import DankMemerCharacterCard from './dankMemer'
 import CarlBotCharacterCard from './carlBot'
-import CatSleeoCharacterCard from './catSleeo'
+import CatSleeoCharacterCard from './sleepy-catSleeo'
 import NyanCatCharacterCard from './nyanCat'
-import QuackittyCharacterCard from './quackitty'
-import IceCreamDealerCharacterCard from './iceCreamDealer'
-import Harison24CharacterCard from './harison24'
+import QuackittyCharacterCard from './andrew-Quackitty'
+import IceCreamDealerCharacterCard from './andrew-iceCreamDealer'
+import Harison24CharacterCard from './harison-harison24'
 import FrogCharacterCard from './frog'
 import ChildCharacterCard from './child'
 import ConfusedMemeCharacterCard from './confusedMeme'
 // rare & ultra rare cards
-import Harison25CharacterCard from './harison25'
-import SleepyCharacterCard from './sleepy'
-import ChubbyCharacterCard from './chubby'
+import Harison25CharacterCard from './harison-Harison25'
+import SleepyCharacterCard from './sleep-Sleepy'
+import ChubbyCharacterCard from './chubby-Chubby'
 import HoglinCharacterCard from './hoglin'
-import DonutCharacterCard from './donut'
+import DonutCharacterCard from './donut-Donut'
 import KarutaCharacterCard from './karuta'
-import CrackKittyCharacterCard from './crackKitty'
+import CrackKittyCharacterCard from './andrew-CrackKitty'
 import MrCatCharacterCard from './mrCat'
 import ZekeCharacterCard from './zeke'
-import CheeseCharacterCard from './cheese'
+import CheeseCharacterCard from './chubby-59'
 
-const HERMIT_CARDS = [
+const CHARACTER_CARDS = [
 	new BananaGunCharacterCard(),
 	new BunnyBoyOkuCharacterCard(),
 	new BurgerCharacterCard(),
@@ -35,8 +35,8 @@ const HERMIT_CARDS = [
 	new DankMemerCharacterCard(),
 	new CarlBotCharacterCard(),
 	new CatSleeoCharacterCard(),
-	// new NyanCatCharacterCard(),
-	// new QuackittyCharacterCard(),
+	new NyanCatCharacterCard(),
+	new QuackittyCharacterCard(),
 	new IceCreamDealerCharacterCard(),
 	new Harison24CharacterCard(),
 	new FrogCharacterCard(),
