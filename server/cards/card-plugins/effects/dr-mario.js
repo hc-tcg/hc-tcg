336ddea8b329a15344a49cbb98ff180603cba1f6
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-22 01:34:43 -0500

    Update and rename milk-bucket.js to dr-mario.js

--- server/cards/card-plugins/effects/dr-mario.js
+++ server/cards/card-plugins/effects/dr-mario.js
@@ -3,17 +3,17 @@ import EffectCard from './_effect-card'
 /**
  * @typedef {import('models/game-model').GameModel} GameModel
  */
-class MilkBucketEffectCard extends EffectCard {
+class DrMarioEffectCard extends EffectCard {
 	constructor() {
 		super({
-			id: 'milk_bucket',
-			name: 'Milk Bucket',
+			id: 'dr_mario',
+			name: 'Dr. Mario',
 			rarity: 'common',
 			description:
-				'Stops POISON.\n\nCan be used on active or AFK Hermits. Discard after Use.\n\nCan also be attached to prevent POISON.\n\nDiscard after user is knocked out.',
+				'Stops RABIES.\n\nCan be used on active or AFK Characters. Discard after Use.\n\nCan also be attached to prevent RABIES.\n\nDiscard after user is knocked out.',
 		})
 		this.pickOn = 'apply'
-		this.pickReqs = [{target: 'player', type: 'hermit', amount: 1}]
+		this.pickReqs = [{target: 'player', type: 'character', amount: 1}]
 	}
 
 	/**
@@ -27,10 +27,10 @@ class MilkBucketEffectCard extends EffectCard {
 				...opponentPlayer.board.rows,
 			]
 			allRows.forEach((row) => {
-				const isPoisoned = row.ailments.some((a) => a.id === 'poison')
+				const isRabid = row.ailments.some((a) => a.id === 'rabies')
 				const hasBucket = row.effectCard?.cardId === this.id
-				if (isPoisoned && hasBucket) {
-					row.ailments = row.ailments.filter((a) => a.id !== 'poison')
+				if (isRabid && hasBucket) {
+					row.ailments = row.ailments.filter((a) => a.id !== 'rabies')
 				}
 			})
 		})
@@ -42,11 +42,11 @@ class MilkBucketEffectCard extends EffectCard {
 				const suPickedCards = pickedCardsInfo[this.id] || []
 				if (suPickedCards?.length !== 1) return 'INVALID'
 				const {row} = suPickedCards[0]
-				row.ailments = row.ailments.filter((a) => a.id !== 'poison')
+				row.ailments = row.ailments.filter((a) => a.id !== 'rabies')
 				return 'DONE'
 			}
 		})
 	}
 }
 
-export default MilkBucketEffectCard
+export default DrMarioEffectCard
