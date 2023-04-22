f73f2e188bfa900a1a710c6634a060e59a437bf8
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-22 01:16:35 -0500

    Update index.js

--- server/cards/card-plugins/items/index.js
+++ server/cards/card-plugins/items/index.js
@@ -1,45 +1,37 @@
-import BuilderCommonItemCard from './builder-common'
-import BuilderRareItemCard from './builder-rare'
-import BalancedCommonItemCard from './balanced-common'
-import BalancedRareItemCard from './balanced-rare'
-import RedstoneCommonItemCard from './redstone-common'
-import RedstoneRareItemCard from './redstone-rare'
-import PranksterCommonItemCard from './prankster-common'
-import PranksterRareItemCard from './prankster-rare'
-import ExplorerCommonItemCard from './explorer-common'
-import ExplorerRareItemCard from './explorer-rare'
-import FarmCommonItemCard from './farm-common'
-import FarmRareItemCard from './farm-rare'
-import PvPCommonItemCard from './pvp-common'
-import PvPRareItemCard from './pvp-rare'
-import SpeedrunnerCommonItemCard from './speedrunner-common'
-import SpeedrunnerRareItemCard from './speedrunner-rare'
-import TerraformCommonItemCard from './terraform-common'
-import TerraformRareItemCard from './terraform-rare'
-import MinerCommonItemCard from './miner-common'
-import MinerRareItemCard from './miner-rare'
+import AustralianCommonItemCard from './australian-common'
+import AustralianRareItemCard from './australian-rare'
+import BaconCommonItemCard from './bacon-common'
+import BaconRareItemCard from './bacon-rare'
+import BotCommonItemCard from './bot-common'
+import BotRareItemCard from './bot-rare'
+import CatCommonItemCard from './cat-common'
+import CatRareItemCard from './cat-rare'
+import IceCreamCommonItemCard from './ice-cream-common'
+import IceCreamRareItemCard from './ice-cream-rare'
+import MinecraftCommonItemCard from './minecraft-common'
+import MinecraftRareItemCard from './minecraft-rare'
+import ToddlerCommonItemCard from './toddler-common'
+import ToddlerRareItemCard from './toddler-rare'
+import AnyTypeCommonItemCard from './any-type-common'
+import AnyTypeRareItemCard from './any-type-rare'
 
 const ITEM_CARDS = [
-	new BuilderCommonItemCard(),
-	new BuilderRareItemCard(),
-	new BalancedCommonItemCard(),
-	new BalancedRareItemCard(),
-	new RedstoneCommonItemCard(),
-	new RedstoneRareItemCard(),
-	new PranksterCommonItemCard(),
-	new PranksterRareItemCard(),
-	new ExplorerCommonItemCard(),
-	new ExplorerRareItemCard(),
-	new FarmCommonItemCard(),
-	new FarmRareItemCard(),
-	new PvPCommonItemCard(),
-	new PvPRareItemCard(),
-	new SpeedrunnerCommonItemCard(),
-	new SpeedrunnerRareItemCard(),
-	new TerraformCommonItemCard(),
-	new TerraformRareItemCard(),
-	new MinerCommonItemCard(),
-	new MinerRareItemCard(),
+	new AustralianCommonItemCard(),
+	new AustralianRareItemCard(),
+	new BaconCommonItemCard(),
+	new BaconRareItemCard(),
+	new BotCommonItemCard(),
+	new BotRareItemCard(),
+	new CatCommonItemCard(),
+	new CatRareItemCard(),
+	new IceCreamCommonItemCard(),
+	new IceCreamRareItemCard(),
+	new MinecraftCommonItemCard(),
+	new MinecraftRareItemCard(),
+	new ToddlerCommonItemCard(),
+	new ToddlerRareItemCard(),
+	new AnyTypeCommonItemCard(),
+	new AnyTypeRareItemCard(),
 ]
 
 export default ITEM_CARDS
