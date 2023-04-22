b80de4f5d5cfc6bdf0cd51c703597462bb7c0d4e
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-22 01:19:02 -0500

    Update and rename redstone-rare.js to any-type-rare.js

---
+++ server/cards/card-plugins/items/any-type-rare.js
@@ -0,0 +1,16 @@
+import ItemCard from './_item-card'
+
+class AnyTypeRareItemCard extends ItemCard {
+	constructor() {
+		super({
+			id: 'item_any_type_rare',
+			name: 'Any Type',
+			rarity: 'ultra-rare',
+			hermitType: 'any',
+		})
+	}
+
+	register(game) {}
+}
+
+export default AnyTypeRareItemCard
