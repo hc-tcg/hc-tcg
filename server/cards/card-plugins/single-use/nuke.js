ca509ccb8c14669611ae81cafb208fb103f8115d
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-27 16:06:23 -0500

    Update and rename crossbow.js to nuke.js

--- server/cards/card-plugins/single-use/nuke.js
+++ server/cards/card-plugins/single-use/nuke.js
@@ -4,23 +4,23 @@ import SingleUseCard from './_single-use-card'
  * @typedef {import('models/game-model').GameModel} GameModel
  */
 
-class CrossbowSingleUseCard extends SingleUseCard {
+class NukeSingleUseCard extends SingleUseCard {
 	constructor() {
 		super({
-			id: 'crossbow',
+			id: '',
 			name: 'Crossbow',
 			rarity: 'rare',
 			description:
-				"Does +40hp damage to opposing Hermit and +10hp damage to AFK Hermit of player's choice.\n\nDiscard after use.",
+				"Does +40hp damage to all of opposing characters active and benched; take 40 damage recoil\n\nDiscard after use.",
 		})
-		this.damage = {target: 40, afkTarget: 10}
+		this.damage = {target: 30, afkTarget: 30}
 
 		this.pickOn = 'attack'
 		this.useReqs = [
-			{target: 'opponent', type: 'hermit', amount: 1, active: true},
+			{target: 'opponent', type: 'character', amount: 1, active: true},
 		]
 		this.pickReqs = [
-			{target: 'opponent', type: 'hermit', amount: 1, active: false},
+			{target: 'opponent', type: 'character', amount: 2, active: false},
 		]
 	}
 
@@ -37,16 +37,10 @@ class CrossbowSingleUseCard extends SingleUseCard {
 				return target
 			}
 
-			// only attack selected afk target
-			const crossbowPickedCards = pickedCardsInfo[this.id] || []
-			if (crossbowPickedCards.length !== 1) return target
-			const pickedHermit = crossbowPickedCards[0]
-			if (pickedHermit.row !== target.row) return target
-
 			target.extraEffectDamage += this.damage.afkTarget
 			return target
 		})
 	}
 }
 
-export default CrossbowSingleUseCard
+export default NukeSingleUseCard
