0474c2f1d56426b604dce9d3e77aa0c81c9e2862
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-26 20:08:42 -0500

    Update tnt.js

--- server/cards/card-plugins/single-use/tnt.js
+++ server/cards/card-plugins/single-use/tnt.js
@@ -4,11 +4,11 @@ import SingleUseCard from './_single-use-card'
  * @typedef {import('models/game-model').GameModel} GameModel
  */
 
-class TNTSingleUseCard extends SingleUseCard {
+class BombSingleUseCard extends SingleUseCard {
 	constructor() {
 		super({
-			id: 'tnt',
-			name: 'TNT',
+			id: 'bomb',
+			name: 'Bomb',
 			rarity: 'common',
 			description:
 				'Does +60hp damage to opposing Hermit.\n\nAlso does +20hp damage to user.\n\nDiscard after use.',
@@ -16,7 +16,7 @@ class TNTSingleUseCard extends SingleUseCard {
 		this.damage = {target: 60, self: 20}
 
 		this.useReqs = [
-			{target: 'opponent', type: 'hermit', amount: 1, active: true},
+			{target: 'opponent', type: 'character', amount: 1, active: true},
 		]
 	}
 
@@ -35,4 +35,4 @@ class TNTSingleUseCard extends SingleUseCard {
 	}
 }
 
-export default TNTSingleUseCard
+export default BombSingleUseCard
