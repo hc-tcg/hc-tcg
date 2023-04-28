fcb174c94fc79091dd7b20b5e814945106581ebc
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-27 19:56:33 -0500

    goodbye 1st ability

--- server/cards/card-plugins/hermits/sleepy-Sleepy.js
+++ server/cards/card-plugins/hermits/sleepy-Sleepy.js
@@ -30,40 +30,6 @@ class SleepyCharacterCard extends CharacterCard {
 		})
 	}
 
-	/**
-	 * @param {GameModel} game
-	 */
-	register(game) {
-		game.hooks.attack.tap(this.id, (target, turnAction, attackState) => {
-			const {currentPlayer} = game.ds
-			const {
-				attackerCharacterCard,
-				attackerCharacterInfo,
-				typeAction,
-				attackerActiveRow,
-			} = attackState
-
-			if (typeAction !== 'PRIMARY_ATTACK') return target
-			if (!target.isActive) return target
-			if (attackerCharacterCard.cardId !== this.id) return target
-
-			const iceCreamDealerRows = currentPlayer.board.rows.filter((row) => {
-				const isIceCreamDealer = row.characterCard?.cardId.startsWith('Andrew-IceCreamDealer')
-				return isIceCreamDealer
-			})
-			const total = Math.min(iceCreamDealerRows.length, 2)
-			target.extraHermitDamage += total * 20
-
-			return target
-			},
-				      
-				      
-
-			},
-		})
-
-	
-	}
-
+}
 
 export default SleepyCharacterCard
