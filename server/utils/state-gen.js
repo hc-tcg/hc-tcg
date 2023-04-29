0db854ff374a29a067a29160d2036197f014eb76
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-29 16:36:18 -0500

    Update state-gen.js

--- server/utils/state-gen.js
+++ server/utils/state-gen.js
@@ -1,104 +1,132 @@
 import CARDS from '../cards'
 import STRENGTHS from '../const/strengths'
 import {CONFIG, DEBUG_CONFIG} from '../../config'
+import {getCardCost, getCardRank} from './validation'
 
 /**
  * @typedef {import("models/game-model").GameModel} GameModel
  * @typedef {import("models/player-model").PlayerModel} PlayerModel
+ * @typedef {import("common/types/game-state").GameState} GameState
+ * @typedef {import("common/types/game-state").PlayerState} PlayerState
+ * @typedef {import("common/types/game-state").RowState} RowState
+ * @typedef {import("common/types/cards").HermitCardT} HermitCardT
+ * @typedef {import("common/types/cards").EffectCardT} EffectCardT
+ * @typedef {import("common/types/cards").ItemCardT} ItemCardT
+ * @typedef {import('common/types/game-state').LocalGameState} LocalGameState
+ * @typedef {import('common/types/game-state').LocalPlayerState} LocalPlayerState
  */
 
 function randomBetween(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+/** @type {(cardInfo: CardInfoT) => cardInfo is CharacterCardT | ItemCardT} */
+const isCharacterOrItem = (cardInfo) => ['character', 'item'].includes(cardInfo.type)
+
+/** @type {(cardInfo: CardInfoT) => cardInfo is CharacterCardT} */
+const isCharacter = (cardInfo) => cardInfo.type === 'character'
+
+/** @type {(cardInfo: CardInfoT) => cardInfo is EffectCardT} */
+const isEffect = (cardInfo) => ['effect', 'single_use'].includes(cardInfo.type)
+
 export function getStarterPack() {
 	const limits = CONFIG.limits
+
+	// only allow some starting types
+	const startingTypes = ['cat', 'bacon', 'bot', 'minecraft']
 	const characterTypesCount = randomBetween(2, 3)
 	const characterTypes = Object.keys(STRENGTHS)
+		.filter((type) => startingTypes.includes(type))
 		.sort(() => 0.5 - Math.random())
 		.slice(0, characterTypesCount)
 
 	const cards = Object.values(CARDS).filter(
 		(cardInfo) =>
-			!['character', 'item'].includes(cardInfo.type) ||
-			characterTypes.includes(cardInfo.characterType)
+			!isCharacterOrItem(cardInfo) || characterTypes.includes(cardInfo.characterType)
 	)
 
-	const characterCard = cards.filter((cardInfo) => cardInfo.type === 'character')
-	const effectCards = cards.filter((cardInfo) =>
-		['effect', 'single_use'].includes(cardInfo.type)
-	)
-
+	const effectCards = cards.filter(isEffect)
 	const characterCount = characterTypesCount === 2 ? 8 : 10
+
 	const deck = []
 
-	const itemsCosts = {}
+	let itemCount = 0
+	let itemCounts = {
+		[characterTypes[0]]: {
+			items: 0,
+			tokens: 0,
+		},
+		[characterTypes[1]]: {
+			items: 0,
+			tokens: 0,
+		},
+	}
+	if (characterTypes[2]) {
+		itemCounts[characterTypes[2]] = {
+			items: 0,
+			tokens: 0,
+		}
+	}
+	let tokens = 0
 
-	// characters
-	while (deck.length < characterCount) {
-		const randomIndex = Math.floor(Math.random() * characterCard.length)
-		const characterCard = characterCard[randomIndex]
+	// characters, but not diamond ones
+	let characterCards = cards
+		.filter(isCharacter)
+		.filter((card) => getCardRank(card.id).name !== 'diamond')
 
-		const duplicates = deck.filter((card) => card.id === characterCard.id)
-		const rarity = characterCard.rarity
-		if (duplicates.length >= limits.maxDuplicates) continue
-		if (rarity === 'ultra_rare' && duplicates.length >= 1) continue
-		if (rarity === 'rare' && duplicates.length >= 2) continue
+	while (deck.length < characterCount && characterCards.length > 0) {
+		const randomIndex = Math.floor(Math.random() * characterCards.length)
+		const characterCard = characterCards[randomIndex]
 
-		deck.push(characterCard)
+		// remove this option
+		characterCards = characterCards.filter((card, index) => index !== randomIndex)
 
-		const cost = characterCard.secondary.cost.filter(
-			(characterType) => characterType === characterCard.characterType
-		).length
-		itemsCosts[characterCard.characterType] = itemsCosts[characterCard.characterType] || 0
-		itemsCosts[characterCard.characterType] += cost
+		// add 1 - 3 of this character
+		const characterAmount = Math.min(
+			randomBetween(1, 3),
+			characterCount - deck.length
+		)
+
+		tokens += getCardCost(characterCard) * characterAmount
+		for (let i = 0; i < characterAmount; i++) {
+			deck.push(characterCard)
+			itemCounts[characterCard.characterType].items += 2
+			itemCount += 2
+		}
 	}
 
 	// items
-	for (let characterType in itemsCosts) {
-		let total = itemsCosts[characterType]
-		let totalRare = 0
-		if (total < 3) total = 3
-		if (total > 4) {
-			totalRare += 1
-			total -= 1
-		}
-		if (total > 6) {
-			totalRare += 1
-			total -= 1
-		}
-		if (total > 8) total = 8
+	for (let characterType in itemCounts) {
+		let counts = itemCounts[characterType]
 
-		const currenTotalRare = deck.filter((card) => card.rarity === 'rare').length
-		if (totalRare + currenTotalRare > limits.maxRare) {
-			const prevTotalRare = totalRare
-			totalRare = Math.max(currenTotalRare - limits.maxRare, 0)
-			total += prevTotalRare - totalRare
-		}
-
-		for (let i = 0; i < totalRare; i++)
-			deck.push(CARDS[`item_${characterType}_rare`])
-		for (let i = 0; i < total; i++)
+		for (let i = 0; i < counts.items; i++) {
 			deck.push(CARDS[`item_${characterType}_common`])
+		}
 	}
 
+	let loopBreaker = 0
 	// effects
 	while (deck.length < limits.maxCards) {
 		const effectCard =
 			effectCards[Math.floor(Math.random() * effectCards.length)]
 
-		const totalRare = deck.filter((card) => card.rarity === 'rare').length
-		const totalUr = deck.filter((card) => card.rarity === 'ultra_rare').length
-
-		if (totalRare >= limits.maxRare && effectCard.rarity === 'rare') continue
-		if (totalUr >= limits.maxUltraRare && effectCard.rarity === 'ultra_rare')
-			continue
-
 		const duplicates = deck.filter((card) => card.id === effectCard.id)
-		const rarity = effectCard.rarity
-		if (rarity === 'ultra_rare' && duplicates.length >= 1) continue
-		if (rarity === 'rare' && duplicates.length >= 2) continue
 		if (duplicates.length >= limits.maxDuplicates) continue
+
+		const tokenCost = getCardCost(effectCard)
+		if (tokens + tokenCost >= limits.maxDeckCost) {
+			loopBreaker++
+			continue
+		} else {
+			loopBreaker = 0
+		}
+		if (loopBreaker >= 10000) {
+			const err = new Error()
+			console.log('Broke out of loop while generating starter deck!', err.stack)
+			break
+		}
+
+		tokens += tokenCost
 		deck.push(effectCard)
 	}
 
@@ -131,10 +159,7 @@ export function getEmptyRow() {
  * @returns {PlayerState}
  */
 export function getPlayerState(player) {
-	const pack = player.playerDeck.map((cardId) => ({
-		cardId,
-		cardInstance: Math.random() + '_' + Math.random(),
-	}))
+	const pack = player.playerDeck.cards
 
 	// shuffle cards
 	pack.sort(() => 0.5 - Math.random())
@@ -168,9 +193,8 @@ export function getPlayerState(player) {
 		followUp: null,
 		lives: 3,
 		hand,
-		rewards: pack.slice(7, 10),
 		discarded: [],
-		pile: pack.slice(10),
+		pile: pack.slice(7),
 		custom: {},
 		board: {
 			activeRow: null,
@@ -193,9 +217,7 @@ export function getGameState(game) {
 	const gameState = {
 		turn: 0,
 		order: playerIds,
-		turnPlayerId: null,
-		turnTime: null,
-		turnRemaining: null,
+		turnPlayerId: /** @type {any} */ (null),
 		players: playerIds.reduce(
 			(playerStates, playerId) => ({
 				...playerStates,
@@ -203,6 +225,93 @@ export function getGameState(game) {
 			}),
 			{}
 		),
+
+		timer: {
+			turnTime: /** @type {any} */ (null),
+			turnRemaining: /** @type {any} */ (null),
+		},
 	}
 	return gameState
+}
+
+/**
+ *
+ * @param {PlayerState} playerState
+ * @returns {LocalPlayerState}
+ */
+export function getLocalPlayerState(playerState) {
+	/** @type {LocalPlayerState} */
+	const localPlayerState = {
+		id: playerState.id,
+		followUp: playerState.followUp,
+		playerName: playerState.playerName,
+		censoredPlayerName: playerState.censoredPlayerName,
+		coinFlips: playerState.coinFlips,
+		custom: playerState.custom,
+		lives: playerState.lives,
+		board: playerState.board,
+	}
+	return localPlayerState
+}
+
+/**
+ *
+ * @param {GameModel} game
+ * @param {PlayerModel} player
+ * @param {AvailableActionsT} availableActions
+ * @param {Array<string>} pastTurnActions
+ * @param {AvailableActionsT} opponentAvailableActions
+ * @returns {LocalGameState | null}
+ */
+export function getLocalGameState(
+	game,
+	player,
+	availableActions = [],
+	pastTurnActions = [],
+	opponentAvailableActions = []
+) {
+	const opponentPlayerId = game
+		.getPlayerIds()
+		.find((id) => id !== player.playerId)
+	if (!opponentPlayerId) {
+		return null
+	}
+
+	const playerState = game.state.players[player.playerId]
+	const opponentState = game.state.players[opponentPlayerId]
+
+	// convert player states
+	/** @type {Record<string, LocalPlayerState>} */
+	const players = {}
+	players[player.playerId] = getLocalPlayerState(playerState)
+	players[opponentPlayerId] = getLocalPlayerState(opponentState)
+
+	/** @type {LocalGameState} */
+	const localGameState = {
+		turn: game.state?.turn || 0,
+		order: game.state?.order || [],
+
+		// personal info
+		hand: playerState.hand,
+		pileCount: playerState.pile.length,
+		discarded: playerState.discarded,
+
+		// ids
+		playerId: player.playerId,
+		opponentPlayerId: opponentPlayerId,
+		currentPlayerId: game.ds.currentPlayer.id,
+
+		players,
+
+		pastTurnActions:
+			player.playerId === game.ds.currentPlayer.id ? pastTurnActions : [],
+		availableActions:
+			player.playerId === game.ds.currentPlayer.id
+				? availableActions
+				: opponentAvailableActions,
+
+		timer: game.state.timer,
+	}
+
+	return localGameState
 }
