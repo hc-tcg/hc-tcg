19df0d52fb827d27035d09410e2624759c6f08ea
Sense101 <personal.sense101@gmail.com>
2023-05-08 20:01:52 +0100

    Remove tournament notif

--- client/src/app/game/end-game-overlay/end-game-overlay.tsx
+++ client/src/app/game/end-game-overlay/end-game-overlay.tsx
@@ -56,9 +56,6 @@ const EndGameOverlay = ({outcome, reason}: Props) => {
 				{!reason || !['you_won', 'you_lost'].includes(outcome)
 					? MESSAGE[outcome]
 					: null}
-				<br />
-				<br />
-				Tournament player? Screenshot this page!
 			</div>
 			<div className={css.buttonWrapper}>
 				<Button size="small" onClick={handleClick}>
