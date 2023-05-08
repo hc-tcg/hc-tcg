c32bc8ad2417e4e9dfc355f215dc203780199f7c
Sense101 <personal.sense101@gmail.com>
2023-05-08 16:10:50 +0100

    Add try catch to firebase logs

--- server/plugins/firebase-logs.js
+++ server/plugins/firebase-logs.js
@@ -72,56 +72,60 @@ class FirebaseLogs {
 		})
 
 		root.hooks.gameRemoved.tap(this.id, (game) => {
-			const playerStates = Object.values(game.state.players)
-			const gameLog = this.gameLogs[game.id]
-			if (!gameLog) return
+			try {
+				const playerStates = Object.values(game.state.players)
+				const gameLog = this.gameLogs[game.id]
+				if (!gameLog) return
 
-			if (
-				!game.endInfo.outcome ||
-				['error', 'timeout'].includes(game.endInfo.outcome)
-			) {
+				if (
+					!game.endInfo.outcome ||
+					['error', 'timeout'].includes(game.endInfo.outcome)
+				) {
+					delete this.gameLogs[game.id]
+					return
+				}
+
+				let ref = '/logs'
+				let summaryObj = {
+					startHand1: gameLog.startHand1,
+					startHand2: gameLog.startHand2,
+					startTimestamp: gameLog.startTimestamp,
+					startDeck: gameLog.startDeck,
+					endTimestamp: new Date().getTime(),
+					turns: game.state.turn,
+					world: CONFIG.world,
+				}
+				if (gameLog.type === 'private') {
+					ref = `/private-logs/${game.code}`
+				}
+
+				let pid0 = playerStates[0].id
+				root.players[pid0]?.socket.emit('gameoverstat', {
+					outcome: game.endInfo.outcome,
+					won: game.endInfo.winner === pid0,
+				})
+				summaryObj.deck1 = root.players[pid0]?.playerDeck
+
+				let pid1 = playerStates[1].id
+				root.players[pid1]?.socket.emit('gameoverstat', {
+					outcome: game.endInfo.outcome,
+					won: game.endInfo.winner === pid1,
+				})
+				summaryObj.deck2 = root.players[pid1]?.playerDeck
+				if (game.endInfo.winner === pid0) {
+					summaryObj.outcome = 'deck1win'
+				} else if (game.endInfo.winner === pid1) {
+					summaryObj.outcome = 'deck2win'
+				} else {
+					summaryObj.outcome = 'tie'
+				}
+				this.db.ref(ref).push(summaryObj)
+			} catch (err) {
+				console.log('Firebase Error: ' + err)
+			} finally {
+				// game is over, delete log
 				delete this.gameLogs[game.id]
-				return
 			}
-
-			let ref = '/logs'
-			let summaryObj = {
-				startHand1: gameLog.startHand1,
-				startHand2: gameLog.startHand2,
-				startTimestamp: gameLog.startTimestamp,
-				startDeck: gameLog.startDeck,
-				endTimestamp: new Date().getTime(),
-				turns: game.state.turn,
-				world: CONFIG.world,
-			}
-			if (gameLog.type === 'private') {
-				ref = `/private-logs/${game.code}`
-			}
-
-			let pid0 = playerStates[0].id
-			root.players[pid0]?.socket.emit('gameoverstat', {
-				outcome: game.endInfo.outcome,
-				won: game.endInfo.winner === pid0,
-			})
-			summaryObj.deck1 = root.players[pid0]?.playerDeck
-
-			let pid1 = playerStates[1].id
-			root.players[pid1]?.socket.emit('gameoverstat', {
-				outcome: game.endInfo.outcome,
-				won: game.endInfo.winner === pid1,
-			})
-			summaryObj.deck2 = root.players[pid1]?.playerDeck
-			if (game.endInfo.winner === pid0) {
-				summaryObj.outcome = 'deck1win'
-			} else if (game.endInfo.winner === pid1) {
-				summaryObj.outcome = 'deck2win'
-			} else {
-				summaryObj.outcome = 'tie'
-			}
-			this.db.ref(ref).push(summaryObj)
-
-			// game is over, delete log
-			delete this.gameLogs[game.id]
 		})
 	}
 }
