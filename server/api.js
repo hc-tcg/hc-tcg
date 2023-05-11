8bf5e0fe60b2b9ee3c60bf2244f6a76b9cd5c721
Sense101 <personal.sense101@gmail.com>
2023-05-11 14:45:14 +0100

    Don't error if there is no api key

--- server/api.js
+++ server/api.js
@@ -6,17 +6,21 @@ import root from './models/root-model'
  * @param {import("express").Express} app
  */
 export function registerApis(app) {
-	const apiKeys = require('./apiKeys.json')
-	app.get('/games', (req, res) => {
-		const apiKey = req.header('api-key')
-		if (apiKey) {
-			if (apiKeys.keys.includes(apiKey)) {
-				res.status(201).send(root.getGames())
+	let apiKeys = null
+	try {
+		apiKeys = require('./apiKeys.json')
+	} finally {
+		app.get('/games', (req, res) => {
+			const apiKey = req.header('api-key')
+			if (apiKey) {
+				if (apiKeys?.keys.includes(apiKey)) {
+					res.status(201).send(root.getGames())
+				} else {
+					res.status(403).send('Access denied - Invalid API key')
+				}
 			} else {
-				res.status(403).send('Access denied - Invalid API key')
+				res.status(403).send('Access denied - Please provide an api key')
 			}
-		} else {
-			res.status(403).send('Access denied - Please provide an api key')
-		}
-	})
+		})
+	}
 }
