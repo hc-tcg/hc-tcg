74d1406310db8536f6172ba54428a6e879b28db2
NikoUY <nikod.uy@gmail.com>
2023-05-15 15:30:01 -0300

    Fix #136 (#139)
    * Handle Emerald-Bed (#136) & reset sleeping counter
    
    * Formatting and fixed comment.
    
    * Use the cardInstance to check if it's the same bed

--- server/cards/card-plugins/single-use/emerald.js
+++ server/cards/card-plugins/single-use/emerald.js
@@ -4,7 +4,6 @@ import SingleUseCard from './_single-use-card'
  * @typedef {import('models/game-model').GameModel} GameModel
  */
 
-// TODO - Make this work with bed (sleeping needs to reset counter)
 class EmeraldSingleUseCard extends SingleUseCard {
 	constructor() {
 		super({
@@ -29,7 +28,6 @@ class EmeraldSingleUseCard extends SingleUseCard {
 			const {singleUseInfo, playerActiveRow, opponentActiveRow} = game.ds
 			if (singleUseInfo?.id === this.id) {
 				if (!playerActiveRow || !opponentActiveRow) return 'INVALID'
-				// TODO - Handle bed
 				const pEffect = playerActiveRow?.effectCard
 				const oEffect = opponentActiveRow?.effectCard
 				playerActiveRow.effectCard = oEffect
