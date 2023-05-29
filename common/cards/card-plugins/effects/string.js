d749cec982d81771b7205697d103ca0a632b75dd
Alice <63879236+Benji42@users.noreply.github.com>
2023-05-29 15:35:19 -0400

    Fix string (#171)

--- common/cards/card-plugins/effects/string.js
+++ common/cards/card-plugins/effects/string.js
@@ -1,5 +1,4 @@
 import EffectCard from './_effect-card'
-import {discardCard} from '../../../../server/utils'
 
 /**
  * @typedef {import('models/game-model').GameModel} GameModel
@@ -14,24 +13,21 @@ class StringEffectCard extends EffectCard {
 			description:
 				"Placed on any of the opposing player's effect or item slots. Prevents other cards from being placed there.",
 		})
-
-		this.attachReq = {target: 'opponent', type: ['effect', 'item']}
 	}
+
 	/**
 	 * @param {GameModel} game
-	 * @param {string} targetPlayerId
-	 * @param {number} rowIndex
-	 * @param {CardTypeT} slotType
+	 * @param {CardPos} pos
 	 * @returns {boolean}
 	 */
-	canAttach(game, targetPlayerId, rowIndex, slotType) {
+	canAttach(game, pos) {
 		const {opponentPlayer} = game.ds
 
 		// attach to effect or item slot
-		if (slotType !== 'effect' && slotType !== 'item') return false
+		if (pos.slotType !== 'effect' && pos.slotType !== 'item') return false
 
 		// can only attach to opponent
-		if (targetPlayerId !== opponentPlayer.id) return false
+		if (pos.playerId !== opponentPlayer.id) return false
 
 		// we don't care if there's a hermit there or not
 		return true
