a3e467dcd7fc6556484f08ed240e9a281eb3fc38
Niko <nikod.uy@gmail.com>
2023-05-30 01:06:52 -0300

    Command Block ported

--- common/cards/card-plugins/effects/command-block.js
+++ common/cards/card-plugins/effects/command-block.js
@@ -1,9 +1,5 @@
 import EffectCard from './_effect-card'
 
-/**
- * @typedef {import('models/game-model').GameModel} GameModel
- */
-
 class CommandBlockEffectCard extends EffectCard {
 	constructor() {
 		super({
@@ -12,15 +8,10 @@ class CommandBlockEffectCard extends EffectCard {
 			rarity: 'rare',
 			description:
 				"Attach to any active or AFK Hermit.\n\nItems attached to this Hermit become any type.\n\nThis card can only be removed once the Hermit it's attached to is knocked out.",
+			removable: false
 		})
 
-		this.removable = false
 	}
-
-	/**
-	 * @param {GameModel} game
-	 */
-	register(game) {}
 }
 
 export default CommandBlockEffectCard
