c448c64952024f8c68faab1781afa054ebee5a27
Niko <nikod.uy@gmail.com>
2023-06-07 14:38:53 -0300

    Fixed imports and removed unnecessary canAttach

--- common/cards/card-plugins/single-use/splash-potion-of-healing-ii.js
+++ common/cards/card-plugins/single-use/splash-potion-of-healing-ii.js
@@ -1,10 +1,10 @@
 import SingleUseCard from './_single-use-card'
 import {HERMIT_CARDS} from '../../index'
+import {GameModel} from '../../../../server/models/game-model'
 
 /**
-* @typedef {import('../../../../server/models/game-model').GameModel} GameModel
-* @typedef {import('../../../types/cards').CardPos} CardPos
-* @typedef {import('../../../types/pick-process').PickedSlotsInfo} PickedSlotsInfo
+* @typedef {import('common/types/cards').CardPos} CardPos
+* @typedef {import('common/types/pick-process').PickedSlots} PickedSlots
 */
 
 class SplashPotionOfHealingIISingleUseCard extends SingleUseCard {
@@ -12,23 +12,10 @@ class SplashPotionOfHealingIISingleUseCard extends SingleUseCard {
 		super({
 			id: 'splash_potion_of_healing_ii',
 			name: 'Splash Potion of Healing',
-			rarity: 'common',
+			rarity: 'rare',
 			description:
 				"Heal each of your active and AFK Hermits 30hp.",
 		})
-	}
-
-	/**
-	 * @param {GameModel} game
-	 * @param {CardPos} pos
-	 */
-	canAttach(game, pos) {
-		if (super.canAttach(game, pos) === 'NO') return 'INVALID'
-
-		const {playerActiveRow} = game.ds
-		if (!playerActiveRow) return 'INVALID'
-
-		return 'YES'
 	}
 
 	canApply() {
@@ -38,17 +25,15 @@ class SplashPotionOfHealingIISingleUseCard extends SingleUseCard {
 	/**
 	 * @param {GameModel} game
 	 * @param {string} instance
-	 * @param {PickedSlotsInfo} pickedSlots
+	 * @param {CardPos} pos
+	 * @param {PickedSlots} pickedSlots
 	 */
-	onApply(game, instance, pickedSlots) {
-		const {singleUseInfo, currentPlayer} = game.ds
-		if (singleUseInfo?.id === this.id) {
-			for (let row of currentPlayer.board.rows) {
-				if (!row.hermitCard) continue
-				const currentRowInfo = HERMIT_CARDS[row.hermitCard.cardId]
-				if (!currentRowInfo) continue
-				row.health = Math.min(row.health + 30, currentRowInfo.health)
-			}
+	onApply(game, instance, pos, pickedSlots) {
+		for (let row of pos.player.board.rows) {
+			if (!row.hermitCard) continue
+			const currentRowInfo = HERMIT_CARDS[row.hermitCard.cardId]
+			if (!currentRowInfo) continue
+			row.health = Math.min(row.health + 30, currentRowInfo.health)
 		}
 	}
 }
--- common/cards/card-plugins/single-use/splash-potion-of-healing.js
+++ common/cards/card-plugins/single-use/splash-potion-of-healing.js
@@ -1,10 +1,10 @@
 import SingleUseCard from './_single-use-card'
 import {HERMIT_CARDS} from '../../index'
+import {GameModel} from '../../../../server/models/game-model'
 
 /**
-* @typedef {import('../../../../server/models/game-model').GameModel} GameModel
-* @typedef {import('../../../types/cards').CardPos} CardPos
-* @typedef {import('../../../types/pick-process').PickedSlotsInfo} PickedSlotsInfo
+* @typedef {import('common/types/cards').CardPos} CardPos
+* @typedef {import('common/types/pick-process').PickedSlots} PickedSlots
 */
 
 class SplashPotionOfHealingSingleUseCard extends SingleUseCard {
@@ -18,19 +18,6 @@ class SplashPotionOfHealingSingleUseCard extends SingleUseCard {
 		})
 	}
 
-	/**
-	 * @param {GameModel} game
-	 * @param {CardPos} pos
-	 */
-	canAttach(game, pos) {
-		if (super.canAttach(game, pos) === 'NO') return 'INVALID'
-
-		const {playerActiveRow} = game.ds
-		if (!playerActiveRow) return 'INVALID'
-
-		return 'YES'
-	}
-
 	canApply() {
 		return true
 	}
@@ -38,17 +25,15 @@ class SplashPotionOfHealingSingleUseCard extends SingleUseCard {
 	/**
 	 * @param {GameModel} game
 	 * @param {string} instance
-	 * @param {PickedSlotsInfo} pickedSlots
+	 * @param {CardPos} pos
+	 * @param {PickedSlots} pickedSlots
 	 */
-	onApply(game, instance, pickedSlots) {
-		const {singleUseInfo, currentPlayer} = game.ds
-		if (singleUseInfo?.id === this.id) {
-			for (let row of currentPlayer.board.rows) {
-				if (!row.hermitCard) continue
-				const currentRowInfo = HERMIT_CARDS[row.hermitCard.cardId]
-				if (!currentRowInfo) continue
-				row.health = Math.min(row.health + 20, currentRowInfo.health)
-			}
+	onApply(game, instance, pos, pickedSlots) {
+		for (let row of pos.player.board.rows) {
+			if (!row.hermitCard) continue
+			const currentRowInfo = HERMIT_CARDS[row.hermitCard.cardId]
+			if (!currentRowInfo) continue
+			row.health = Math.min(row.health + 20, currentRowInfo.health)
 		}
 	}
 }
