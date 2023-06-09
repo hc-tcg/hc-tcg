dda35321ec7208850c18ad5ebab60805a1c72f92
Niko <nikod.uy@gmail.com>
2023-06-09 20:55:45 -0300

    Shield Ported

--- common/cards/card-plugins/effects/shield.js
+++ common/cards/card-plugins/effects/shield.js
@@ -1,7 +1,10 @@
 import EffectCard from './_effect-card'
 import {discardCard} from '../../../../server/utils'
-import {AttackModel} from '../../../../server/models/attack-model'
 import {GameModel} from '../../../../server/models/game-model'
+
+/**
+ * @typedef {import('common/types/cards').CardPos} CardPos
+*/
 
 class ShieldEffectCard extends EffectCard {
 	constructor() {
@@ -10,33 +13,57 @@ class ShieldEffectCard extends EffectCard {
 			name: 'Shield',
 			rarity: 'common',
 			description:
-				'Protects from the first +10hp damage taken.\n\nDiscard following any damage taken.',
+			"Prevent up to 60hp damage for 1 turn.\n\nDiscard following any damage taken.",
 		})
 	}
 
 	/**
-	 *
 	 * @param {GameModel} game
 	 * @param {string} instance
-	 * @param {AttackModel} attack
+	 * @param {CardPos} pos
 	 */
-	onDefence(game, instance, attack) {
-		if (attack.type !== 'ailment') {
-			attack.defence.damageReduction += 10
+	onAttach(game, instance, pos) {
+		const {otherPlayer, player} = pos
+		const instanceKey = this.getInstanceKey(instance)
+
+		otherPlayer.hooks.onAttack[instance] = (attack, pickedSlots) => {
+			if (attack.target.index !== pos.rowIndex || attack.type === 'ailment') return
+
+			if (player.custom[instanceKey] === undefined) {
+				player.custom[instanceKey] = 0
+			} 
+			
+			const totalReduction = player.custom[instanceKey]
+
+			if (totalReduction < 60) {
+				const damageReduction = Math.min(attack.damage, 60 - totalReduction)
+				player.custom[instanceKey] += damageReduction
+				attack.reduceDamage(damageReduction)
+			}
 		}
 
-		return attack
+		otherPlayer.hooks.afterAttack[instance] = (attackResult) => {
+			const {player, row} = pos
+
+			if (player.custom[instanceKey] > 0 && row) {
+				discardCard(game, row.effectCard)
+			}
+		}
+
+		otherPlayer.hooks.turnEnd[instance] = () => {
+			delete player.custom[instanceKey]
+		}
 	}
 
 	/**
 	 * @param {GameModel} game
 	 * @param {string} instance
-	 * @param {import('models/attack-model').AttackResult} attackResult
+	 * @param {CardPos} pos
 	 */
-	afterDefence(game, instance, attackResult) {
-		if (attackResult.blockedDamage > 0) {
-			discardCard(game, {cardId: this.id, cardInstance: instance})
-		}
+	onDetach(game, instance, pos) {
+		const {otherPlayer, player} = pos
+		delete otherPlayer.hooks.onAttack[instance]
+		delete player.custom[this.getInstanceKey(instance)]
 	}
 }
 
