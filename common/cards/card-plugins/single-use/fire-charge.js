88d8247da833d9d7f6673f4194b8ec9b97e02561
Niko <nikod.uy@gmail.com>
2023-06-10 03:44:14 -0300

    Updated Fire Charge

--- common/cards/card-plugins/single-use/fire-charge.js
+++ common/cards/card-plugins/single-use/fire-charge.js
@@ -1,10 +1,16 @@
-import {discardCard, discardSingleUse, rowHasItem} from '../../../../server/utils'
+import {
+	discardCard,
+	discardSingleUse,
+	rowHasItem,
+	isRemovable,
+} from '../../../../server/utils'
 import SingleUseCard from './_single-use-card'
+import {GameModel} from '../../../../server/models/game-model'
 
 /**
  * @typedef {import('common/types/pick-process').PickRequirmentT} PickRequirmentT
- * @typedef {import('common/types/pick-process').PickedSlotsInfo} PickedSlotsInfo
- * @typedef {import('server/models/game-model').GameModel} GameModel
+ * @typedef {import('common/types/pick-process').PickedSlots} PickedSlots
+ * @typedef {import('common/types/cards').CardPos} CardPos
  */
 
 class FireChargeSingleUseCard extends SingleUseCard {
@@ -14,80 +20,88 @@ class FireChargeSingleUseCard extends SingleUseCard {
 			name: 'Fire Charge',
 			rarity: 'common',
 			description:
-				"Discard 1 attached item or effect card from your active or AFK Hermit.\n\nYou can use another single use effect card this turn.",
+				'Discard 1 attached item or effect card from your active or AFK Hermit.\n\nYou can use another single use effect card this turn.',
+
+			pickOn: 'apply',
+			pickReqs: /** @satisfies {Array<PickRequirmentT>} */ ([
+				{target: 'player', type: ['item', 'effect'], amount: 1},
+			]),
 		})
-		this.pickOn = 'apply'
-		this.pickReqs = /** @satisfies {Array<PickRequirmentT>} */ ([
-			{target: 'player', type: ['item', 'effect'], amount: 1},
-		])
 	}
 
 	/**
 	 * @param {GameModel} game
 	 * @param {string} instance
-	 * @param {PickedSlotsInfo} pickedSlotsInfo
+	 * @param {CardPos} pos
+	 * @param {PickedSlots} pickedSlots
 	 */
-	onApply(game, instance, pickedSlotsInfo) {
-		const {singleUseInfo, currentPlayer} = game.ds
-		if (singleUseInfo?.id !== this.id) return
+	onApply(game, instance, pos, pickedSlots) {
+		const slots = pickedSlots[this.id] || []
+		const {player} = pos
 
-		const pickedSlots = pickedSlotsInfo[this.id] || []
-        if (pickedSlots.length !== 1) return
+		if (slots.length !== 1) return
 
-        const slot = pickedSlots[0]
-		if (slot.card !== null) return
+		const pickedCard = slots[0]
+		if (pickedCard.slot.card === null) return
 
-        discardCard(game, slot.card)
+		discardCard(game, pickedCard.slot.card)
+		discardSingleUse(game, player)
 
-        currentPlayer.custom[this.getInstanceKey(instance)] = true
-
-		discardSingleUse(game, currentPlayer)
+		player.custom[this.getInstanceKey(instance)] = true
 	}
-	
-    /**
+
+	/**
 	 * @param {GameModel} game
 	 * @param {CardPos} pos
 	 * @returns {"YES" | "NO" | "INVALID"}
 	 */
 	canAttach(game, pos) {
-		if (super.canAttach(game, pos) === 'NO') return 'INVALID'
-		const {currentPlayer} = game.ds
+		if (super.canAttach(game, pos) === 'INVALID') return 'INVALID'
+		const {player} = pos
 
-        for (const row of currentPlayer.board.rows) {
-           if (row.effectCard !== null || rowHasItem(row)) return 'YES'
-        }
+		for (const row of player.board.rows) {
+			if (
+				(row.effectCard !== null && isRemovable(row.effectCard)) ||
+				rowHasItem(row)
+			)
+				return 'YES'
+		}
 
-		return 'INVALID'
+		return 'NO'
 	}
 
 	/**
 	 * @param {GameModel} game
 	 * @param {string} instance
 	 */
-	onAttach(game, instance) {
-		const {currentPlayer} = game.ds
+	onAttach(game, instance, pos) {
+		const {player} = pos
 
-		currentPlayer.hooks.availableActions[instance] = (availableActions) => {
-			const singleUseCardUsed = currentPlayer.board.singleUseCardUsed
-			// Allow to play another single use card until another card single use card is used
-			// We have to do this because getAvailibleActions checks pastTurnActions and won't add it if it's already there
-			// We also have to check if PLAY_SINGLE_USE_CARD is already there because it's possible that another card added it
+		player.hooks.availableActions[instance] = (availableActions) => {
+			// We have to check if PLAY_SINGLE_USE_CARD is already there because it's possible that another card added it
 			// e.g. if you play a card that allows you to play another single use card like multiple Pistons back to back
-			if (currentPlayer.custom[this.getInstanceKey(instance)] && !singleUseCardUsed && !availableActions.includes('PLAY_SINGLE_USE_CARD')) {
+			if (!availableActions.includes('PLAY_SINGLE_USE_CARD')) {
 				availableActions.push('PLAY_SINGLE_USE_CARD')
-			} else if (currentPlayer.custom[this.getInstanceKey(instance)] && singleUseCardUsed) {
-				delete currentPlayer.hooks.availableActions[instance]
-				delete currentPlayer.custom[this.getInstanceKey(instance)]
 			}
 			return availableActions
 		}
 
+		player.hooks.onApply[instance] = (instance) => {
+			if (
+				player.custom[this.getInstanceKey(instance)] &&
+				player.board.singleUseCardUsed
+			) {
+				delete player.hooks.availableActions[instance]
+				delete player.custom[this.getInstanceKey(instance)]
+			}
+		}
+
 		// We remove on turnEnd instead of onDetach because we need to keep the hooks
 		// until the end of the turn in case the player plays another single use card
-		currentPlayer.hooks.turnEnd[instance] = () => {
-			delete currentPlayer.hooks.turnEnd[instance]
-			delete currentPlayer.hooks.availableActions[instance]
-			delete currentPlayer.custom[this.getInstanceKey(instance)]
+		player.hooks.turnEnd[instance] = () => {
+			delete player.hooks.turnEnd[instance]
+			delete player.hooks.availableActions[instance]
+			delete player.custom[this.getInstanceKey(instance)]
 		}
 	}
 }
