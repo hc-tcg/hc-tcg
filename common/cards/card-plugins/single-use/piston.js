4052e98883229901cf80bc81a62ab758e373eca7
Niko <nikod.uy@gmail.com>
2023-06-10 03:48:58 -0300

    Updated Piston

--- common/cards/card-plugins/single-use/piston.js
+++ common/cards/card-plugins/single-use/piston.js
@@ -1,10 +1,16 @@
 import SingleUseCard from './_single-use-card'
-import {discardCard, discardSingleUse, getAdjacentRows, rowHasEmptyItemSlot, rowHasItem} from '../../../../server/utils'
+import {
+	discardSingleUse,
+	getAdjacentRows,
+	rowHasEmptyItemSlot,
+	rowHasItem,
+} from '../../../../server/utils'
+import {GameModel} from '../../../../server/models/game-model'
 
 /**
  * @typedef {import('common/types/pick-process').PickRequirmentT} PickRequirmentT
- * @typedef {import('common/types/pick-process').PickedSlotsInfo} PickedSlotsInfo
- * @typedef {import('server/models/game-model').GameModel} GameModel
+ * @typedef {import('common/types/pick-process').PickedSlots} PickedSlots
+ * @typedef {import('common/types/cards').CardPos} CardPos
  */
 
 class PistonSingleUseCard extends SingleUseCard {
@@ -14,93 +20,110 @@ class PistonSingleUseCard extends SingleUseCard {
 			name: 'Piston',
 			rarity: 'common',
 			description:
-				"Move 1 of your attached item cards to an adjacent active or AFK Hermit. You can use another single use effect card this turn.",
+				'Move 1 of your attached item cards to an adjacent active or AFK Hermit. You can use another single use effect card this turn.',
+			pickOn: 'apply',
+			pickReqs: /** @satisfies {Array<PickRequirmentT>} */ ([
+				{target: 'player', type: ['item'], amount: 1},
+				{
+					target: 'player',
+					type: ['item'],
+					amount: 1,
+					empty: true,
+					adjacent: 'req',
+				},
+			]),
 		})
-		this.pickOn = 'apply'
-		this.pickReqs = /** @satisfies {Array<PickRequirmentT>} */ ([
-			{target: 'player', type: 'item', amount: 1},
-			{target: 'player', type: 'item', amount: 1, empty: true, adjacent: 'req'},
-		])
 	}
 
 	/**
 	 * @param {GameModel} game
 	 * @param {string} instance
-	 * @param {PickedSlotsInfo} pickedSlotsInfo
+	 * @param {CardPos} pos
+	 * @param {PickedSlots} pickedSlots
 	 */
-	onApply(game, instance, pickedSlotsInfo) {
-		const {singleUseInfo, currentPlayer} = game.ds
-		if (singleUseInfo?.id !== this.id) return
+	onApply(game, instance, pos, pickedSlots) {
+		const {player} = pos
+		const slots = pickedSlots[this.id] || []
 
-		const pickedSlots = pickedSlotsInfo[this.id] || []
+		if (slots.length !== 2) return
 
-        if (pickedSlots.length !== 2) return
+		const itemCardInfo = slots[0]
+		const targetSlotInfo = slots[1]
 
-        const itemCardInfo = pickedSlots[0]
-        const targetSlotInfo = pickedSlots[1]
+		if (
+			targetSlotInfo.slot.card !== null ||
+			!itemCardInfo.row ||
+			!targetSlotInfo.row
+		)
+			return
 
-		if (targetSlotInfo.card !== null) return
+		itemCardInfo.row.state.itemCards[itemCardInfo.slot.index] =
+			targetSlotInfo.slot.card
+		targetSlotInfo.row.state.itemCards[targetSlotInfo.slot.index] =
+			itemCardInfo.slot.card
 
-        // Remove item from source
-		itemCardInfo.row.itemCards[itemCardInfo.slotIndex] = null
+		player.custom[this.getInstanceKey(instance)] = true
 
-		// Add item to target
-		targetSlotInfo.row.itemCards[targetSlotInfo.slotIndex] =
-			itemCardInfo.card
-
-        currentPlayer.custom[this.getInstanceKey(instance)] = true
-
-		discardSingleUse(game, currentPlayer)
+		discardSingleUse(game, player)
 	}
-	
-    /**
+
+	/**
 	 * @param {GameModel} game
 	 * @param {CardPos} pos
 	 */
 	canAttach(game, pos) {
-		if (super.canAttach(game, pos) === 'NO') return 'INVALID'
-		const {currentPlayer} = game.ds
+		if (super.canAttach(game, pos) === 'INVALID') return 'INVALID'
 
-        const adjacents = getAdjacentRows(currentPlayer)
-        let validPairs = 0
-        for (const rows of adjacents) {
-            if (rowHasEmptyItemSlot(rows[0]) && rowHasEmptyItemSlot(rows[1]) && (rowHasItem(rows[0]) || rowHasItem(rows[1]))) validPairs++
-        }
-        if (validPairs === 0) return 'INVALID'
+		const adjacents = getAdjacentRows(pos.player)
+		let validPairs = 0
+		for (const rows of adjacents) {
+			if (
+				rowHasEmptyItemSlot(rows[0]) &&
+				rowHasEmptyItemSlot(rows[1]) &&
+				(rowHasItem(rows[0]) || rowHasItem(rows[1]))
+			)
+				validPairs++
+		}
+		if (validPairs === 0) return 'NO'
 
 		return 'YES'
 	}
 
-    /**
+	/**
 	 * @param {GameModel} game
 	 * @param {string} instance
+	 * @param {CardPos} pos
 	 */
-	onAttach(game, instance) {
-        const {currentPlayer} = game.ds
+	onAttach(game, instance, pos) {
+		const {player} = pos
 
-        currentPlayer.hooks.availableActions[instance] = (availableActions) => {
-			const singleUseCardUsed = currentPlayer.board.singleUseCardUsed
-			// Allow to play another single use card until another card single use card is used
-			// We have to do this because getAvailibleActions checks pastTurnActions and won't add it if it's already there
-			// We also have to check if PLAY_SINGLE_USE_CARD is already there because it's possible that another card added it
+		player.hooks.availableActions[instance] = (availableActions) => {
+			// We have to check if PLAY_SINGLE_USE_CARD is already there because it's possible that another card added it
 			// e.g. if you play a card that allows you to play another single use card like multiple Pistons back to back
-            if (currentPlayer.custom[this.getInstanceKey(instance)] && !singleUseCardUsed && !availableActions.includes('PLAY_SINGLE_USE_CARD')) {
-    			availableActions.push('PLAY_SINGLE_USE_CARD')
-            } else if (currentPlayer.custom[this.getInstanceKey(instance)] && singleUseCardUsed) {
-				delete currentPlayer.hooks.availableActions[instance]
-				delete currentPlayer.custom[this.getInstanceKey(instance)]
+			if (!availableActions.includes('PLAY_SINGLE_USE_CARD')) {
+				availableActions.push('PLAY_SINGLE_USE_CARD')
 			}
-            return availableActions
-        }
+			return availableActions
+		}
+
+		player.hooks.onApply[instance] = (instance) => {
+			if (
+				player.custom[this.getInstanceKey(instance)] &&
+				player.board.singleUseCardUsed
+			) {
+				delete player.hooks.availableActions[instance]
+				delete player.custom[this.getInstanceKey(instance)]
+			}
+		}
 
 		// We remove on turnEnd instead of onDetach because we need to keep the hooks
 		// until the end of the turn in case the player plays another single use card
-		currentPlayer.hooks.turnEnd[instance] = () => {
-			delete currentPlayer.hooks.turnEnd[instance]
-			delete currentPlayer.hooks.availableActions[instance]
-			delete currentPlayer.custom[this.getInstanceKey(instance)]
+		player.hooks.turnEnd[instance] = () => {
+			delete player.hooks.turnEnd[instance]
+			delete player.hooks.availableActions[instance]
+			delete player.custom[this.getInstanceKey(instance)]
 		}
-    }
+	}
 }
 
 export default PistonSingleUseCard
