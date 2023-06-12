e4cf531bd16378f0801be9354914b21bb2baeeab
Joseph Lyman <jmlyman424@gmail.com>
2023-06-12 14:55:55 -0400

    new chat ui

--- client/src/app/game/chat/chat.tsx
+++ client/src/app/game/chat/chat.tsx
@@ -1,12 +1,14 @@
-import {SyntheticEvent} from 'react'
+import {SyntheticEvent, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import classnames from 'classnames'
-import {getChatMessages, getPlayerStates} from 'logic/game/game-selectors'
+import {getChatMessages, getOpponentName, getPlayerStates} from 'logic/game/game-selectors'
 import {chatMessage} from 'logic/game/game-actions'
 import {getPlayerId} from 'logic/session/session-selectors'
 import {getSettings} from 'logic/local-settings/local-settings-selectors'
-import css from './chat.module.css'
+import css from './chat.module.scss'
 import Button from 'components/button'
+import {setSetting} from 'logic/local-settings/local-settings-actions'
+import {useDrag} from '@use-gesture/react'
 
 function Chat() {
 	const dispatch = useDispatch()
@@ -14,6 +16,17 @@ function Chat() {
 	const playerStates = useSelector(getPlayerStates)
 	const playerId = useSelector(getPlayerId)
 	const settings = useSelector(getSettings)
+	const opponent = useSelector(getOpponentName)
+	const [chatHeight, setChatHeight] = useState<string | undefined>()
+	const [chatPos, setChatPos] = useState({x: 0, y: 0})
+	const bindChatPos = useDrag((params: any) =>
+		setChatPos({
+			x: params.offset[0],
+			y: params.offset[1],
+		})
+	)
+
+	const isAndroid = /(android)/i.test(navigator.userAgent)
 
 	if (settings.showChat !== 'on') return null
 
@@ -28,53 +41,71 @@ function Chat() {
 		dispatch(chatMessage(message))
 	}
 
-	console.log('Chat messages:', chatMessages)
+	const toggleChat = () => {
+		dispatch(setSetting('showChat', 'off'))
+	}
+
+	const handleChatHeight = (toggle: boolean) => {
+		if (isAndroid && toggle) {
+			setChatHeight('55vh')
+		}
+		if (!toggle) {
+			setChatHeight(undefined)
+		}
+	}
 
 	return (
-		<div className={css.chat}>
-			<div className={css.messageList}>
-				{chatMessages
-					.slice()
-					.reverse()
-					.map((messageInfo) => {
-						const time = new Date(messageInfo.createdAt).toLocaleString()
-						const hmTime = new Date(messageInfo.createdAt).toLocaleTimeString([], {
-							hour: '2-digit',
-							minute: '2-digit',
-						})
-						const name = playerStates?.[messageInfo.playerId]?.playerName || 'unknown'
-						const isPlayer = playerId === messageInfo.playerId
-						return (
-							<div
-								key={messageInfo.createdAt}
-								className={classnames(css.message, {
-									[css.player]: isPlayer,
-									[css.opponent]: !isPlayer,
-								})}
-								title={time}
-							>
-								<span className={css.time}>{hmTime}</span>
-								<span className={css.playerName}>{name}</span>
-								<span className={css.text}>
-									{/* :&nbsp; */}
-									{settings.profanityFilter !== 'off'
-										? messageInfo.censoredMessage
-										: messageInfo.message}
-								</span>
-							</div>
-						)
-					})}
+		<div className={css.chat} style={{height: chatHeight, top: chatPos.y, left: chatPos.x}}>
+			<div className={css.header} {...bindChatPos()}>
+				<p>Chatting with {opponent}</p>
+				<button onClick={toggleChat}>
+					<img src="/images/CloseX.svg" alt="close" />
+				</button>
+			</div>
+
+			<div className={css.messagesWrapper}>
+				<div className={css.messages}>
+					{chatMessages
+						.slice()
+						.reverse()
+						.map((msg) => {
+							const time = new Date(msg.createdAt).toLocaleString()
+							const hmTime = new Date(msg.createdAt).toLocaleTimeString([], {
+								hour: '2-digit',
+								minute: '2-digit',
+							})
+							const name = playerStates?.[msg.playerId]?.playerName || 'unknown'
+							const isPlayer = playerId === msg.playerId
+							return (
+								<p
+									key={msg.createdAt}
+									className={classnames(css.message, {
+										[css.player]: isPlayer,
+										[css.opponent]: !isPlayer,
+									})}
+									title={time}
+								>
+									<span className={css.time}>{hmTime}</span>
+									<span className={css.playerName}>{name}</span>
+									<span className={css.text}>
+										{settings.profanityFilter !== 'off' ? msg.censoredMessage : msg.message}
+									</span>
+								</p>
+							)
+						})}
+				</div>
 			</div>
 
 			<form className={css.publisher} onSubmit={handleNewMessage}>
 				<input
-					style={{height: '2rem'}}
 					autoComplete="off"
 					autoFocus
+					onFocus={() => handleChatHeight(true)}
+					onBlur={() => handleChatHeight(false)}
 					name="message"
 					maxLength={140}
 				/>
-				<Button variant="default" size="small" style={{height: '2rem'}}>
+				<Button variant="default" size="small">
 					Send
 				</Button>
 			</form>
