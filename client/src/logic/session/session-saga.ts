3108fb28bd5bc64a1da2a0c09501c67df38be200
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2023-06-12 19:14:48 +0100

    Prettify session-sage.ts

--- client/src/logic/session/session-saga.ts
+++ client/src/logic/session/session-saga.ts
@@ -12,8 +12,8 @@ import {
 	saveDeck,
 	setActiveDeck,
 } from 'logic/saved-decks/saved-decks'
+import {validateDeck} from 'server/utils/validation'
 import {PlayerDeckT} from '../../../../common/types/deck'
-import {validateDeck} from 'common/utils/validation'
 
 type PlayerInfoT = {
 	playerName: string
