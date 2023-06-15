074d1f5e70d21e017010453005bcab8df8465ad6
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2023-06-16 00:11:24 +0100

    Golden apple

--- common/cards/card-plugins/single-use/golden-apple.js
+++ common/cards/card-plugins/single-use/golden-apple.js
@@ -1,6 +1,8 @@
 import SingleUseCard from './_single-use-card'
 import {validPick} from '../../../../server/utils/reqs'
 import {GameModel} from '../../../../server/models/game-model'
+import HERMIT_CARDS from '../hermits'
+import { getNonEmptyRows } from 'server/utils'
 
 /**
  * @typedef {import('common/types/pick-process').PickRequirmentT} PickRequirmentT
@@ -12,38 +14,44 @@ class GoldenAppleSingleUseCard extends SingleUseCard {
 			id: 'golden_apple',
 			name: 'Golden Apple',
 			rarity: 'ultra_rare',
-			description:
-				'Heals +100hp.\n\nCan be used on active or AFK Hermits.\n\nDiscard after use.',
+			description: 'Heal AFK Hermit 100hp.',
+			pickOn: 'apply',
+			pickReqs: [
+				{target: 'player', type: ['hermit'], amount: 1, active: false},
+			],
 		})
-		this.heal = 100
-		this.pickOn = 'apply'
-		this.pickReqs = /** @satisfies {Array<PickRequirmentT>} */ ([
-			{target: 'player', type: ['hermit'], amount: 1},
-		])
 	}
 
 	/**
 	 * @param {GameModel} game
+	 * @param {string} instance
+	 * @param {import('../../../types/cards').CardPos} pos
+	 * @param {import('../../../../server/utils/picked-cards').PickedSlots} pickedSlots
 	 */
-	register(game) {
-		game.hooks.applyEffect.tap(this.id, (action, actionState) => {
-			const {singleUseInfo} = game.ds
-			const {pickedSlots} = actionState
-			if (singleUseInfo?.id === this.id) {
-				const suPickedCards = pickedSlots[this.id] || []
-				if (suPickedCards?.length !== 1) return 'INVALID'
-				if (!validPick(game.state, this.pickReqs[0], suPickedCards[0]))
-					return 'INVALID'
-				const {row, cardInfo} = suPickedCards[0]
-				if (row.health === null) return 'INVALID'
-				row.health = Math.min(
-					row.health + this.heal,
-					cardInfo.health // max health
-				)
+	onApply(game, instance, pos, pickedSlots) {
+		const pickedCards = pickedSlots[this.id] || []
+		if (pickedCards.length !== 1) return
 
-				return 'DONE'
-			}
-		})
+		const targetSlot = pickedCards[0]
+		if (!targetSlot.row?.state.health) return
+		const rowState = targetSlot.row.state
+		rowState.health = Math.min(
+			rowState.health + 100,
+			HERMIT_CARDS[rowState.hermitCard.cardId].health
+		)
+	}
+
+	/**
+	 * @param {GameModel} game
+	 * @param {import('../../../types/cards').CardPos} pos
+	 */
+	canAttach(game, pos) {
+		if (super.canAttach(game, pos) === 'INVALID') return 'INVALID'
+
+		const inactiveRows = getNonEmptyRows(pos.player, false)
+		if (inactiveRows.length === 0) return 'NO'
+
+		return 'YES'
 	}
 }
 
