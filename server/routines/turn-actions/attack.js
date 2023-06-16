113bbd6215ceeb70d2176b63e8635149edeeaa15
Sense101 <personal.sense101@gmail.com>
2023-06-16 12:19:31 +0100

    New ailments, and poison doesn't kill

--- server/routines/turn-actions/attack.js
+++ server/routines/turn-actions/attack.js
@@ -101,6 +101,87 @@ function executeAttack(game, attack) {
 }
 
 /**
+ *
+ * @param {PlayerState} player
+ * @param {Array<AttackModel>} attacks
+ * @param {import('types/pick-process').PickedSlots} pickedSlots
+ */
+function runBeforeAttacks(player, attacks, pickedSlots = {}) {
+	const beforeAttackKeys = Object.keys(player.hooks.beforeAttack)
+	const beforeAttacks = Object.values(player.hooks.beforeAttack)
+	for (let attackIndex = 0; attackIndex < attacks.length; attackIndex++) {
+		const attack = attacks[attackIndex]
+
+		for (let i = 0; i < beforeAttackKeys.length; i++) {
+			const instance = beforeAttackKeys[i]
+			// if we are not ignoring this hook, call it
+			if (!shouldIgnoreCard(attack, instance)) {
+				beforeAttacks[i](attack, pickedSlots)
+			}
+		}
+	}
+}
+
+/**
+ *
+ * @param {PlayerState} player
+ * @param {Array<AttackModel>} attacks
+ * @param {import('types/pick-process').PickedSlots} pickedSlots
+ */
+function runOnAttacks(player, attacks, pickedSlots = {}) {
+	const onAttackKeys = Object.keys(player.hooks.onAttack)
+	const onAttacks = Object.values(player.hooks.onAttack)
+	for (let attackIndex = 0; attackIndex < attacks.length; attackIndex++) {
+		const attack = attacks[attackIndex]
+
+		for (let i = 0; i < onAttackKeys.length; i++) {
+			const instance = onAttackKeys[i]
+			// if we are not ignoring this hook, call it
+			if (!shouldIgnoreCard(attack, instance)) {
+				onAttacks[i](attack, pickedSlots)
+			}
+		}
+	}
+}
+
+/**
+ *
+ * @param {GameModel} game
+ * @param {Array<AttackModel>} attacks
+ * @returns {Array<AttackResult>}
+ */
+function executeAttacks(game, attacks) {
+	/** @type {Array<AttackResult>} */
+	const results = []
+	for (let attackIndex = 0; attackIndex < attacks.length; attackIndex++) {
+		const result = executeAttack(game, attacks[attackIndex])
+		results.push(result)
+	}
+	return results
+}
+
+/**
+ *
+ * @param {PlayerState} player
+ * @param {Array<AttackResult>} results
+ */
+function runAfterAttacks(player, results) {
+	const afterAttackKeys = Object.keys(player.hooks.afterAttack)
+	const afterAttacks = Object.values(player.hooks.afterAttack)
+	for (let resultsIndex = 0; resultsIndex < results.length; resultsIndex++) {
+		const result = results[resultsIndex]
+
+		for (let i = 0; i < afterAttackKeys.length; i++) {
+			const instance = afterAttackKeys[i]
+			// if we are not ignoring this hook, call it
+			if (!shouldIgnoreCard(result.attack, instance)) {
+				afterAttacks[i](result)
+			}
+		}
+	}
+}
+
+/**
  * Returns if we should ignore the hooks for an instance or not
  * @param {AttackModel} attack
  * @param {string} instance
@@ -159,6 +240,7 @@ function* attackSaga(game, turnAction, actionState) {
 	console.log('We got', attacks.length, 'attacks')
 
 	// Store all results
+	/** @type {Array<AttackResult>} */
 	const results = []
 
 	// Main attack loop
@@ -166,41 +248,13 @@ function* attackSaga(game, turnAction, actionState) {
 		// Process all current attacks one at a time
 
 		// STEP 1 - Call before attack for all attacks
-		const beforeAttackKeys = Object.keys(currentPlayer.hooks.beforeAttack)
-		const beforeAttacks = Object.values(currentPlayer.hooks.beforeAttack)
-		for (let attackIndex = 0; attackIndex < attacks.length; attackIndex++) {
-			const attack = attacks[attackIndex]
-
-			for (let i = 0; i < beforeAttackKeys.length; i++) {
-				const instance = beforeAttackKeys[i]
-				// if we are not ignoring this hook, call it
-				if (!shouldIgnoreCard(attack, instance)) {
-					beforeAttacks[i](attack, pickedSlots)
-				}
-			}
-		}
+		runBeforeAttacks(currentPlayer, attacks, pickedSlots)
 
 		// STEP 2 - Call on attack for all attacks
-		const onAttackKeys = Object.keys(currentPlayer.hooks.onAttack)
-		const onAttacks = Object.values(currentPlayer.hooks.onAttack)
-		for (let attackIndex = 0; attackIndex < attacks.length; attackIndex++) {
-			const attack = attacks[attackIndex]
-
-			for (let i = 0; i < onAttackKeys.length; i++) {
-				const instance = onAttackKeys[i]
-				// if we are not ignoring this hook, call it
-				if (!shouldIgnoreCard(attack, instance)) {
-					onAttacks[i](attack, pickedSlots)
-				}
-			}
-		}
+		runOnAttacks(currentPlayer, attacks, pickedSlots)
 
 		// STEP 3 - Execute all attacks, and store the results
-		/** @type {Array<AttackResult>} */
-		for (let attackIndex = 0; attackIndex < attacks.length; attackIndex++) {
-			const result = executeAttack(game, attacks[attackIndex])
-			results.push(result)
-		}
+		results.push(...executeAttacks(game, attacks))
 
 		// STEP 4 - Get all the next attacks, and repeat the process
 		const newAttacks = []
@@ -211,21 +265,73 @@ function* attackSaga(game, turnAction, actionState) {
 	}
 
 	// STEP 5 - Finally, call afterAttack for all results
-	const afterAttackKeys = Object.keys(currentPlayer.hooks.afterAttack)
-	const afterAttacks = Object.values(currentPlayer.hooks.afterAttack)
-	for (let resultsIndex = 0; resultsIndex < results.length; resultsIndex++) {
-		const result = results[resultsIndex]
+	runAfterAttacks(currentPlayer, results)
 
-		for (let i = 0; i < afterAttackKeys.length; i++) {
-			const instance = afterAttackKeys[i]
-			// if we are not ignoring this hook, call it
-			if (!shouldIgnoreCard(result.attack, instance)) {
-				afterAttacks[i](result)
-			}
+	return 'DONE'
+}
+
+/**
+ * @param {GameModel} game
+ * @param {PlayerState} player
+ */
+export function runAilmentAttacks(game, player) {
+	/** @type {Array<AttackModel>} */
+	let attacks = []
+
+	// Get ailment attacks
+	for (let i = 0; i < player.board.rows.length; i++) {
+		const row = player.board.rows[i]
+		if (!row.health) continue
+
+		const attack = new AttackModel({
+			target: {
+				index: i,
+				row,
+			},
+			type: 'ailment',
+		})
+
+		if (row.ailments.find((a) => a.id === 'fire')) {
+			attack.id = 'fire'
+			attacks.push(attack.addDamage(20))
+		}
+
+		if (row.ailments.find((a) => a.id === 'poison')) {
+			attack.id = 'poison'
+
+			// Calculate max poison damage
+			const poisonDamage = Math.max(Math.min(row.health - 10, 20), 0)
+			attacks.push(attack.addDamage(poisonDamage))
 		}
 	}
 
-	return 'DONE'
+	// Run the code for the attacks
+
+	// Store all results
+	/** @type {Array<AttackResult>} */
+	const results = []
+
+	// Main attack loop
+	while (attacks.length > 0) {
+		// STEP 1 - Call before attack for all attacks
+		runBeforeAttacks(player, attacks)
+
+		// STEP 2 - Call on attack for all attacks
+		runOnAttacks(player, attacks)
+
+		// STEP 3 - Execute all attacks, and store the results
+		results.push(...executeAttacks(game, attacks))
+
+		// STEP 4 - Get all the next attacks, and repeat the process
+		const newAttacks = []
+		for (let attackIndex = 0; attackIndex < attacks.length; attackIndex++) {
+			newAttacks.push(...attacks[attackIndex].nextAttacks)
+		}
+		attacks = newAttacks
+	}
+
+	// STEP 5 - Finally, call afterAttack for all results
+	runAfterAttacks(player, results)
 }
 
 export default attackSaga
