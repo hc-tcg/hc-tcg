f559b327319f98259796c85192ab5ee386ec4e35
Sense101 <personal.sense101@gmail.com>
2023-06-20 20:40:37 +0100

    Import RowStateWithHermit correctly so we can build

--- common/types/cards.ts
+++ common/types/cards.ts
@@ -1,4 +1,4 @@
-import {PlayerState, RowState} from './game-state'
+import {PlayerState, RowState, RowStateWithHermit} from './game-state'
 import {PickRequirmentT} from './pick-process'
 
 export type CardRarityT = 'common' | 'rare' | 'ultra_rare'
