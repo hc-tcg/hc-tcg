f236a5adb6f2aaa3094bc51800e9f672c7c518c4
Alice <63879236+Benji42@users.noreply.github.com>
2023-06-25 10:10:27 -0400

    Fix type advantage issues (#318)
    * Fix type advantage issues
    
    * requested changes

--- common/cards/card-plugins/single-use/target-block.js
+++ common/cards/card-plugins/single-use/target-block.js
@@ -1,5 +1,6 @@
 import {getNonEmptyRows} from '../../../../server/utils'
 import {GameModel} from '../../../../server/models/game-model'
+import {createWeaknessAttack} from '../../../../server/utils/attacks'
 import SingleUseCard from './_single-use-card'
 
 /**
@@ -29,6 +30,10 @@ class TargetBlockSingleUseCard extends SingleUseCard {
 	 */
 	onAttach(game, instance, pos) {
 		const {player} = pos
+		const ignoreThisWeakness = this.getInstanceKey(
+			instance,
+			'ignoreThisWeakness'
+		)
 
 		player.hooks.onApply[instance] = (pickedSlots, modalResult) => {
 			const pickedSlot = pickedSlots[this.id]?.[0]
@@ -37,9 +42,22 @@ class TargetBlockSingleUseCard extends SingleUseCard {
 			player.hooks.beforeAttack[instance] = (attack) => {
 				if (['backlash', 'ailment'].includes(attack.type)) return
 				if (!pickedSlot.row || !pickedSlot.row.state.hermitCard) return
+
 				attack.target.rowIndex = pickedSlot.row.index
 				attack.target.row = pickedSlot.row.state
-				delete player.hooks.beforeAttack[instance]
+
+				if (['primary', 'secondary'].includes(attack.type)) {
+					const weaknessAttack = createWeaknessAttack(attack)
+					if (weaknessAttack) {
+						attack.addNewAttack(weaknessAttack)
+						player.custom[ignoreThisWeakness] = true
+					}
+				} else if (attack.type === 'weakness') {
+					if (!player.custom[ignoreThisWeakness]) {
+						attack.multiplyDamage(0).lockDamage()
+					}
+					delete player.custom[ignoreThisWeakness]
+				}
 			}
 		}
 	}
@@ -65,7 +83,13 @@ class TargetBlockSingleUseCard extends SingleUseCard {
 	 */
 	onDetach(game, instance, pos) {
 		const {player} = pos
+		const ignoreThisWeakness = this.getInstanceKey(
+			instance,
+			'ignoreThisWeakness'
+		)
 		delete player.hooks.onApply[instance]
+		delete player.hooks.beforeAttack[instance]
+		delete player.custom[ignoreThisWeakness]
 	}
 
 	getExpansion() {
