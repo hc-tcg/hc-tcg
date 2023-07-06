f4860bba9ea0f56f3dbcccd75417c93e8fc115d6
Niko <nikod.uy@gmail.com>
2023-07-06 03:17:04 -0300

    Removed the ability to attach to the effect slot

--- common/cards/card-plugins/effects/armor-stand.js
+++ common/cards/card-plugins/effects/armor-stand.js
@@ -1,5 +1,4 @@
 import {GameModel} from '../../../../server/models/game-model'
-import {HERMIT_CARDS} from '../../../cards'
 import {discardCard} from '../../../../server/utils'
 import EffectCard from '../effects/_effect-card'
 import {isTargetingPos} from '../../../../server/utils/attacks'
@@ -25,67 +24,31 @@ class ArmorStandEffectCard extends EffectCard {
 	 * @param {CardPos} pos
 	 */
 	onAttach(game, instance, pos) {
-		const {player, opponentPlayer, slot, row} = pos
+		const {player, opponentPlayer, row} = pos
 		if (!row) return
 
-		if (slot.type === 'hermit') {
-			row.health = 50
-			if (player.board.activeRow === null) {
-				player.board.activeRow = pos.rowIndex
+		row.health = 50
+		if (player.board.activeRow === null) {
+			player.board.activeRow = pos.rowIndex
+		}
+
+		// The menu won't show up but just in case someone tries to cheat
+		player.hooks.blockedActions[instance] = (blockedActions, pastTurnActions, availableEnergy) => {
+			if (player.board.activeRow === pos.rowIndex) {
+				blockedActions.push('PRIMARY_ATTACK')
+				blockedActions.push('SECONDARY_ATTACK')
+				blockedActions.push('ZERO_ATTACK')
 			}
 
-			// The menu won't show up but just in case someone tries to cheat
-			player.hooks.blockedActions[instance] = (
-				blockedActions,
-				pastTurnActions,
-				availableEnergy
-			) => {
-				if (player.board.activeRow === pos.rowIndex) {
-					blockedActions.push('PRIMARY_ATTACK')
-					blockedActions.push('SECONDARY_ATTACK')
-					blockedActions.push('ZERO_ATTACK')
-				}
+			return blockedActions
+		}
 
-				return blockedActions
-			}
-
-			opponentPlayer.hooks.afterAttack[instance] = (attack) => {
-				if (attack.calculateDamage() >= 50 && attack.attacker && isTargetingPos(attack, pos)) {
-					// Discard to prevent losing a life
-					discardCard(game, row.hermitCard)
-					// Reset the active row so the player can switch
-					player.board.activeRow = null
-				}
-			}
-		} else {
-			const instanceKey = this.getInstanceKey(instance)
-
-			opponentPlayer.hooks.onAttack[instance] = (attack, pickedSlots) => {
-				if (!isTargetingPos(attack, pos) || attack.isType('ailment')) return
-
-				if (player.custom[instanceKey] === undefined) {
-					player.custom[instanceKey] = 0
-				}
-
-				const totalReduction = player.custom[instanceKey]
-
-				if (totalReduction < 50) {
-					const damageReduction = Math.min(attack.getDamage(), 60 - totalReduction)
-					player.custom[instanceKey] += damageReduction
-					attack.reduceDamage(this.id, damageReduction)
-				}
-			}
-
-			opponentPlayer.hooks.afterAttack[instance] = (attackResult) => {
-				const {player, row} = pos
-
-				if (player.custom[instanceKey] > 0 && row) {
-					discardCard(game, row.effectCard)
-				}
-			}
-
-			opponentPlayer.hooks.onTurnEnd[instance] = () => {
-				delete player.custom[instanceKey]
+		opponentPlayer.hooks.afterAttack[instance] = (attack) => {
+			if (!row.health && attack.attacker && isTargetingPos(attack, pos)) {
+				// Discard to prevent losing a life
+				discardCard(game, row.hermitCard)
+				// Reset the active row so the player can switch
+				player.board.activeRow = null
 			}
 		}
 	}
@@ -115,18 +78,11 @@ class ArmorStandEffectCard extends EffectCard {
 	 * @param {CardPos} pos
 	 */
 	canAttach(game, pos) {
-		const {player, slot, row} = pos
+		const {slot} = pos
 		const {currentPlayer} = game.ds
 
-		if (!['hermit', 'effect'].includes(slot.type)) return 'INVALID'
+		if (slot.type !== 'hermit') return 'INVALID'
 		if (pos.player.id !== currentPlayer.id) return 'INVALID'
-
-		if (slot.type === 'effect') {
-			if (!row || !row.hermitCard) return 'INVALID'
-			const hermitInfo = HERMIT_CARDS[row.hermitCard?.cardId]
-			if (!hermitInfo) return 'INVALID'
-			if (!hermitInfo.canAttachToCard(game, pos)) return 'NO'
-		}
 
 		return 'YES'
 	}
