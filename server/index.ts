97899de771026b730ea093efb8c684dce2e1fa74
Sense101 <personal.sense101@gmail.com>
2023-07-17 12:46:15 +0100

    Also server correct static assets

--- server/index.ts
+++ server/index.ts
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use(
-	express.static(path.join(__dirname, '..', CONFIG.clientPath), {
+	express.static(path.join(__dirname, '../../..', CONFIG.clientPath), {
 		maxAge: 1000 * 60 * 60,
 	})
 )
