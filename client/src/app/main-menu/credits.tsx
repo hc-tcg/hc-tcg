0878f89bd9ebbe36729e3d26971d0fa0644da48d
Joseph Lyman <jmlyman424@gmail.com>
2023-08-17 14:22:33 -0400

    added developer credits

--- client/src/app/main-menu/credits.tsx
+++ client/src/app/main-menu/credits.tsx
@@ -31,14 +31,62 @@ function Credits({setMenuSection}: Props) {
 				/>
 			</div>
 			<h2>Website Developers</h2>
-			{/* <div className={css.creditsContainer}>
+			<div className={css.creditsContainer}>
 				<Credit
 					name="Benji"
 					handle="alicetouhou"
 					social="discord"
-					avatar="https://pbs.twimg.com/profile_images/1382001684151332867/iYD2Xj7c_400x400.jpg"
+					avatar="https://avatars.githubusercontent.com/u/63879236?v=4"
 				/>
-			</div> */}
+				<Credit
+					name="ChimeraDev"
+					handle="ChimeraDev"
+					social="discord"
+					avatar="https://avatars.githubusercontent.com/u/109681545?v=4"
+				/>
+				<Credit
+					name="Harou"
+					handle="Harou"
+					social="discord"
+					avatar="https://cdn.discordapp.com/avatars/171689337954500608/c17287ea15fbbbf66f8bfcbcdf6bd705.webp"
+				/>
+				<Credit
+					name="ImagineFyre"
+					handle="ImagineFyre"
+					social="discord"
+					avatar="https://cdn.discordapp.com/avatars/395760322864218113/89d3e188881c3aacd62a91c8f6b0b2f5.webp"
+				/>
+				<Credit
+					name="Niko"
+					handle="niko.uy"
+					social="discord"
+					avatar="https://avatars.githubusercontent.com/u/12455733?v=4"
+				/>
+				<Credit
+					name="ProfNinja"
+					handle="ProfNinja"
+					social="discord"
+					avatar="https://avatars.githubusercontent.com/u/671639?v=4"
+				/>
+				<Credit
+					name="Scopop"
+					handle="Scopop"
+					social="discord"
+					avatar="https://cdn.discordapp.com/avatars/625061304939446273/6ec5d4462e4d4a0e4833ed908dbd9c2f.webp"
+				/>
+				<Credit
+					name="Sense"
+					handle="Sense101"
+					social="discord"
+					avatar="https://avatars.githubusercontent.com/u/67970865?v=4"
+				/>
+				<Credit
+					name="Tyrannicodin"
+					handle="Tyrannicodin"
+					social="discord"
+					avatar="https://cdn.discordapp.com/avatars/547104418131083285/0e6fa62e2f647943f21ecbe2d21a9291.webp"
+				/>
+			</div>
 		</MenuLayout>
 	)
 }
--- client/src/components/credit/credit.tsx
+++ client/src/components/credit/credit.tsx
@@ -1,6 +1,5 @@
 import css from './credit.module.scss'
 import classnames from 'classnames'
-import {Fragment} from 'react'
 
 interface Props {
 	name: string
@@ -10,27 +9,26 @@ interface Props {
 	avatar: string
 }
 
-const Credits = (inputProps: Props) => {
-	const props = {...inputProps}
-	const hasLink = props.link !== undefined
+const Credits = ({name, social, handle, link, avatar}: Props) => {
+	const hasLink = link !== undefined
 	const content = (
-		<Fragment>
-			<img src={props.avatar} className={css.avatar} />
+		<>
+			<img src={avatar} className={css.avatar} />
 			<div>
-				<div>{props.name}</div>
+				<div>{name}</div>
 				<div className={css.handle}>
 					<img
-						src={`images/icons/${props.social}.svg`}
+						src={`images/icons/${social}.svg`}
 						className={classnames(css.handle, css.handleImage)}
 					/>
-					{props.handle}
+					{handle}
 				</div>
 			</div>
-		</Fragment>
+		</>
 	)
 
 	return hasLink ? (
-		<a className={css.credit} href={props.link} target="_blank" rel="noreferrer">
+		<a className={css.credit} href={link} target="_blank" rel="noreferrer">
 			{content}
 		</a>
 	) : (
