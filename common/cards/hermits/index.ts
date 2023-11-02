be3f89c28a51dd5495313b9c3985412a725bfade
Alice <63879236+Benji42@users.noreply.github.com>
2023-11-01 22:10:25 -0400

    Katherineeliz card

--- common/cards/hermits/index.ts
+++ common/cards/hermits/index.ts
@@ -76,6 +76,7 @@ import HermitCard from '../base/hermit-card'
 import SolidaritygamingRareHermitCard from './solidaritygaming-rare'
 import Smajor1995RareHermitCard from './smajor1995'
 import Biffa2001RareHermitCard from './biffa2001-rare'
+import KatherineelizRareHermitCard from './katherineeliz-rare'
 
 export const HERMIT_CARD_CLASSES: Array<HermitCard> = [
 	new BdoubleO100CommonHermitCard(),
@@ -155,7 +156,8 @@ export const HERMIT_CARD_CLASSES: Array<HermitCard> = [
 	new SolidaritygamingRareHermitCard(),
 	new MonkeyfarmRareHermitCard(),
 	new Smajor1995RareHermitCard(),
-	new Biffa2001RareHermitCard()
+	new Biffa2001RareHermitCard(),
+	new KatherineelizRareHermitCard(),
 ]
 
 export default HERMIT_CARD_CLASSES
