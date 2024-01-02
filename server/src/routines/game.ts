8c5fad7ca221164e96e8f3818496f6b23f654051
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2024-01-02 17:30:57 +0000

    Lint

--- server/src/routines/game.ts
+++ server/src/routines/game.ts
@@ -217,7 +217,11 @@ function* checkHermitHealth(game: GameModel) {
 			playerState.lives >= 3 &&
 			game.state.turn.turnNumber <= game.getPlayerIds().findIndex((id) => id === playerState.id) + 1
 
-		const noHermitsLeft = !firstPlayerTurn && playerState.board.rows.every((row) => !row.hermitCard || CARDS[row.hermitCard.cardId].type !== 'hermit')
+		const noHermitsLeft =
+			!firstPlayerTurn &&
+			playerState.board.rows.every(
+				(row) => !row.hermitCard || CARDS[row.hermitCard.cardId].type !== 'hermit'
+			)
 		if (isDead || noHermitsLeft) {
 			deadPlayerIds.push(playerState.id)
 		}
