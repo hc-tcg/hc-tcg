8a6f07bd6af6b2943860c2f741f887cdd0250c28
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2024-01-19 20:20:29 +0000

    Add mass import and export from files

--- client/src/app/main-menu/main-menu.tsx
+++ client/src/app/main-menu/main-menu.tsx
@@ -7,6 +7,9 @@ import TcgLogo from 'components/tcg-logo'
 import Beef from 'components/beef'
 import Button from 'components/button'
 import {VersionLinks} from 'components/link-container'
+import { useState } from 'react'
+import { MassImportModal } from 'components/import-export/mass-import-modal'
+import { MassExportModal } from 'components/import-export/mass-export-modal'
 
 type Props = {
 	setMenuSection: (section: string) => void
@@ -20,46 +23,68 @@ function MainMenu({setMenuSection}: Props) {
 	const handleLogOut = () => dispatch(logout())
 	const handleDeck = () => setMenuSection('deck')
 	const handleSettings = () => setMenuSection('settings')
+	const [showImportModal, setShowImportModal] = useState(false)
+	const [showExportModal, setShowExportModal] = useState(false)
 
 	const welcomeMessage = playerDeck.name === 'Starter Deck' ? 'Welcome' : 'Welcome Back'
 
 	return (
-		<div className={css.mainmenu}>
-			<div className={css.playerInfo}>
-				<p id={css.infoName}>
-					{welcomeMessage}, {playerName}
-				</p>
-				<p id={css.infoDeck}>{'Active Deck - ' + playerDeck.name}</p>
-				<img id={css.infoIcon} src={`/images/types/type-${playerDeck.icon}.png`} alt="deck-icon" />
-			</div>
-			<div className={css.content}>
-				<div className={css.logo}>
-					<TcgLogo />
+		<>
+			<MassImportModal
+				setOpen={showImportModal}
+				onClose={() => setShowImportModal(false)}
+			/>
+			<MassExportModal 
+				setOpen={showExportModal}
+				onClose={() => setShowExportModal(false)}
+			/>
+			<div className={css.mainmenu}>
+				<div className={css.playerInfo}>
+					<p id={css.infoName}>
+						{welcomeMessage}, {playerName}
+					</p>
+					<p id={css.infoDeck}>{'Active Deck - ' + playerDeck.name}</p>
+					<img
+						id={css.infoIcon}
+						src={`/images/types/type-${playerDeck.icon}.png`}
+						alt="deck-icon"
+					/>
 				</div>
-				<nav>
-					<Button variant="stone" id={css.public} onClick={handleJoinQueue}>
-						Public Game
-					</Button>
-					<Button variant="stone" id={css.privateCreate} onClick={handleCreatePrivateGame}>
-						Create Private Game
-					</Button>
-					<Button variant="stone" id={css.privateJoin} onClick={handleJoinPrivateGame}>
-						Join Private Game
-					</Button>
-					<Button variant="stone" id={css.deck} onClick={handleDeck}>
-						Customize Deck
-					</Button>
-					<Button variant="stone" id={css.settings} onClick={handleSettings}>
-						More
-					</Button>
-					<Button variant="stone" id={css.logout} onClick={handleLogOut}>
-						Log Out
-					</Button>
-				</nav>
-				<Beef />
-				<VersionLinks />
+				<div className={css.content}>
+					<div className={css.logo}>
+						<TcgLogo />
+					</div>
+					<nav>
+						<Button variant="stone" id={css.public} onClick={handleJoinQueue}>
+							Public Game
+						</Button>
+						<Button variant="stone" id={css.privateCreate} onClick={handleCreatePrivateGame}>
+							Create Private Game
+						</Button>
+						<Button variant="stone" id={css.privateJoin} onClick={handleJoinPrivateGame}>
+							Join Private Game
+						</Button>
+						<Button variant="stone" id={css.deck} onClick={handleDeck}>
+							Customize Deck
+						</Button>
+						<Button variant="stone" id={css.import} onClick={() => setShowImportModal(true)}>
+							Mass Import
+						</Button>
+						<Button variant="stone" id={css.export} onClick={() => setShowExportModal(true)}>
+							Mass Export
+						</Button>
+						<Button variant="stone" id={css.settings} onClick={handleSettings}>
+							More
+						</Button>
+						<Button variant="stone" id={css.logout} onClick={handleLogOut}>
+							Log Out
+						</Button>
+					</nav>
+					<Beef />
+					<VersionLinks />
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
