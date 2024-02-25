c79768fe1a6e5aad8f8b378acea6a7a93682f72d
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2024-02-25 17:25:08 -0500

    Update boss-landing.tsx
    Revise formatting and grammar

--- client/src/app/main-menu/boss-landing.tsx
+++ client/src/app/main-menu/boss-landing.tsx
@@ -49,52 +49,48 @@ function BossLanding({setMenuSection}: Props) {
 		>
 			<CardList cards={[{cardId: 'evilxisuma_boss', cardInstance: 'preview'}]} />
 			<div className={css.bossRules}>
-				That's right, the Hermitcraft TCG has its first boss fight! This is no challenge deck, Evil
-				X cares not for the cards. He brings his own moves, and they are vicious! If you think you
-				can defeat him, you'll need to bring your best game. Be sure to check your audio settings to
-				hear the voice commands during the battle.
+				<p>
+					That's right, the Hermitcraft TCG has its first boss fight! This is no challenge deck,
+					Evil X cares not for the cards. He brings his own moves, and they are vicious! If you
+					think you can defeat him, you'll need to bring your best game. Be sure to check your audio
+					settings to hear the voice commands during the battle.
+				</p>
 				<h1>Rules</h1>
-				You will always go first but can only have three rows to play on.
-				<br />
-				<br />
-				EX has only one row to play on and has no item slots to attach to his boss card. However,
-				his card has 300hp, comes back again at full health when knocked out, and will perform
-				harder attacks with every life lost.
-				<br />
-				<br />
-				<span>
+				<p>You will always go first but can only have three rows to play on.</p>
+				<p>
+					EX has only one row to play on and has no item slots to attach to his boss card. However,
+					his card has 300hp, comes back again at full health when knocked out, and will perform
+					harder attacks with every life lost.
+				</p>
+				<p>
 					For the purposes of <u>Renbob</u> and <u>Anvil</u>, EX is always directly opposite your
 					active Hermit.
-				</span>
-				<br />
-				EX is immune to Fire, Poison, and Slowness; and cannot be inflected upon him.
-				<br />
-				<br />
-				The following cards don't work in this battle:
+				</p>
+				<p>EX is immune to and cannot be inflicted with Fire, Poison, and Slowness.</p>
+				<p>The following cards don't work in this battle:</p>
 				<div>
 					<CardList cards={nonFunctionalCards} wrap={true} />
 				</div>
 				<h1>EX's Moves & Special</h1>
-				Evil X can attack for 50, 70 or 90 damage.
-				<br />
-				<br />
-				After losing a life, EX can also either heal for 150hp, set your active Hermit on fire, or
-				double the damage of his main attack.
-				<br />
-				<br />
-				<span>
+				<p>Evil X can attack for 50, 70 or 90 damage.</p>
+				<p>
+					After losing a life, EX can also either heal for 150hp, set your active Hermit on fire, or
+					double the damage of his main attack.
+				</p>
+				<p>
 					On his last life, EX can deal 20 damage to all AFK Hermits, discard your active Hermit's
 					attached effect card, or force you to discard an item card from your active Hermit.
 					Discarded effect cards act as if <u>Curse of Vanishing</u> was used and do not trigger
 					from his attack.
-				</span>
-				<br />
-				If a special move disables EX's attack, this only prevents attack damage, being set on fire
-				and damage against AFK Hermits.
-				<br />
-				<br />
-				At the end of EX's ninth turn, even if he cannot attack, he will perform one of two special
-				moves:
+				</p>
+				<p>
+					If a special move disables EX's attack, this only prevents attack damage, being set on
+					fire and damage against AFK Hermits.
+				</p>
+				<p>
+					At the end of EX's ninth turn, even if he cannot attack, he will perform one of two
+					special moves:
+				</p>
 				<ol>
 					<li>Discard your whole hand and draw one new card.</li>
 					<li>Remove all attached item and effect cards from your active Hermit.</li>
