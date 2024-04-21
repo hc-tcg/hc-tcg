a3b07ba045e4e6ebe97c69809f80a013f7ac3f70
Benji42 <63879236+Benji42@users.noreply.github.com>
2024-04-21 18:27:38 -0400

    Update description

--- common/cards/default/single-use/looting.ts
+++ common/cards/default/single-use/looting.ts
@@ -13,7 +13,7 @@ class LootingSingleUseCard extends SingleUseCard {
 			name: 'Looting',
 			rarity: 'rare',
 			description:
-				'Flip a coin. If heads, pick 1 item card from opposing active Hermit and add it to your hand.',
+				'Flip a coin. If heads, pick 1 item card from your opponent\'s active Hermit and add it to your hand.',
 		})
 	}
 
