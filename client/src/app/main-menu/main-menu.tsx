edb8abdb0fdff070320ed509d258e76fd44afbf8
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2024-04-22 17:30:28 +0100

    Check timestamp to decide whether to show updates

--- client/src/app/main-menu/main-menu.tsx
+++ client/src/app/main-menu/main-menu.tsx
@@ -23,36 +23,46 @@ function MainMenu({setMenuSection}: Props) {
 	const handleDeck = () => setMenuSection('deck')
 	const handleSettings = () => setMenuSection('settings')
 
-	const [updatesOpen, setUpdatesOpen] = useState<boolean>(true)
 	const updates = useSelector(getUpdates)
+	const [updatesOpen, setUpdatesOpen] = useState<boolean>(true)
+	const latestUpdateView = localStorage.getItem('latestUpdateView')
 
 	const welcomeMessage = playerDeck.name === 'Starter Deck' ? 'Welcome' : 'Welcome Back'
 
 	return (
 		<>
-			<AlertModal
-				setOpen={updatesOpen}
-				onClose={() => setUpdatesOpen(false)}
-				cancelText="Close"
-				title="Latest updates"
-				action={() => {}}
-				description={
-					<ul className={css.updatesList}>
-						{updates['updates'] ? (
-							updates['updates'].map((text, i) => (
-								<li className={css.updateItem} key={i}>
-									{text.split('\n').map((line) => (
-										<p>{line}</p>
-									))}
-								</li>
-							))
-						) : (
-							<li className={css.updateItem}>Failed to load updates</li>
-						)}
-						<li className={css.updateItem}>For more updates, visit the HC-TCG discord.</li>
-					</ul>
-				}
-			/>
+			{!updates['timestamps'] ||
+			!latestUpdateView ||
+			parseInt(updates['timestamps'][0]) > parseInt(latestUpdateView) ? (
+				<AlertModal
+					setOpen={updatesOpen}
+					onClose={() => {
+						setUpdatesOpen(false)
+						localStorage.setItem('latestUpdateView', (new Date().valueOf() / 1000).toFixed())
+					}}
+					cancelText="Close"
+					title="Latest updates"
+					action={() => {}}
+					description={
+						<ul className={css.updatesList}>
+							{updates['updates'] ? (
+								updates['updates'].map((text, i) => (
+									<li className={css.updateItem} key={i}>
+										{text.split('\n').map((line) => (
+											<p>{line}</p>
+										))}
+									</li>
+								))
+							) : (
+								<li className={css.updateItem}>Failed to load updates</li>
+							)}
+							<li className={css.updateItem}>For more updates, visit the HC-TCG discord.</li>
+						</ul>
+					}
+				/>
+			) : (
+				<></>
+			)}
 			<div className={css.mainmenu}>
 				<div className={css.playerInfo}>
 					<p id={css.infoName}>
