662bcc94c1c6bb410c7ef439a4042022066e38aa
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2024-05-08 15:49:00 -0400

    Fix Looting checking the wrong player's row (#527)

--- common/cards/default/single-use/looting.ts
+++ common/cards/default/single-use/looting.ts
@@ -54,7 +54,7 @@ class LootingSingleUseCard extends SingleUseCard {
 					if (pickResult.slot.type !== 'item') return 'FAILURE_INVALID_SLOT'
 					if (!pickResult.card) return 'FAILURE_INVALID_SLOT'
 
-					const playerRow = player.board.rows[pickResult.rowIndex]
+					const playerRow = opponentPlayer.board.rows[pickResult.rowIndex]
 					const hermitCard = playerRow.hermitCard
 					if (!hermitCard || !playerRow.health) return 'FAILURE_INVALID_SLOT'
 					moveCardToHand(game, pickResult.card, player)
