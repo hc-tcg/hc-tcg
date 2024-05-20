8785d6c1f2d478c910b6994d5575567e94ca4d8e
Benji42 <63879236+Benji42@users.noreply.github.com>
2024-05-20 14:23:06 -0400

    Update scar description to be more clear
    A better suggestion was given a bit after I merged, this flow is much easier to read

--- common/cards/default/hermits/goodtimeswithscar-rare.ts
+++ common/cards/default/hermits/goodtimeswithscar-rare.ts
@@ -23,7 +23,7 @@ class GoodTimesWithScarRareHermitCard extends HermitCard {
 				cost: ['builder', 'any'],
 				damage: 70,
 				power:
-					'Until the start of your next turn, if this Hermit is knocked out they are revived with 50hp.\n\nDoes not count as a knockout. This Hermit can only be revived once using this ability.',
+					'If this Hermit is knocked out before the start of your next turn, they are revived with 50hp.\n\nDoes not count as a knockout. This Hermit can only be revived once using this ability.',
 			},
 		})
 	}
