1fd204aa87d13506d6b8157e0ee372d2cd00c8b1
Lunarmagpie <bambolambo0@gmail.com>
2024-05-24 04:52:54 -0400

    add tests

---
+++ tests/formatting/profanity-filter.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, test } from '@jest/globals';
+
+import { censorString } from '../..//common/utils/formatting'
+
+describe('censorship tests', () => {
+  test('simple censorship', () => {
+    expect(censorString("fuck")).toBe("f**k")
+    // Do not censor sub words, ie ass
+    expect(censorString("pass")).toBe("pass")
+    expect(censorString("assert")).toBe("assert")
+    expect(censorString("hello pass assert hello")).toBe("hello pass assert hello")
+  });
+});
