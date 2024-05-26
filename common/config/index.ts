f36bb8d1ef2b1ef45d16468477117e0dbdc70db0
Benji42 <63879236+Benji42@users.noreply.github.com>
2024-05-25 22:21:07 -0400

    changes so it builds

--- common/config/index.ts
+++ common/config/index.ts
@@ -1,21 +1,9 @@
 import config from './server-config.json'
 import ranks from './ranks.json'
 import expansions from './expansions.json'
-import {createRequire} from 'module'
-
-function getDebugConfig() {
-	const require = createRequire(import.meta.url)
-	try {
-		return require('./debug-config.json')
-	} catch {
-		console.warn(
-			'Warning: `common/config/debug-config.json` is missing. To create it, copy `common/config/debug-config.json.example` and rename it.'
-		)
-		return {}
-	}
-}
+import debugConfig from './debug-config.json'
 
 export const CONFIG = config
-export const DEBUG_CONFIG = getDebugConfig()
+export const DEBUG_CONFIG = debugConfig
 export const RANKS = ranks
 export const EXPANSIONS = expansions
