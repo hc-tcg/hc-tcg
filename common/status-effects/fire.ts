716120edbd936aa749dfad9dee67366e8b07025b
Lunarmagpie <Bambolambo0@gmail.com>
2024-05-27 15:34:01 -0400

    Chagne prettier target to entire project (#550)
    * change prettier target to entire project
    
    * fix formatting

--- common/status-effects/fire.ts
+++ common/status-effects/fire.ts
@@ -43,7 +43,7 @@ class FireStatusEffect extends StatusEffect {
 						player: activeRowPos.player,
 						rowIndex: activeRowPos.rowIndex,
 						row: activeRowPos.row,
-					}
+				  }
 				: null
 
 			const targetRow: RowPos = {
