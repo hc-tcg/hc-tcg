e696ef920ada712eee9093421848bc26b2129b14
Benji42 <63879236+Benji42@users.noreply.github.com>
2024-05-29 00:27:10 -0400

    bad omen

--- common/cards/alter-egos/single-use/bad-omen.ts
+++ common/cards/alter-egos/single-use/bad-omen.ts
@@ -25,9 +25,6 @@ class BadOmenSingleUseCard extends SingleUseCard {
 		if (activeRow === null) return
 
 		player.hooks.onApply.add(instance, () => {
-			if (pos.card) {
-				game.battleLog.addCustomEntry(player.id, `$p${CARDS[pos.card.cardId].name}$ was burned`)
-			}
 			applyStatusEffect(
 				game,
 				'badomen',
