785f729cf2487f5037acb89352051a14489acd4e
Lunarmagpie <bambolambo0@gmail.com>
2024-06-10 15:15:57 -0400

    simplify code

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import path from 'path'
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import {CONFIG} from '../common/config'
+import CONFIG from '../common/config/server-config.json'
 import {getVersion} from '../version'
 
 export default defineConfig({
