153c230fb708b7e90da98c945c0d266b5a9021d8
cheezyanimations@gmail.com <cheezyanimations@gmail.com>
2024-06-20 04:56:47 +0000

    slimeball

--- common/cards/advent-of-tcg/effects/slimeball.ts
+++ common/cards/advent-of-tcg/effects/slimeball.ts
@@ -1,6 +1,6 @@
-import {CardPosModel} from '../../../models/card-pos-model'
+import {CardPosModel, getCardPos} from '../../../models/card-pos-model'
 import {GameModel} from '../../../models/game-model'
-import {slot} from '../../../slot'
+import {callSlotConditionWithCardPosModel, slot} from '../../../slot'
 import {discardCard} from '../../../utils/movement'
 import EffectCard from '../../base/effect-card'
 
@@ -12,23 +12,30 @@ class SlimeballEffectCard extends EffectCard {
 			name: 'Slimeball',
 			rarity: 'ultra_rare',
 			description:
-				"Attach to any Hermit, including your opponent's. That Hermit and its attached items will not be removed from the slot they are attached to, unless that Hermit is knocked out. After either player attempts to remove any of these cards, Slimeball will be discarded.",
+				"Attach to any Hermit, including your opponent's. That Hermit and its attached items will not be removed from the slot they are attached to, unless that Hermit is knocked out. Attached cards cannot be removed until slimeball is discarded.",
 		})
 	}
 
-	override _attachCondition = slot.every(slot.opponent, slot.effectSlot, slot.empty)
+	override _attachCondition = slot.every(
+		slot.opponent,
+		slot.effectSlot,
+		slot.empty,
+		slot.rowHasHermit,
+		slot.not(slot.locked),
+		(game, pos) => game.state.turn.availableActions.includes('PLAY_EFFECT_CARD')
+	)
 
-	//@TODO Fix this card to work with new system
 	override onAttach(game: GameModel, instance: string, pos: CardPosModel) {
 		const {player} = pos
 
-		// player.hooks.shouldLockSlots.add(instance, () => {
-		// 	return slot.every(slot.not(slot.empty), slot.rowIndex(pos.rowIndex), (game, pos) => {
-		// 		pos.player.hooks.shouldLockSlots.remove(instance)
-		// 		discardCard(game, pos.card)
-		// 		return true
-		// 	})
-		// })
+		player.hooks.shouldLockSlots.add(instance, () => {
+			return slot.every(
+				slot.player,
+				slot.rowIndex(pos.rowIndex),
+				slot.not(slot.effectSlot),
+				slot.not(slot.empty)
+			)
+		})
 	}
 
 	override onDetach(game: GameModel, instance: string, pos: CardPosModel) {
