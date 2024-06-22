2fb86149a86efe0ccc2a435f3aafb9eddd110274
Lunarmagpie <bambolambo0@gmail.com>
2024-06-22 01:18:14 -0400

    zombiecleo shouldn't be allowed to pick herself

--- common/cards/default/hermits/zombiecleo-rare.ts
+++ common/cards/default/hermits/zombiecleo-rare.ts
@@ -31,7 +31,7 @@ class ZombieCleoRareHermitCard extends HermitCard {
 		})
 	}
 
-	pickCondition = slot.every(slot.player, slot.hermitSlot, slot.not(slot.activeRow))
+	pickCondition = slot.every(slot.player, slot.hermitSlot, slot.not(slot.activeRow), slot.not(slot.has(this.id)))
 
 	override getAttack(
 		game: GameModel,
