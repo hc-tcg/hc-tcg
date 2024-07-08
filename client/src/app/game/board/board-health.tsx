c9bc3f1052eb1e6713332ad3f90d4a9757f066db
Lunarmagpie <bambolambo0@gmail.com>
2024-07-07 23:33:17 -0400

    fix incorrect highlighting for health cards

--- client/src/app/game/board/board-health.tsx
+++ client/src/app/game/board/board-health.tsx
@@ -4,6 +4,8 @@ import slotCss from './board.module.scss'
 import cardCss from './board.module.scss'
 import cn from 'classnames'
 import StatusEffectContainer from './board-status-effects'
+import {useSelector} from 'react-redux'
+import {getGameState, getSelectedCard} from 'logic/game/game-selectors'
 
 type HealthSlotProps = {
 	rowState: RowState
@@ -11,10 +13,17 @@ type HealthSlotProps = {
 }
 
 const HealthSlot = ({rowState, statusEffects}: HealthSlotProps) => {
+	const localGameState = useSelector(getGameState)
+	const selectedCard = useSelector(getSelectedCard)
+
 	return (
 		<div
 			id={slotCss.health}
-			className={cn(slotCss.cardWrapper, slotCss.health, slotCss.slot, cardCss.card)}
+			className={cn(slotCss.cardWrapper, slotCss.health, slotCss.slot, cardCss.card, {
+				[slotCss.unpickable]:
+					(selectedCard || localGameState?.currentPickableSlots) &&
+					localGameState?.turn.currentPlayerId === localGameState?.playerId,
+			})}
 		>
 			{rowState.health && <HealthDisplayModule health={rowState.health} />}
 			<StatusEffectContainer statusEffects={statusEffects} />
