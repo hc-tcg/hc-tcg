3e1ecc5b57ff214e7755c12fed0c9045b0bb9149
Lunarmagpie <bambolambo0@gmail.com>
2024-07-10 23:15:35 -0400

    instance -> component

--- common/status-effects/status-effect.ts
+++ common/status-effects/status-effect.ts
@@ -46,14 +46,14 @@ abstract class StatusEffect<Props extends StatusEffectProps = StatusEffectProps>
 	/**
 	 * Called when this statusEffect is applied
 	 */
-	public onApply(game: GameModel, instance: StatusEffectComponent, pos: CardPosModel) {
+	public onApply(game: GameModel, component: StatusEffectComponent, pos: CardPosModel) {
 		// default is do nothing
 	}
 
 	/**
 	 * Called when the statusEffect is removed, from either timeout or other means
 	 */
-	public onRemoval(game: GameModel, instance: StatusEffectComponent, pos: CardPosModel) {
+	public onRemoval(game: GameModel, component: StatusEffectComponent, pos: CardPosModel) {
 		// default is do nothing
 	}
 }
