bf504bbe1b3f08e93313e6a987bc8c34d72b0c16
Lunarmagpie <bambolambo0@gmail.com>
2024-07-13 14:39:40 -0400

    anvil

--- common/cards/alter-egos/single-use/anvil.ts
+++ common/cards/alter-egos/single-use/anvil.ts
@@ -1,10 +1,11 @@
-import {AttackModel} from '../../../models/attack-model'
 import {GameModel} from '../../../models/game-model'
-import {CardComponent} from '../../../components'
+import {CardComponent, RowComponent} from '../../../components'
 import {applySingleUse} from '../../../utils/board'
 import Card from '../../base/card'
 import {SingleUse} from '../../base/types'
 import {singleUse} from '../../base/defaults'
+import {AttackModel} from '../../../models/attack-model'
+import {row} from '../../../components/query'
 
 class AnvilSingleUseCard extends Card {
 	props: SingleUse = {
@@ -21,53 +22,43 @@ class AnvilSingleUseCard extends Card {
 	}
 
 	override onAttach(game: GameModel, component: CardComponent) {
-		const {player, opponentPlayer} = pos
+		const {player} = component
 
 		player.hooks.getAttack.add(component, () => {
-			const activePos = getActiveRowPos(player)
-			if (!activePos) return null
-			const activeIndex = activePos.rowIndex
-
-			const opponentRows = opponentPlayer.board.rows
-
-			const attack = opponentRows.reduce((r: null | AttackModel, row, rowIndex) => {
-				if (!row || !row.hermitCard) return r
-				if (rowIndex < activeIndex) return r
-				const newAttack = new AttackModel({
-					id: this.getInstanceKey(component, activeIndex === rowIndex ? 'active' : 'inactive'),
-					attacker: activePos,
-					target: {
-						player: opponentPlayer,
-						rowIndex: rowIndex,
-						row: row,
-					},
-					type: 'effect',
-					log: (values) =>
-						rowIndex === activeIndex
-							? `${values.defaultLog} to attack ${values.target} for ${values.damage} damage`
-							: `, ${values.target} for ${values.damage} damage`,
-				}).addDamage(this.props.id, rowIndex === activeIndex ? 30 : 10)
-
-				if (r) return r.addNewAttack(newAttack)
-
-				return newAttack
-			}, null)
-
-			return attack
+			return game.components
+				.filter(
+					RowComponent,
+					row.opponentPlayer,
+					(_game, row) => player.activeRow !== null && row.index >= player.activeRow?.index
+				)
+				.reduce((attacks: null | AttackModel, row) => {
+					const newAttack = game
+						.newAttack({
+							attacker: component.entity,
+							target: row.entity,
+							type: 'effect',
+							log: (values) =>
+								row.index === player.activeRow?.index
+									? `${values.defaultLog} to attack ${values.target} for ${values.damage} damage`
+									: `, ${values.target} for ${values.damage} damage`,
+						})
+						.addDamage(component.entity, row.index === player.activeRow?.index ? 30 : 10)
+					if (attacks === null) {
+						return newAttack
+					} else {
+						attacks.addNewAttack(newAttack)
+						return attacks
+					}
+				}, null)
 		})
 
-		player.hooks.onAttack.add(component, (attack) => {
-			const attackId = this.getInstanceKey(component, 'active')
-			const inactiveAttackId = this.getInstanceKey(component, 'active')
-			if (attack.id !== attackId && attackId !== inactiveAttackId) return
-
-			applySingleUse(game)
-
+		player.hooks.afterAttack.add(component, (_attack) => {
+			applySingleUse(game, component.slot)
 			player.hooks.onAttack.remove(component)
 		})
 	}
 
-	override onDetach(game: GameModel, component: CardComponent) {
+	override onDetach(_game: GameModel, component: CardComponent) {
 		const {player} = component
 		player.hooks.getAttack.remove(component)
 		player.hooks.onAttack.remove(component)
