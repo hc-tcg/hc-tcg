b64a99ab2cc23c73ebc714cba652b70d945d8a2b
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2024-07-20 10:56:56 +0100

    Steampunk Tango

---
+++ common/cards/season-ii/hermits/steampunktango-rare.ts
@@ -0,0 +1,50 @@
+import {GameModel} from '../../../models/game-model'
+import {CardComponent, ObserverComponent, RowComponent} from '../../../components'
+import Card from '../../base/card'
+import {hermit} from '../../base/defaults'
+import {Hermit} from '../../base/types'
+import * as query from '../../../components/query'
+
+class SteampunkTangoRare extends Card {
+	props: Hermit = {
+		...hermit,
+		id: 'steampunk_tango_rare',
+		numericId: 174,
+		name: 'Steampunk Tango',
+		expansion: 'default',
+		rarity: 'rare',
+		tokens: 0,
+		type: 'terraform',
+		health: 270,
+		primary: {
+			name: 'Porkchop Power',
+			cost: ['any'],
+			damage: 40,
+			power: null,
+		},
+		secondary: {
+			name: 'Assembly line',
+			cost: ['terraform', 'terraform'],
+			damage: 80,
+			power: 'Deal an additional 10hp damage for each afk hermit you have.',
+		},
+	}
+
+	override onAttach(game: GameModel, component: CardComponent, observer: ObserverComponent) {
+		const {player} = component
+
+		observer.subscribe(player.hooks.onAttack, (attack) => {
+			if (!attack.isAttacker(component.entity) || attack.type !== 'secondary') return
+
+			const afkHermits = game.components.filter(
+				CardComponent,
+				query.card.slot(query.slot.hermit),
+				query.not(query.card.active)
+			).length
+
+			attack.addDamage(component.entity, afkHermits * 10)
+		})
+	}
+}
+
+export default SteampunkTangoRare
