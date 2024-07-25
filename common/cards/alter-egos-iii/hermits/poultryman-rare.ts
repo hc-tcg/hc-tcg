79a2598690df769857e923603ebd8ada10556e84
Lunarmagpie <bambolambo0@gmail.com>
2024-07-25 12:58:16 -0400

    make 3

--- common/cards/alter-egos-iii/hermits/poultryman-rare.ts
+++ common/cards/alter-egos-iii/hermits/poultryman-rare.ts
@@ -16,7 +16,7 @@ class PoultryManRare extends Card {
 		background: 'alter_egos',
 		palette: 'alter_egos',
 		rarity: 'rare',
-		tokens: 5,
+		tokens: 3,
 		type: 'farm',
 		health: 280,
 		primary: {
