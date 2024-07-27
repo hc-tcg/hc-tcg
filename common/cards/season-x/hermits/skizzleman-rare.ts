cf56d57802176fe229d6dba8e04c9bd6f007719b
Lunarmagpie <bambolambo0@gmail.com>
2024-07-26 23:08:01 -0400

    convert to a stauts effect

--- common/cards/season-x/hermits/skizzleman-rare.ts
+++ common/cards/season-x/hermits/skizzleman-rare.ts
@@ -1,12 +1,10 @@
-import {CardComponent, ObserverComponent} from '../../../components'
+import {CardComponent, ObserverComponent, StatusEffectComponent} from '../../../components'
 import Card from '../../base/card'
 import {hermit} from '../../base/defaults'
 import {Hermit} from '../../base/types'
 import {GameModel} from '../../../models/game-model'
-import {executeExtraAttacks} from '../../../utils/attacks'
 import * as query from '../../../components/query'
-import {RowEntity} from '../../../entities'
-import {AttackDefs} from '../../../types/attack'
+import {GasLightEffect} from '../../../status-effects/gas-light'
 
 class SkizzlemanRare extends Card {
 	props: Hermit = {
@@ -34,60 +32,16 @@ class SkizzlemanRare extends Card {
 		},
 	}
 
-	newGasLightAttack(component: CardComponent, target: RowEntity) {
-		return {
-			attacker: component.entity,
-			target: target,
-			type: 'secondary',
-			log: (values) => `${values.target} took ${values.damage} damage from $vGas Light$`,
-		} satisfies AttackDefs
-	}
-
 	override onAttach(game: GameModel, component: CardComponent, observer: ObserverComponent) {
-		const {player, opponentPlayer} = component
-
-		let attackedEntities = new Set<RowEntity>()
-		let usedSecondary = false
-
-		observer.subscribe(player.hooks.onTurnStart, () => {
-			attackedEntities = new Set()
-			usedSecondary = false
-		})
+		const {player} = component
 
 		observer.subscribe(player.hooks.onAttack, (attack) => {
-			if (attack.isAttacker(component.entity) && attack.type === 'secondary') {
-				usedSecondary = true
-			}
-		})
-
-		observer.subscribe(opponentPlayer.hooks.onDefence, (attack) => {
-			// Status effect attacks have a special case because they happen at the end of the turn
-			if (attack.type === 'status-effect' && attack.targetEntity) {
-				if (
-					attackedEntities.has(attack.targetEntity) ||
-					opponentPlayer.activeRowEntity === attack.targetEntity
-				)
-					return
-				attack.addNewAttack(game.newAttack(this.newGasLightAttack(component, attack.targetEntity)))
-				return
-			}
-
-			if (attack.targetEntity && opponentPlayer.activeRowEntity !== attack.targetEntity) {
-				attackedEntities.add(attack.targetEntity)
-			}
-		})
-
-		observer.subscribe(player.hooks.onTurnEnd, () => {
-			if (!usedSecondary) return
-			let extraAttacks = [...attackedEntities.values()].map((entity) => {
-				let attack = game
-					.newAttack(this.newGasLightAttack(component, entity))
-					.addDamage(component.entity, 20)
-				attack.shouldIgnoreCards.push(query.card.entity(component.entity))
-				return attack
-			})
-
-			executeExtraAttacks(game, extraAttacks)
+			if (!attack.isAttacker(component.entity) || attack.type !== 'secondary') return
+			game.components
+				.filter(CardComponent, query.card.opponentPlayer, query.card.afk)
+				.map((card) => {
+					game.components.new(StatusEffectComponent, GasLightEffect).apply(card.entity)
+				})
 		})
 	}
 }
