b12b365bf783a12ff65290e97deae7aabbd19ece
Benji42 <63879236+Benji42@users.noreply.github.com>
2024-07-30 07:40:32 -0400

    Use metatype

--- common/const/strengths.ts
+++ common/const/strengths.ts
@@ -1,17 +1,7 @@
 import {TypeT} from '../../common/types/cards'
 
 type strengths = {
-	readonly any: Array<TypeT>
-	readonly balanced: Array<TypeT>
-	readonly builder: Array<TypeT>
-	readonly explorer: Array<TypeT>
-	readonly farm: Array<TypeT>
-	readonly miner: Array<TypeT>
-	readonly prankster: Array<TypeT>
-	readonly pvp: Array<TypeT>
-	readonly redstone: Array<TypeT>
-	readonly speedrunner: Array<TypeT>
-	readonly terraform: Array<TypeT>
+	readonly [Type in TypeT]: Array<TypeT>
 }
 
 export const STRENGTHS: strengths = {
