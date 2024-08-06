6f33d6f73620b6c4eb28aff3c2705061ce693eee
Rvtar <106639908+Rvtar@users.noreply.github.com>
2024-08-05 20:59:06 -0400

    Fix wrong ability trigger.

--- common/cards/alter-egos/hermits/potatoboy-rare.ts
+++ common/cards/alter-egos/hermits/potatoboy-rare.ts
@@ -44,7 +44,7 @@ class PotatoBoyRare extends Card {
 		const {player} = component
 
 		observer.subscribe(player.hooks.onAttack, (attack) => {
-			if (!attack.isAttacker(component.entity) || attack.type !== 'secondary')
+			if (!attack.isAttacker(component.entity) || attack.type == 'secondary')
 				return
 			game.components
 				.filter(
