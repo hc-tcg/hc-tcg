584edc6a16a46fb99366a680bd24558604610233
Lunarmagpie <Bambolambo0@gmail.com>
2024-08-10 22:35:04 -0400

    Move Egg To After Attack (#831)
    This fixes a bug where Joel can not steal an item from an egged hermit

--- common/cards/alter-egos/single-use/egg.ts
+++ common/cards/alter-egos/single-use/egg.ts
@@ -60,7 +60,7 @@ class Egg extends Card {
 			})
 		})
 
-		observer.subscribe(player.hooks.onAttack, (attack) => {
+		observer.subscribe(player.hooks.afterAttack, (attack) => {
 			if (!afkHermitSlot?.inRow()) return
 			const activeHermit = player.getActiveHermit()
 			if (!attack.isAttacker(activeHermit?.entity)) return
