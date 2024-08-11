070ca7de70a0480cf5ecbf66c4139dd1145074b3
Lunarmagpie <bambolambo0@gmail.com>
2024-08-10 23:03:16 -0400

    fix socket bug

--- server/src/utils/comm.ts
+++ server/src/utils/comm.ts
@@ -1,11 +1,12 @@
 import {ServerMessage} from 'common/socket-messages/server-messages'
 
 export function broadcast(
-	players: Array<{socket: any}>,
+	players: Array<{socket: any} | undefined>,
 	message: ServerMessage,
 ) {
 	players.forEach((player) => {
-		const playerSocket = player.socket
+		if (!player) return
+    const playerSocket = player.socket
 		if (playerSocket && playerSocket.connected) {
 			playerSocket.emit(message.type, message)
 		}
