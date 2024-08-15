77a53b8cbd4d959ed8ce90df93b289a97a9a217f
Lunarmagpie <bambolambo0@gmail.com>
2024-08-15 02:36:45 -0400

    Add dwarf impulse test

---
+++ tests/game/dwarf-impulse-rare.ts
@@ -0,0 +1,102 @@
+import {describe, expect, test} from '@jest/globals'
+import FiveAMPearlRare from 'common/cards/alter-egos-ii/hermits/fiveampearl-rare'
+import DwarfImpulseRare from 'common/cards/alter-egos-iii/hermits/dwarfimpulse-rare'
+import LightningRod from 'common/cards/alter-egos/effects/lightning-rod'
+import Wolf from 'common/cards/default/effects/wolf'
+import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
+import EthosLabRare from 'common/cards/default/hermits/ethoslab-rare'
+import GoldenAxe from 'common/cards/default/single-use/golden-axe'
+import {RowComponent, SlotComponent} from 'common/components'
+import query from 'common/components/query'
+import {GameModel} from 'common/models/game-model'
+import {attack, endTurn, findCardInHand, playCard, testGame} from './utils'
+
+function* testDwarfImpulseHelperSaga(game: GameModel) {
+	yield* playCard(
+		game,
+		findCardInHand(game.currentPlayer, DwarfImpulseRare),
+		game.components.find(
+			SlotComponent,
+			query.slot.currentPlayer,
+			query.slot.hermit,
+		)!,
+	)
+
+	yield* endTurn(game)
+
+	yield* playCard(
+		game,
+		findCardInHand(game.currentPlayer, FiveAMPearlRare),
+		game.components.find(
+			SlotComponent,
+			query.slot.currentPlayer,
+			query.slot.hermit,
+			query.slot.rowIndex(0),
+		)!,
+	)
+
+	yield* playCard(
+		game,
+		findCardInHand(game.currentPlayer, EthosLabRare),
+		game.components.find(
+			SlotComponent,
+			query.slot.currentPlayer,
+			query.slot.hermit,
+			query.slot.rowIndex(1),
+		)!,
+	)
+
+	yield* playCard(
+		game,
+		findCardInHand(game.currentPlayer, Wolf),
+		game.components.find(
+			SlotComponent,
+			query.slot.currentPlayer,
+			query.slot.hermit,
+			query.slot.rowIndex(0),
+		)!,
+	)
+
+	yield* playCard(
+		game,
+		findCardInHand(game.currentPlayer, LightningRod),
+		game.components.find(
+			SlotComponent,
+			query.slot.currentPlayer,
+			query.slot.hermit,
+			query.slot.rowIndex(1),
+		)!,
+	)
+
+	yield* endTurn(game)
+
+	yield* playCard(
+		game,
+		findCardInHand(game.currentPlayer, GoldenAxe),
+		game.components.find(SlotComponent, query.slot.singleUse)!,
+	)
+
+	yield* attack(game, 'secondary')
+
+	// Dwarf impulse should have disabled wolf, so it should not have triggered.
+	expect(
+		game.components.find(
+			RowComponent,
+			query.row.currentPlayer,
+			query.row.active,
+		)?.health,
+	).toEqual(DwarfImpulseRare.health)
+}
+
+describe('Test Dwarf Impulse Rare', () => {
+	test('Test Dwarf Impulse works with lightning rod.', () => {
+		testGame(
+			{
+				saga: testDwarfImpulseHelperSaga,
+				playerOneDeck: [DwarfImpulseRare, GoldenAxe],
+				playerTwoDeck: [FiveAMPearlRare, EthosLabCommon, LightningRod, Wolf],
+			},
+			{startWithAllCards: true},
+		)
+	})
+})
