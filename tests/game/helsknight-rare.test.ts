fa6a10b51063391f917a31c9207dac7c1ba89e74
Lunarmagpie <bambolambo0@gmail.com>
2024-08-31 14:18:23 -0400

    Add helsknight rare test

---
+++ tests/game/helsknight-rare.test.ts
@@ -0,0 +1,37 @@
+import {describe, expect, test} from '@jest/globals'
+import HelsknightRare from 'common/cards/alter-egos/hermits/helsknight-rare'
+import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
+import TNT from 'common/cards/default/single-use/tnt'
+import {attack, endTurn, playCardFromHand, testGame} from './utils'
+
+describe('Test Hels Trap Hole', () => {
+	test('Test Trap Hole with TNT', () => {
+		testGame(
+			{
+				playerOneDeck: [EthosLabCommon, TNT],
+				playerTwoDeck: [HelsknightRare],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, HelsknightRare, 'hermit', 0)
+
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, TNT, 'single_use')
+
+					yield* attack(game, 'single-use')
+
+					expect(
+						game.opponentPlayer.getHand().map((card) => card.props),
+					).toStrictEqual([TNT])
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true, forceCoinFlip: true},
+		)
+	})
+})
+
