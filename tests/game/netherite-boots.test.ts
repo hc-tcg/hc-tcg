b984f346ccda55e86e90edcfa78101edacd83ad4
Lunarmagpie <bambolambo0@gmail.com>
2024-09-03 08:50:20 -0400

    finish netherite boot tests

--- tests/game/netherite-boots.test.ts
+++ tests/game/netherite-boots.test.ts
@@ -3,6 +3,7 @@ import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
 import {
 	applyEffect,
 	attack,
+	changeActiveHermit,
 	endTurn,
 	pick,
 	playCardFromHand,
@@ -16,6 +17,8 @@ import FireEffect from 'common/status-effects/fire'
 import PoultryManRare from 'common/cards/alter-egos-iii/hermits/poultryman-rare'
 import TangoTekCommon from 'common/cards/default/hermits/tangotek-common'
 import Egg from 'common/cards/alter-egos/single-use/egg'
+import Knockback from 'common/cards/default/single-use/knockback'
+import TargetBlock from 'common/cards/alter-egos/single-use/target-block'
 
 describe('Test Netherite Boots', () => {
 	test('Test Netherite Boots Prevents 20 Damage and Burn', () => {
@@ -92,7 +95,7 @@ describe('Test Netherite Boots', () => {
 		testGame(
 			{
 				playerOneDeck: [EthosLabCommon, TangoTekCommon, NetheriteBoots],
-				playerTwoDeck: [EthosLabCommon, Egg],
+				playerTwoDeck: [EthosLabCommon, Knockback],
 				saga: function* (game) {
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
 					yield* playCardFromHand(game, NetheriteBoots, 'attach', 0)
@@ -100,19 +103,40 @@ describe('Test Netherite Boots', () => {
 					yield* endTurn(game)
 
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
-					yield* playCardFromHand(game, Egg, 'single_use')
+					yield* playCardFromHand(game, Knockback, 'single_use')
 
 					yield* attack(game, 'secondary')
+
+					expect(game.hasActiveRequests()).toBe(false)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+
+	test('Test Netherite Boots prevent damage redirect', () => {
+		testGame(
+			{
+				playerOneDeck: [EthosLabCommon, EthosLabCommon, NetheriteBoots],
+				playerTwoDeck: [EthosLabCommon, TargetBlock],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
+					yield* playCardFromHand(game, NetheriteBoots, 'attach', 1)
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, TargetBlock, 'single_use')
 					yield* pick(
 						game,
-						query.slot.opponent,
 						query.slot.rowIndex(1),
+						query.slot.opponent,
 						query.slot.hermit,
 					)
+					yield* attack(game, 'secondary')
 
-					expect(
-						game.components.find(RowComponent, query.row.active)?.index,
-					).toBe(0)
+          expect(game.components.find(RowComponent, query.row.index(0), query.row.opponentPlayer)?.health).toBe(EthosLabCommon.health)
+          expect(game.components.find(RowComponent, query.row.index(1), query.row.opponentPlayer)?.health).toBe(EthosLabCommon.health)
 				},
 			},
 			{startWithAllCards: true, noItemRequirements: true},
