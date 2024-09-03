b51b154df540c89cf1df138f1e7d656e136ccc6f
Lunarmagpie <bambolambo0@gmail.com>
2024-09-02 23:46:24 -0400

    more changes

--- tests/game/netherite-boots.test.ts
+++ tests/game/netherite-boots.test.ts
@@ -1,15 +1,21 @@
 import {describe, expect, test} from '@jest/globals'
 import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
-import IJevinRare from 'common/cards/default/hermits/ijevin-rare'
-import Iskall85Common from 'common/cards/default/hermits/iskall85-common'
-import {printBoardState} from 'server/utils'
-import {applyEffect, attack, endTurn, finishModalRequest, playCardFromHand, testGame} from './utils'
+import {
+	applyEffect,
+	attack,
+	endTurn,
+	pick,
+	playCardFromHand,
+	testGame,
+} from './utils'
 import NetheriteBoots from 'common/cards/alter-egos-iii/effects/netherite-boots'
-import assert from 'assert'
 import LavaBucket from 'common/cards/default/single-use/lava-bucket'
-import {StatusEffectComponent} from 'common/components'
+import {RowComponent, StatusEffectComponent} from 'common/components'
 import query from 'common/components/query'
 import FireEffect from 'common/status-effects/fire'
+import PoultryManRare from 'common/cards/alter-egos-iii/hermits/poultryman-rare'
+import TangoTekCommon from 'common/cards/default/hermits/tangotek-common'
+import Egg from 'common/cards/alter-egos/single-use/egg'
 
 describe('Test Netherite Boots', () => {
 	test('Test Netherite Boots Prevents 20 Damage and Burn', () => {
@@ -24,7 +30,7 @@ describe('Test Netherite Boots', () => {
 
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
 					yield* playCardFromHand(game, LavaBucket, 'single_use')
-          yield* applyEffect(game) 
+					yield* applyEffect(game)
 
 					yield* attack(game, 'secondary')
 
@@ -36,6 +42,40 @@ describe('Test Netherite Boots', () => {
 					).not.toBeNull()
 
 					yield* endTurn(game)
+
+					// We are immune to burn so we do not take damage from it.
+					expect(
+						game.components.find(
+							RowComponent,
+							query.row.active,
+							query.row.currentPlayer,
+						)?.health,
+					).toBe(EthosLabCommon.health - EthosLabCommon.secondary.damage)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+
+	test('Test Netherite Boots Prevents Knockback', () => {
+		testGame(
+			{
+				playerOneDeck: [EthosLabCommon, TangoTekCommon, NetheriteBoots],
+				playerTwoDeck: [EthosLabCommon, Egg],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, NetheriteBoots, 'attach', 0)
+					yield* playCardFromHand(game, TangoTekCommon, 'hermit', 1)
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, Egg, 'single_use')
+
+					yield* attack(game, 'secondary')
+
+					expect(
+						game.components.find(RowComponent, query.row.active)?.index,
+					).toBe(0)
 				},
 			},
 			{startWithAllCards: true, noItemRequirements: true},
