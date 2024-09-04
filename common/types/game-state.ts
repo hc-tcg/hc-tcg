b385a2db9704f6396163612e973414893a6aa1fe
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2024-09-03 21:53:45 -0400

    Fix Amnesia blocking Cleo's primary instead of Puppetry

--- common/types/game-state.ts
+++ common/types/game-state.ts
@@ -201,6 +201,12 @@ export type GameLog = {
 	startDeck: string
 }
 
+export type UsedHermitAttackInfo = {
+	readonly attackType: 'primary' | 'secondary'
+	readonly attacker: CardComponent
+	readonly turn: number
+}
+
 export abstract class DefaultDictionary<Keys, Type> {
 	default: () => Type
 	values: Record<string, Type> = {}
--- tests/game/zombie-cleo-rare.test.ts
+++ tests/game/zombie-cleo-rare.test.ts
@@ -1,10 +1,19 @@
 import {describe, expect, test} from '@jest/globals'
+import ArchitectFalseRare from 'common/cards/alter-egos-iii/hermits/architectfalse-rare'
 import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
 import ZombieCleoRare from 'common/cards/default/hermits/zombiecleo-rare'
-import {RowComponent} from 'common/components'
+import {RowComponent, StatusEffectComponent} from 'common/components'
 import query from 'common/components/query'
 import {GameModel} from 'common/models/game-model'
-import {attack, endTurn, playCardFromHand, testGame} from './utils'
+import {MultiturnSecondaryAttackDisabledEffect} from 'common/status-effects/multiturn-attack-disabled'
+import {
+	attack,
+	endTurn,
+	finishModalRequest,
+	pick,
+	playCardFromHand,
+	testGame,
+} from './utils'
 
 function* testPrimaryDoesNotCrash(game: GameModel) {
 	yield* playCardFromHand(game, ZombieCleoRare, 'hermit', 0)
@@ -27,6 +36,41 @@ function* testPrimaryDoesNotCrash(game: GameModel) {
 	).not.toEqual(EthosLabCommon.health)
 }
 
+function* testAmnesiaDisablesPuppetry(game: GameModel) {
+	yield* playCardFromHand(game, ArchitectFalseRare, 'hermit', 0)
+
+	yield* endTurn(game)
+
+	yield* playCardFromHand(game, ZombieCleoRare, 'hermit', 0)
+	yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
+
+	yield* attack(game, 'secondary')
+
+	yield* pick(
+		game,
+		query.slot.currentPlayer,
+		query.slot.hermit,
+		query.slot.rowIndex(1),
+	)
+
+	yield* finishModalRequest(game, {pick: 'primary'})
+
+	yield* endTurn(game)
+
+	yield* attack(game, 'secondary')
+
+	expect(
+		game.components.find(
+			StatusEffectComponent,
+			query.effect.is(MultiturnSecondaryAttackDisabledEffect),
+			query.effect.targetIsCardAnd(
+				query.card.opponentPlayer,
+				query.card.active,
+			),
+		),
+	)
+}
+
 describe('Test Zombie Cleo', () => {
 	test('Test Zombie Cleo Primary Does Not Crash Server', () => {
 		testGame(
@@ -34,6 +78,17 @@ describe('Test Zombie Cleo', () => {
 				saga: testPrimaryDoesNotCrash,
 				playerOneDeck: [ZombieCleoRare],
 				playerTwoDeck: [EthosLabCommon],
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+
+	test('Test Puppetry is Disabled by Amnesia', () => {
+		testGame(
+			{
+				saga: testAmnesiaDisablesPuppetry,
+				playerOneDeck: [ArchitectFalseRare],
+				playerTwoDeck: [ZombieCleoRare, EthosLabCommon],
 			},
 			{startWithAllCards: true, noItemRequirements: true},
 		)
