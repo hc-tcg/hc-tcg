3452666df06d3e49e6b3a420849858bebc313c7c
Lunarmagpie <bambolambo0@gmail.com>
2024-09-07 08:58:25 -0400

    Skizz rare test fixed

--- tests/game/skizzleman-rare.test.ts
+++ tests/game/skizzleman-rare.test.ts
@@ -1,31 +1,23 @@
 import {describe, expect, test} from '@jest/globals'
 import Anvil from 'common/cards/alter-egos/single-use/anvil'
 import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
-import PearlescentMoonRare from 'common/cards/default/hermits/pearlescentmoon-rare'
 import SkizzlemanRare from 'common/cards/season-x/hermits/skizzleman-rare'
-import {RowComponent, StatusEffectComponent} from 'common/components'
+import {RowComponent} from 'common/components'
 import query from 'common/components/query'
-import {
-	AussiePingEffect,
-	AussiePingImmuneEffect,
-} from 'common/status-effects/aussie-ping'
 import {attack, endTurn, playCardFromHand, testGame} from './utils'
 
 describe('Test Skizzleman Rare Rare', () => {
 	test('Gaslight works as intended', () => {
 		testGame(
 			{
-				playerOneDeck: [SkizzlemanRare, Anvil],
-				playerTwoDeck: [EthosLabCommon, EthosLabCommon],
+				playerOneDeck: [EthosLabCommon, EthosLabCommon],
+				playerTwoDeck: [SkizzlemanRare, Anvil],
 				saga: function* (game) {
-					yield* playCardFromHand(game, SkizzlemanRare, 'hermit', 0)
-					yield* endTurn(game)
-
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
-
 					yield* endTurn(game)
 
+					yield* playCardFromHand(game, SkizzlemanRare, 'hermit', 0)
 					// Use Anvil to trigger Skizz's bonus damage.
 					yield* playCardFromHand(game, Anvil, 'single_use')
 					yield* attack(game, 'secondary')
@@ -40,7 +32,6 @@ describe('Test Skizzleman Rare Rare', () => {
 					).toBe(
 						EthosLabCommon.health -
 							SkizzlemanRare.secondary.damage -
-							20 /* Gaslight */ -
 							30 /* Anvil damage */,
 					)
 					expect(
@@ -50,10 +41,7 @@ describe('Test Skizzleman Rare Rare', () => {
 							query.row.index(1),
 						)?.health,
 					).toBe(
-						EthosLabCommon.health -
-							SkizzlemanRare.secondary.damage -
-							20 /* Gaslight */ -
-							10 /* Anvil damage */,
+						EthosLabCommon.health - 20 /* Gaslight */ - 10 /* Anvil damage */,
 					)
 				},
 			},
