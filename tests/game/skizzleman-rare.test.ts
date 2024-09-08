c2ad220c837fda7963bef1b22e180e188721ba29
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2024-09-08 00:32:42 -0400

    Add tests for race conditions

--- tests/game/skizzleman-rare.test.ts
+++ tests/game/skizzleman-rare.test.ts
@@ -1,12 +1,13 @@
 import {describe, expect, test} from '@jest/globals'
 import Anvil from 'common/cards/alter-egos/single-use/anvil'
+import Thorns from 'common/cards/default/effects/thorns'
 import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
 import SkizzlemanRare from 'common/cards/season-x/hermits/skizzleman-rare'
 import {RowComponent} from 'common/components'
 import query from 'common/components/query'
 import {attack, endTurn, playCardFromHand, testGame} from './utils'
 
-describe('Test Skizzleman Rare Rare', () => {
+describe('Test Skizzleman Rare', () => {
 	test('Gaslight works as intended', () => {
 		testGame(
 			{
@@ -43,6 +44,36 @@ describe('Test Skizzleman Rare Rare', () => {
 					).toBe(
 						EthosLabCommon.health - 20 /* Gaslight */ - 10 /* Anvil damage */,
 					)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+
+	test('Gaslight only triggers Thorns once', () => {
+		testGame(
+			{
+				playerOneDeck: [EthosLabCommon, EthosLabCommon, Thorns],
+				playerTwoDeck: [SkizzlemanRare, Anvil],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
+					yield* playCardFromHand(game, Thorns, 'attach', 1)
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, SkizzlemanRare, 'hermit', 0)
+					// Use Anvil to trigger Skizz's bonus damage.
+					yield* playCardFromHand(game, Anvil, 'single_use')
+					yield* attack(game, 'secondary')
+					yield* endTurn(game)
+
+					expect(
+						game.components.find(
+							RowComponent,
+							query.row.opponentPlayer,
+							query.row.index(0),
+						)?.health,
+					).toBe(SkizzlemanRare.health - 20 /* Thorns damage */)
 				},
 			},
 			{startWithAllCards: true, noItemRequirements: true},
