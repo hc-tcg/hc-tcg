48a91d754b28a41de38ea3e8d7b236fe1f4a8c92
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2024-09-08 15:18:53 -0400

    Create tests for turtle shell

---
+++ tests/game/turtle-shell.test.ts
@@ -0,0 +1,626 @@
+import {describe, expect, test} from '@jest/globals'
+import ArmorStand from 'common/cards/alter-egos/effects/armor-stand'
+import TurtleShell from 'common/cards/alter-egos/effects/turtle-shell'
+import Egg from 'common/cards/alter-egos/single-use/egg'
+import Ladder from 'common/cards/alter-egos/single-use/ladder'
+import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
+import GrianRare from 'common/cards/default/hermits/grian-rare'
+import Iskall85Common from 'common/cards/default/hermits/iskall85-common'
+import ZombieCleoCommon from 'common/cards/default/hermits/zombiecleo-common'
+import ChorusFruit from 'common/cards/default/single-use/chorus-fruit'
+import Knockback from 'common/cards/default/single-use/knockback'
+import LavaBucket from 'common/cards/default/single-use/lava-bucket'
+import Mending from 'common/cards/default/single-use/mending'
+import SkizzlemanRare from 'common/cards/season-x/hermits/skizzleman-rare'
+import {CardComponent} from 'common/components'
+import query from 'common/components/query'
+import {SelectCards} from 'common/types/modal-requests'
+import {
+	applyEffect,
+	attack,
+	changeActiveHermit,
+	endTurn,
+	finishModalRequest,
+	pick,
+	playCardFromHand,
+	testGame,
+} from './utils'
+import Emerald from 'common/cards/default/single-use/emerald'
+import Cubfan135Rare from 'common/cards/default/hermits/cubfan135-rare'
+
+describe('Test Turtle Shell', () => {
+	test('Turtle Shell applies to next turn', () => {
+		testGame(
+			{
+				playerOneDeck: [EthosLabCommon, Iskall85Common, TurtleShell],
+				playerTwoDeck: [ZombieCleoCommon],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, Iskall85Common, 'hermit', 1)
+					yield* playCardFromHand(game, TurtleShell, 'attach', 1)
+
+					yield* changeActiveHermit(game, 1)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, ZombieCleoCommon, 'hermit', 0)
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					expect(game.currentPlayer.activeRow?.health).toBe(
+						Iskall85Common.health,
+					)
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.slot(query.slot.discardPile),
+						),
+					).not.toBe(null)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+
+	test('Turtle Shell protects against Gas Light and Egg, but not status-effect damage', () => {
+		testGame(
+			{
+				playerOneDeck: [EthosLabCommon, Iskall85Common, TurtleShell],
+				playerTwoDeck: [SkizzlemanRare, LavaBucket, Egg],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, Iskall85Common, 'hermit', 1)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, SkizzlemanRare, 'hermit', 0)
+
+					yield* playCardFromHand(game, LavaBucket, 'single_use')
+					yield* applyEffect(game)
+
+					yield* endTurn(game)
+
+					yield* changeActiveHermit(game, 1)
+
+					yield* endTurn(game)
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, TurtleShell, 'attach', 0)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, Egg, 'single_use')
+					yield* attack(game, 'secondary')
+
+					yield* pick(
+						game,
+						query.slot.opponent,
+						query.slot.hermit,
+						query.slot.rowIndex(0),
+					)
+
+					yield* endTurn(game)
+
+					expect(game.currentPlayer.activeRow?.index).toBe(0)
+					expect(game.currentPlayer.activeRow?.health).toBe(
+						EthosLabCommon.health - 60 /* 3x Burn damage*/,
+					)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+
+	test('Turtle Shell is not discarded when row is activated by Knockback', () => {
+		testGame(
+			{
+				playerOneDeck: [EthosLabCommon, Iskall85Common, TurtleShell],
+				playerTwoDeck: [ZombieCleoCommon, Knockback],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, Iskall85Common, 'hermit', 1)
+					yield* playCardFromHand(game, TurtleShell, 'attach', 1)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, ZombieCleoCommon, 'hermit', 0)
+					yield* playCardFromHand(game, Knockback, 'single_use')
+					yield* attack(game, 'secondary')
+
+					yield* pick(
+						game,
+						query.slot.opponent,
+						query.slot.hermit,
+						query.slot.rowIndex(1),
+					)
+
+					yield* endTurn(game)
+
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.attached,
+						),
+					).not.toBe(null)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+
+	test("Turtle Shell works with Ladder only on hermit's first turn active", () => {
+		testGame(
+			{
+				playerOneDeck: [
+					EthosLabCommon,
+					EthosLabCommon,
+					TurtleShell,
+					Ladder,
+					TurtleShell,
+					Ladder,
+				],
+				playerTwoDeck: [ZombieCleoCommon],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
+					yield* playCardFromHand(game, TurtleShell, 'attach', 1)
+
+					yield* playCardFromHand(game, Ladder, 'single_use')
+					yield* pick(
+						game,
+						query.slot.currentPlayer,
+						query.slot.hermit,
+						query.slot.rowIndex(1),
+					)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, ZombieCleoCommon, 'hermit', 0)
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					expect(game.currentPlayer.activeRow?.health).toBe(
+						EthosLabCommon.health,
+					)
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.slot(query.slot.discardPile),
+						),
+					).not.toBe(null)
+
+					yield* playCardFromHand(game, TurtleShell, 'attach', 0)
+
+					yield* playCardFromHand(game, Ladder, 'single_use')
+					yield* pick(
+						game,
+						query.slot.currentPlayer,
+						query.slot.hermit,
+						query.slot.rowIndex(0),
+					)
+
+					yield* endTurn(game)
+
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					expect(game.currentPlayer.activeRow?.health).toBe(
+						EthosLabCommon.health - ZombieCleoCommon.secondary.damage,
+					)
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.attached,
+						),
+					).not.toBe(null)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+
+	test('Ladder deactivates Turtle Shell after active moves', () => {
+		testGame(
+			{
+				playerOneDeck: [
+					ArmorStand,
+					EthosLabCommon,
+					EthosLabCommon,
+					TurtleShell,
+					Ladder,
+				],
+				playerTwoDeck: [ZombieCleoCommon],
+				saga: function* (game) {
+					yield* playCardFromHand(game, ArmorStand, 'hermit', 0)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 2)
+					yield* playCardFromHand(game, TurtleShell, 'attach', 1)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, ZombieCleoCommon, 'hermit', 0)
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					yield* changeActiveHermit(game, 1)
+					yield* playCardFromHand(game, Ladder, 'single_use')
+					yield* pick(
+						game,
+						query.slot.currentPlayer,
+						query.slot.hermit,
+						query.slot.rowIndex(2),
+					)
+
+					yield* endTurn(game)
+
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					expect(game.currentPlayer.activeRow?.health).toBe(
+						EthosLabCommon.health - ZombieCleoCommon.secondary.damage,
+					)
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.attached,
+						),
+					).not.toBe(null)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+
+	test('Mending deactivates moved Turtle Shell', () => {
+		testGame(
+			{
+				playerOneDeck: [
+					ArmorStand,
+					EthosLabCommon,
+					Iskall85Common,
+					TurtleShell,
+					Mending,
+				],
+				playerTwoDeck: [ZombieCleoCommon],
+				saga: function* (game) {
+					yield* playCardFromHand(game, ArmorStand, 'hermit', 0)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
+					yield* playCardFromHand(game, Iskall85Common, 'hermit', 2)
+					yield* playCardFromHand(game, TurtleShell, 'attach', 1)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, ZombieCleoCommon, 'hermit', 0)
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					yield* changeActiveHermit(game, 1)
+					yield* playCardFromHand(game, Mending, 'single_use')
+					yield* pick(
+						game,
+						query.slot.currentPlayer,
+						query.slot.attach,
+						query.slot.rowIndex(2),
+					)
+
+					yield* endTurn(game)
+
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					expect(game.currentPlayer.activeRow?.health).toBe(
+						EthosLabCommon.health - ZombieCleoCommon.secondary.damage,
+					)
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.attached,
+						),
+					).not.toBe(null)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+
+	test('Turtle Shell is still discarded when row is deactivated by Knockback', () => {
+		testGame(
+			{
+				playerOneDeck: [EthosLabCommon, Iskall85Common, TurtleShell],
+				playerTwoDeck: [ZombieCleoCommon, Knockback],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, Iskall85Common, 'hermit', 1)
+					yield* playCardFromHand(game, TurtleShell, 'attach', 1)
+					yield* changeActiveHermit(game, 1)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, ZombieCleoCommon, 'hermit', 0)
+					yield* playCardFromHand(game, Knockback, 'single_use')
+					yield* attack(game, 'secondary')
+
+					yield* pick(
+						game,
+						query.slot.opponent,
+						query.slot.hermit,
+						query.slot.rowIndex(0),
+					)
+
+					yield* endTurn(game)
+
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.slot(query.slot.discardPile),
+						),
+					).not.toBe(null)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+
+	test("Turtle Shells borrowed by Grian only work on Grian's first turn", () => {
+		testGame(
+			{
+				playerOneDeck: [
+					EthosLabCommon,
+					EthosLabCommon,
+					TurtleShell,
+					TurtleShell,
+					ChorusFruit,
+				],
+				playerTwoDeck: [GrianRare],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
+					yield* playCardFromHand(game, TurtleShell, 'attach', 1)
+					yield* changeActiveHermit(game, 1)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, GrianRare, 'hermit', 0)
+					yield* attack(game, 'primary')
+					expect(
+						(game.state.modalRequests[0].modal as SelectCards.Data)
+							.primaryButton,
+					).toStrictEqual({text: 'Attach', variant: 'default'})
+					// If `primaryButton` is null, Grian may not attach borrowed Turtle Shells
+					yield* finishModalRequest(game, {result: true, cards: null})
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, TurtleShell, 'attach', 0)
+					yield* playCardFromHand(game, ChorusFruit, 'single_use')
+					yield* attack(game, 'secondary')
+					yield* pick(
+						game,
+						query.slot.currentPlayer,
+						query.slot.hermit,
+						query.slot.rowIndex(0),
+					)
+
+					yield* endTurn(game)
+
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.slot(query.slot.discardPile),
+						),
+					).not.toBe(null)
+					expect(game.currentPlayer.activeRow?.health).toBe(GrianRare.health)
+
+					yield* attack(game, 'primary')
+					yield* finishModalRequest(game, {result: true, cards: null})
+
+					yield* endTurn(game)
+
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					expect(game.currentPlayer.activeRow?.health).toBe(
+						GrianRare.health - EthosLabCommon.secondary.damage,
+					)
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.attached,
+						),
+					).not.toBe(null)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true, forceCoinFlip: true},
+		)
+	})
+
+	test("Turtle Shells taken by Emerald only work on destination hermit's first turn", () => {
+		testGame(
+			{
+				playerOneDeck: [
+					EthosLabCommon,
+					EthosLabCommon,
+					TurtleShell,
+					TurtleShell,
+					ChorusFruit,
+				],
+				playerTwoDeck: [ZombieCleoCommon, Emerald, Emerald],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
+					yield* playCardFromHand(game, TurtleShell, 'attach', 1)
+					yield* changeActiveHermit(game, 1)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, ZombieCleoCommon, 'hermit', 0)
+					yield* playCardFromHand(game, Emerald, 'single_use')
+					yield* applyEffect(game)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, TurtleShell, 'attach', 0)
+					yield* playCardFromHand(game, ChorusFruit, 'single_use')
+					yield* attack(game, 'secondary')
+					yield* pick(
+						game,
+						query.slot.currentPlayer,
+						query.slot.hermit,
+						query.slot.rowIndex(0),
+					)
+
+					yield* endTurn(game)
+
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.slot(query.slot.discardPile),
+						),
+					).not.toBe(null)
+					expect(game.currentPlayer.activeRow?.health).toBe(
+						ZombieCleoCommon.health,
+					)
+
+					yield* playCardFromHand(game, Emerald, 'single_use')
+					yield* applyEffect(game)
+
+					yield* endTurn(game)
+
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					expect(game.currentPlayer.activeRow?.health).toBe(
+						GrianRare.health - EthosLabCommon.secondary.damage,
+					)
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.attached,
+						),
+					).not.toBe(null)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true, forceCoinFlip: true},
+		)
+	})
+
+	test("Turtle Shells given by Emerald only work on destination hermit's first turn", () => {
+		testGame(
+			{
+				playerOneDeck: [
+					ArmorStand,
+					ArmorStand,
+					Cubfan135Rare,
+					Cubfan135Rare,
+					TurtleShell,
+					TurtleShell,
+					Emerald,
+				],
+				playerTwoDeck: [EthosLabCommon],
+				saga: function* (game) {
+					yield* playCardFromHand(game, ArmorStand, 'hermit', 0)
+					yield* playCardFromHand(game, Cubfan135Rare, 'hermit', 1)
+					yield* playCardFromHand(game, TurtleShell, 'attach', 1)
+					yield* playCardFromHand(game, ArmorStand, 'hermit', 2)
+					yield* playCardFromHand(game, Cubfan135Rare, 'hermit', 3)
+					yield* playCardFromHand(game, TurtleShell, 'attach', 3)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					yield* changeActiveHermit(game, 1)
+					yield* playCardFromHand(game, Emerald, 'single_use')
+					yield* applyEffect(game)
+
+					yield* attack(game, 'secondary')
+					yield* pick(
+						game,
+						query.slot.currentPlayer,
+						query.slot.hermit,
+						query.slot.rowIndex(2),
+					)
+
+					yield* endTurn(game)
+
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.slot(query.slot.discardPile),
+						),
+					).not.toBe(null)
+					expect(game.currentPlayer.activeRow?.health).toBe(
+						EthosLabCommon.health,
+					)
+
+					yield* attack(game, 'secondary')
+
+					yield* endTurn(game)
+
+					yield* changeActiveHermit(game, 3)
+					yield* playCardFromHand(game, Emerald, 'single_use')
+					yield* applyEffect(game)
+
+					yield* attack(game, 'secondary')
+					yield* pick(
+						game,
+						query.slot.currentPlayer,
+						query.slot.hermit,
+						query.slot.rowIndex(2),
+					)
+
+					yield* endTurn(game)
+
+					expect(game.currentPlayer.activeRow?.health).toBe(
+						EthosLabCommon.health - Cubfan135Rare.secondary.damage,
+					)
+					expect(
+						game.components.find(
+							CardComponent,
+							query.card.is(TurtleShell),
+							query.card.currentPlayer,
+							query.card.attached,
+						),
+					).not.toBe(null)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true, forceCoinFlip: true},
+		)
+	})
+})
