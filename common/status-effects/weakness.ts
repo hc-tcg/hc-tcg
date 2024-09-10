de252e97cb648e76e24f840c638e8371a3554447
Rvtar <106639908+Rvtar@users.noreply.github.com>
2024-09-10 10:07:22 -0400

    first draft

--- common/status-effects/weakness.ts
+++ common/status-effects/weakness.ts
@@ -1,16 +1,25 @@
 import {
-	CardComponent,
+	PlayerComponent,
 	ObserverComponent,
 	StatusEffectComponent,
+	CardComponent,
+	RowComponent
 } from '../components'
+import query from '../components/query'
 import {GameModel} from '../models/game-model'
+import { getGameState } from '../utils/state-gen'
+import '../cards/index'
 import {Counter, statusEffect} from './status-effect'
+import { targetEntity } from '../components/query/effect'
+import { CARDS } from '../cards'
+import { Hermit } from '../cards/base/types'
+import { TypeT } from '../types/cards'
 
 const WeaknessEffect: Counter<CardComponent> = {
 	...statusEffect,
 	icon: 'weakness',
 	name: 'Weakness',
-	description: "This Hermit is weak to the opponent's active Hermit's type.",
+	description: "[weakType] currently has modified weakness to [strongType].",
 	counter: 3,
 	counterType: 'turns',
 	onApply(
@@ -19,17 +28,27 @@ const WeaknessEffect: Counter<CardComponent> = {
 		target: CardComponent,
 		observer: ObserverComponent,
 	) {
-		const {player} = target
+		const { player } = target
 
 		if (!effect.counter) effect.counter = this.counter
 
-		observer.subscribe(player.hooks.onTurnStart, () => {
+		const weakHermit = _game.components.find(
+			CardComponent,
+			query.card.rowEntity(player.activeRowEntity),
+			query.card.isHermit,
+		)
+
+		if (!weakHermit?.isHermit()) return null
+
+		const weakType = weakHermit.props.type
+
+		observer.subscribe(player.hooks.onTurnEnd, () => {
 			if (!effect.counter) return
 			effect.counter--
 
 			if (effect.counter === 0) effect.remove()
 		})
-
+		
 		observer.subscribe(player.hooks.beforeDefence, (attack) => {
 			if (!target.slot.inRow()) return
 			if (
