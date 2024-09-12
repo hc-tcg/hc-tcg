30e2d758c00f90d91a15f5cbaa387e26b92fed0f
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2024-09-12 00:36:42 -0400

    create failing tests

--- tests/game/beetlejhost-rare.test.ts
+++ tests/game/beetlejhost-rare.test.ts
@@ -1,11 +1,14 @@
 import {describe, expect, test} from '@jest/globals'
 import BeetlejhostRare from 'common/cards/alter-egos-iii/hermits/beetlejhost-rare'
+import BadOmen from 'common/cards/alter-egos/single-use/bad-omen'
 import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
 import GeminiTayCommon from 'common/cards/default/hermits/geminitay-common'
+import InvisibilityPotion from 'common/cards/default/single-use/invisibility-potion'
 import {RowComponent, StatusEffectComponent} from 'common/components'
 import query from 'common/components/query'
 import ChromaKeyedEffect from 'common/status-effects/chroma-keyed'
 import {
+	applyEffect,
 	attack,
 	changeActiveHermit,
 	endTurn,
@@ -157,6 +160,45 @@ describe('Test Beetlejhost Rare', () => {
 				},
 			},
 			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+	test('Test Jopacity with Invisibility tails', () => {
+		testGame(
+			{
+				playerOneDeck: [GeminiTayCommon, InvisibilityPotion],
+				playerTwoDeck: [BeetlejhostRare, BadOmen],
+				saga: function* (game) {
+					yield* playCardFromHand(game, GeminiTayCommon, 'hermit', 0)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, BeetlejhostRare, 'hermit', 0)
+					yield* playCardFromHand(game, BadOmen, 'single_use')
+					yield* applyEffect(game)
+
+					yield* attack(game, 'secondary')
+
+					expect(game.opponentPlayer.activeRow?.health).toBe(
+						GeminiTayCommon.health - BeetlejhostRare.secondary.damage,
+					)
+
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, InvisibilityPotion, 'single_use')
+					yield* applyEffect(game)
+
+					yield* endTurn(game)
+
+					yield* attack(game, 'secondary')
+
+					expect(game.opponentPlayer.activeRow?.health).toBe(
+						GeminiTayCommon.health -
+							BeetlejhostRare.secondary.damage -
+							(BeetlejhostRare.secondary.damage - 10) * 2,
+					)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true, forceCoinFlip: true},
 		)
 	})
 })
