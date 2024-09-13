a687309bcc39f2654e514dd06a02aa05b7ee0275
blockgoblin31 <57573828+blockgoblin31@users.noreply.github.com>
2024-09-13 09:14:18 -0500

    Add tests for a couple different older bugs (#889)
    Tests added:
    - Add test for gaslight where afk hermit was attacked but took no damage
    https://github.com/hc-tcg/hc-tcg/pull/834
    - Add test for thorns with effect card
    https://github.com/hc-tcg/hc-tcg/pull/779
     - Add test for armor stand(Two bugfixes in 0.8.4)
     - Add test for anvil drop(Bugfixed in 0.8.3)
    
    ---------
    
    Co-authored-by: tuc56 <57573828+tuc56@users.noreply.github.com>
    Co-authored-by: Lunarmagpie <Bambolambo0@gmail.com>

--- tests/game/skizzleman-rare.test.ts
+++ tests/game/skizzleman-rare.test.ts
@@ -1,5 +1,6 @@
 import {describe, expect, test} from '@jest/globals'
 import Anvil from 'common/cards/alter-egos/single-use/anvil'
+import IronArmor from 'common/cards/default/effects/iron-armor'
 import Thorns from 'common/cards/default/effects/thorns'
 import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
 import SkizzlemanRare from 'common/cards/season-x/hermits/skizzleman-rare'
@@ -74,6 +75,35 @@ describe('Test Skizzleman Rare', () => {
 							query.row.index(0),
 						)?.health,
 					).toBe(SkizzlemanRare.health - 20 /* Thorns damage */)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+	test("Gaslight doesn't trigger if the hermit takes no damage", () => {
+		testGame(
+			{
+				playerOneDeck: [EthosLabCommon, EthosLabCommon, IronArmor],
+				playerTwoDeck: [SkizzlemanRare, Anvil],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
+					yield* playCardFromHand(game, IronArmor, 'attach', 1)
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, SkizzlemanRare, 'hermit', 0)
+					// Use anvil to trigger attack the afk hermit for zero damage
+					yield* playCardFromHand(game, Anvil, 'single_use')
+					yield* attack(game, 'secondary')
+					yield* endTurn(game)
+
+					expect(
+						game.components.find(
+							RowComponent,
+							query.row.currentPlayer,
+							query.row.index(1),
+						)?.health,
+					).toBe(EthosLabCommon.health)
 				},
 			},
 			{startWithAllCards: true, noItemRequirements: true},
