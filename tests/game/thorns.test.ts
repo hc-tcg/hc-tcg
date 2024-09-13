4d963478eaaeb6f05ae717fb4bec551e0c60cdbb
tuc56 <57573828+tuc56@users.noreply.github.com>
2024-09-12 22:33:08 -0500

    Add tests for higher thorns levels

--- tests/game/thorns.test.ts
+++ tests/game/thorns.test.ts
@@ -1,40 +1,71 @@
-import {describe, expect, test} from "@jest/globals"
-import { attack, endTurn, playCardFromHand, testGame } from "./utils"
-import EthosLabCommon from "common/cards/default/hermits/ethoslab-common"
-import Thorns from "common/cards/default/effects/thorns"
-import IronSword from "common/cards/default/single-use/iron-sword"
-import { RowComponent } from "common/components"
-import query from "common/components/query"
+import {describe, expect, test} from '@jest/globals'
+import ThornsII from 'common/cards/alter-egos/effects/thorns_ii'
+import ThornsIII from 'common/cards/alter-egos/effects/thorns_iii'
+import Thorns from 'common/cards/default/effects/thorns'
+import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
+import IronSword from 'common/cards/default/single-use/iron-sword'
+import {RowComponent} from 'common/components'
+import query from 'common/components/query'
+import {attack, endTurn, playCardFromHand, testGame} from './utils'
 
-describe("Test Thorns", () => {
-    test("Test thorns damage after attacks with effect card only", () => {
-        testGame(
-            {
-                playerOneDeck: [EthosLabCommon, Thorns],
-                playerTwoDeck: [EthosLabCommon, IronSword],
-                saga: function* (game) {
-                    yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
-                    yield* playCardFromHand(game, Thorns, 'attach', 0)
-                    yield* endTurn(game)
+let thornsMap = {
+	Thorns: {
+		card: Thorns,
+		damage: 20,
+	},
+	'Thorns II': {
+		card: ThornsII,
+		damage: 30,
+	},
+	'Thorns III': {
+		card: ThornsIII,
+		damage: 40,
+	},
+}
 
-                    yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
-                    yield* playCardFromHand(game, IronSword, 'single_use')
-                    yield* attack(game, 'single-use')
+type ThornsType = keyof typeof thornsMap
 
-                    expect(game.components.find(
-                        RowComponent,
-                        query.row.currentPlayer,
-                        query.row.index(0)
-                    )?.health).toBe(EthosLabCommon.health - 20 /*Thorns damage*/)
+function testThorns(thorns: ThornsType) {
+	test('Test ' + thorns + ' damage after attacks with effect card only', () => {
+		testGame(
+			{
+				playerOneDeck: [EthosLabCommon, thornsMap[thorns].card],
+				playerTwoDeck: [EthosLabCommon, IronSword],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, thornsMap[thorns].card, 'attach', 0)
+					yield* endTurn(game)
 
-                    expect(game.components.find(
-                        RowComponent,
-                        query.row.opponentPlayer,
-                        query.row.index(0)
-                    )?.health).toBe(EthosLabCommon.health - 20 /*Iron sword damage*/)
-                }
-            },
-            {startWithAllCards: true, noItemRequirements: true}
-        )
-    })
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, IronSword, 'single_use')
+					yield* attack(game, 'single-use')
+
+					expect(
+						game.components.find(
+							RowComponent,
+							query.row.currentPlayer,
+							query.row.index(0),
+						)?.health,
+					).toBe(
+						EthosLabCommon.health - thornsMap[thorns].damage /*Thorns damage*/,
+					)
+
+					expect(
+						game.components.find(
+							RowComponent,
+							query.row.opponentPlayer,
+							query.row.index(0),
+						)?.health,
+					).toBe(EthosLabCommon.health - 20 /*Iron sword damage*/)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+}
+
+describe('Test Thorns', () => {
+	testThorns('Thorns')
+	testThorns('Thorns II')
+	testThorns('Thorns III')
 })
