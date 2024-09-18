5f205979929d6c4bb8e9fb202629473b948c6a58
tuc56 <57573828+tuc56@users.noreply.github.com>
2024-09-18 14:28:05 -0500

    Add test for lightning rod

---
+++ tests/game/effects/lightning-rod.test.ts
@@ -0,0 +1,69 @@
+import {describe, expect, test} from '@jest/globals'
+import LightningRod from 'common/cards/alter-egos/effects/lightning-rod'
+import EthosLabCommon from 'common/cards/default/hermits/ethoslab-common'
+import Bow from 'common/cards/default/single-use/bow'
+import {RowComponent} from 'common/components'
+import query from 'common/components/query'
+import {attack, endTurn, pick, playCardFromHand, testGame} from '../utils'
+
+describe('Test Lightning Rod', () => {
+	test('Test redirecting multiple attacks at once', () => {
+		testGame(
+			{
+				playerOneDeck: [
+					EthosLabCommon,
+					EthosLabCommon,
+					EthosLabCommon,
+					LightningRod,
+				],
+				playerTwoDeck: [EthosLabCommon, Bow],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 2)
+					yield* playCardFromHand(game, LightningRod, 'attach', 2)
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, Bow, 'single_use')
+					yield* attack(game, 'primary')
+					yield* pick(
+						game,
+						query.slot.opponent,
+						query.slot.hermit,
+						query.slot.rowIndex(1),
+					)
+
+					expect(
+						game.components.find(
+							RowComponent,
+							query.row.opponentPlayer,
+							query.row.index(0),
+						)?.health,
+					).toBe(EthosLabCommon.health)
+
+					expect(
+						game.components.find(
+							RowComponent,
+							query.row.opponentPlayer,
+							query.row.index(1),
+						)?.health,
+					).toBe(EthosLabCommon.health)
+
+					expect(
+						game.components.find(
+							RowComponent,
+							query.row.opponentPlayer,
+							query.row.index(2),
+						)?.health,
+					).toBe(
+						EthosLabCommon.health -
+							EthosLabCommon.primary.damage -
+							40 /*bow damage*/,
+					)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+})
