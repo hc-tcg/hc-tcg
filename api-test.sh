49e6b31cedca5e690f266e820b9cf2f57426ca38
Lunarmagpie <bambolambo0@gmail.com>
2024-09-23 10:42:38 -0400

    API test draft

---
+++ api-test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+#
+# Tests for the HC-TCG API. Depends on `curl` and `jq`.
+#
+
+set -o pipefail
+
+PORT=9000
+kill -9 $(lsof -t -i:$PORT) || true
+
+cleanup() {
+  pkill -f "npm run server:dev"
+}
+
+test_api_cards() {
+	WILD_ITEM=$(curl http://localhost:9000/api/cards 2> /dev/null | jq '.[] | select(.id == "item_any_common")')
+	test -z "$(echo $WILD_ITEM | jq 'select(.tokens == 1)')"
+}
+
+test_card_images_exist() {
+	TEST_HERMIT=$(curl http://localhost:9000/api/cards 2> /dev/null | jq '.[] | select(.id == "ethoslab_common")')
+	IMAGE=$(echo $TEST_HERMIT | jq -r .image)
+	BACKGROUND=$(echo $TEST_HERMIT | jq -r .background)
+
+	curl -f $IMAGE &> /dev/null
+	curl -f $BACKGROUND &> /dev/null
+}
+
+trap cleanup EXIT
+
+npm run server:dev &
+
+# Give the server some time to start
+sleep 2
+
+test_api_cards &
+test_card_images_exist
+
---
+++ tests/api/api.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from '@jest/globals'
+
+describe('Test API Endpoints', () => {
+	test('Test /api/cards', () => {
+
+	})
+})
+
