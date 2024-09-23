a6ebd38ecb0f3af684407f01ca728c47db9bcfd4
Lunarmagpie <bambolambo0@gmail.com>
2024-09-23 13:53:55 -0400

    More verification

--- tests/test-api.sh
+++ tests/test-api.sh
@@ -12,27 +12,39 @@ cleanup() {
   pkill -f "npm run server:dev"
 }
 
-test_api_cards() {
-	WILD_ITEM=$(curl http://localhost:9000/api/cards 2> /dev/null | jq '.[] | select(.id == "item_any_common")')
-	test -z "$(echo $WILD_ITEM | jq 'select(.tokens == 1)')"
-}
-
 test_card_images_exist() {
-	TEST_HERMIT=$(curl http://localhost:9000/api/cards 2> /dev/null | jq '.[] | select(.id == "ethoslab_common")')
-	IMAGE=$(echo $TEST_HERMIT | jq -r .image)
-	BACKGROUND=$(echo $TEST_HERMIT | jq -r .background)
+	test_hermit=$(curl http://localhost:9000/api/cards | jq '.[] | select(.id == "ethoslab_common")')
+	image=$(echo $test_hermit | jq -r .image)
+	background=$(echo $test_hermit | jq -r .background)
+	hermit_image_output=$(mktemp)
+	hermit_background_output=$(mktemp)
+	curl -f $image -o $hermit_image_output
+	curl -f $background -o $hermit_background_output
+	test -s $hermit_image_output
+	test -s $hermit_background_output
 
-	curl -f $IMAGE &> /dev/null
-	curl -f $BACKGROUND &> /dev/null
+	test_item=$(curl http://localhost:9000/api/cards | jq '.[] | select(.id == "item_builder_common")')
+	image=$(echo $test_item | jq -r .image)
+	item_image_output=$(mktemp)
+	curl -f $image -o $item_image_output
+	test -s "$item_image_output"
+
+	test_effect=$(curl http://localhost:9000/api/cards | jq '.[] | select(.id == "bed")')
+	image=$(echo $test_effect | jq -r .image)
+	effect_image_output=$(mktemp)
+	curl -f $image -o $effect_image_output
+	test -s "$effect_image_output"
 }
 
 trap cleanup EXIT
 
-npm run server:dev
+npm run server:dev &
 
 # Give the server some time to start
 sleep 2
 
-test_api_cards &
+echo "Running `test_card_images_exist`"
 test_card_images_exist
+
+cleanup
 
