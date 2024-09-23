12f5ed90f3e56d4624598b91109dc5feb345b892
Lunarmagpie <bambolambo0@gmail.com>
2024-09-23 16:06:17 -0400

    use literal strings

--- tests/test-api.sh
+++ tests/test-api.sh
@@ -55,9 +55,9 @@ while [[ -z $(cat $output_file | grep "Server listening on port") ]]; do
 	sleep .1
 done
 
-echo "Server started! Running tests."
+echo 'Server started! Running tests.'
 
-echo "Running `test_card_images_exist`"
+echo 'Running `test_card_images_exist`'
 test_card_images_exist
-echo "Running `test_card_token_costs`"
+echo 'Running `test_card_token_costs`'
 test_card_token_costs
