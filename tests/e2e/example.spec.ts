73c5180567923660fa1ea399d5f5a25f81da9322
Lunarmagpie <bambolambo0@gmail.com>
2024-09-23 23:40:35 -0400

    check

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -12,7 +12,8 @@ test('is still connected after reload', async ({page}) => {
 	await page.getByPlaceholder(' ').click()
 	await page.getByPlaceholder(' ').fill('Test Player')
 	await page.getByPlaceholder(' ').press('Enter')
-	await page.reload()
 	// We expect that the player is still logged in because the session was saved properly.
+	await page.reload()
+	// We check that the big HC-TCG logo is there.
 	await expect(page.locator('._logo_6trod_2 > img')).toBeVisible()
 })
