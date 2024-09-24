b7078783c26cc009212901a2b983eae3e5d27685
Lunarmagpie <bambolambo0@gmail.com>
2024-09-24 01:12:18 -0400

    npm run start

--- playwright.config.ts
+++ playwright.config.ts
@@ -66,7 +66,7 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: 'npm run server',
+		command: 'npm run start',
 		url: 'http://localhost:9000/',
 		reuseExistingServer: !process.env.CI,
 	},
