fd57c5cb917dcfc4b66222f6b9f479f5677b983b
Lunarmagpie <bambolambo0@gmail.com>
2024-09-23 23:18:55 -0400

    changes

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,18 +1,9 @@
 import {expect, test} from '@playwright/test'
 
-test('has title', async ({page}) => {
-	await page.goto('https://playwright.dev/')
+test('has correct title', async ({page}) => {
+	await page.goto('http://localhost:9000/')
 
 	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle(/Playwright/)
+	await expect(page).toHaveTitle(/Hermitcraft TCG/)
 })
 
-test('get started link', async ({page}) => {
-	await page.goto('https://playwright.dev/')
-
-	// Click the get started link.
-	await page.getByRole('link', {name: 'Get started'}).click()
-
-	// Expects page to have a heading with the name of Installation.
-	await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible()
-})
