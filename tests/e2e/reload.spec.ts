c95d0776d06485f403258235d7fb81ffa1beb37f
Lunarmagpie <bambolambo0@gmail.com>
2024-09-24 01:19:48 -0400

    remove click that isn't needed

--- tests/e2e/reload.spec.ts
+++ tests/e2e/reload.spec.ts
@@ -3,7 +3,6 @@ import {expect, test} from '@playwright/test'
 test('is still connected after reload', async ({page}) => {
   await page.goto('/');
 
-	await page.getByPlaceholder(' ').click()
 	await page.getByPlaceholder(' ').fill('Test Player')
 	await page.getByPlaceholder(' ').press('Enter')
 
