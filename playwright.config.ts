2f363b25a49df68599e048526ec417269967238b
Lunarmagpie <bambolambo0@gmail.com>
2024-09-24 02:30:13 -0400

    increase timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	retries: process.env.CI ? 0 : 0,
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -31,6 +31,8 @@ export default defineConfig({
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
 	},
+
+	timeout: 120 * 1000,
 
 	/* Configure projects for major browsers */
 	projects: [
