bcde85c6b498ffa57a615766efdbbe726f6fb1f3
Lunarmagpie <bambolambo0@gmail.com>
2024-09-24 00:48:11 -0400

    format

--- playwright.config.ts
+++ playwright.config.ts
@@ -66,8 +66,8 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-	  command: 'npm run server',
-	  url: 'http://localhost:9000',
-	  reuseExistingServer: !process.env.CI,
+		command: 'npm run server',
+		url: 'http://localhost:9000',
+		reuseExistingServer: !process.env.CI,
 	},
 })
