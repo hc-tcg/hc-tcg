6fa5eb53bfdeaf12c01e395aac5cc6aa783f039a
Lunarmagpie <bambolambo0@gmail.com>
2024-09-23 23:39:36 -0400

    changes

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -6,3 +6,13 @@ test('has correct title', async ({page}) => {
 	// Expect a title "to contain" a substring.
 	await expect(page).toHaveTitle(/Hermitcraft TCG/)
 })
+
+test('is still connected after reload', async ({page}) => {
+	await page.goto('http://localhost:9000/')
+	await page.getByPlaceholder(' ').click()
+	await page.getByPlaceholder(' ').fill('Test Player')
+	await page.getByPlaceholder(' ').press('Enter')
+	await page.reload()
+	// We expect that the player is still logged in because the session was saved properly.
+	await expect(page.locator('._logo_6trod_2 > img')).toBeVisible()
+})
