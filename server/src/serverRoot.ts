4a4f8dc18220f3750ca83209b5dc7f735d03ec64
Lunarmagpie <bambolambo0@gmail.com>
2024-09-24 23:26:01 -0400

    firebase removal

--- server/src/serverRoot.ts
+++ server/src/serverRoot.ts
@@ -1,15 +1,6 @@
 import {RootModel} from 'common/models/root-model'
-import {plugins} from './plugins'
 
-// Create root and register plugins
-
-/** The root of the server. */
 const root = new RootModel()
 
-// initialize plugins
-plugins.forEach((plugin) => {
-	const result = plugin.register(root)
-	if (result) console.log('plugin registered: ' + plugin.id)
-})
-
 export default root
+
