d110d886543c81d7a7cbeebbf07ec0ae9c30ada9
Lunarmagpie <bambolambo0@gmail.com>
2024-09-25 10:15:01 -0400

    fixes

--- tests/e2e/reload.spec.ts
+++ tests/e2e/reload.spec.ts
@@ -28,24 +28,28 @@ test('player does not stay in queue after reloading the page', async ({
 	await page.getByPlaceholder(' ').fill('Test Player')
 	await page.getByPlaceholder(' ').press('Enter')
 
-	await page.waitForFunction(() => global.getState().session.connected)
+	await page.waitForFunction(() => {
+		console.log(global.getState().session.connected)
+		return global.getState().session.connected
+	})
 
 	let playerId = page.evaluate(() => global.getState().session.playerId)
 	await page.getByText('Public Game').click()
 
-	let game = await (
+	let queue= await (
 		await fetch('http://localhost:9000/debug/root-state/queue')
 	).json()
 
-	expect(game).toContain(playerId)
+	expect(queue).toContain(playerId)
 
 	await page.reload()
 	await page.waitForFunction(() => global.getState().session.connected)
 
 	// We should be automatically removed from the queue.
-	game = await (
+	queue= await (
 		await fetch('http://localhost:9000/debug/root-state/queue')
 	).json()
-	expect(game).not.toContain(playerId)
+	expect(queue).not.toContain(playerId)
+	expect(queue).not.toContain(playerId)
 	expect(page.evaluate(() => global.getState().matching.status)).toBe(null)
 })
