faed4fcfd9a2addb8693b5c1a2eab4884a682ab6
Lunarmagpie <bambolambo0@gmail.com>
2024-09-26 12:52:40 -0400

    Cancel matches correctly

--- server/src/api/games.ts
+++ server/src/api/games.ts
@@ -1,4 +1,6 @@
+import {serverMessages} from 'common/socket-messages/server-messages'
 import root from 'serverRoot'
+import {broadcast} from 'utils/comm'
 
 /** Create a hc-tcg game through the HC-TCG API.
  * API games automatically time out after 5 minutes if it is not started.
@@ -30,6 +32,13 @@ export function cancelApiGame(code: string) {
 	if (!game) {
 		return {
 			error: 'Could not find API code',
+		}
+	}
+
+	if (game.playerId) {
+		const player = root.players[game.playerId]
+		if (player) {
+			broadcast([player], {type: serverMessages.PRIVATE_GAME_CANCELLED})
 		}
 	}
 
