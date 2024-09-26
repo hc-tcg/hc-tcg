d3b83e4d9bf414cf201a643bd86c65bb27f3e1e9
Lunarmagpie <bambolambo0@gmail.com>
2024-09-26 13:24:00 -0400

    Add test

--- tests/e2e/private-queue.spec.ts
+++ tests/e2e/private-queue.spec.ts
@@ -1,9 +1,31 @@
 import {expect, test} from '@playwright/test'
 
-test('Private queue is exited when API game is cancelled', () => {
+test('Private queue is exited when API game is cancelled', async ({page}) => {
+	await page.goto('/')
 
+	let privateGame = await (
+		await fetch('http://localhost:9000/api/games/create')
+	).json()
 
+	let gameCode = privateGame.gameCode
+	let apiSecret = privateGame.apiSecret
 
+	await page.getByPlaceholder(' ').fill('Test Player')
+	await page.getByPlaceholder(' ').press('Enter')
+	await page.getByText('Close').click()
+	await page.getByText('Join Private Game').click()
+	await page.getByLabel('Enter game or spectator code:').fill(gameCode)
+	await page.getByLabel('Enter game or spectator code:').fill('Enter')
 
+	expect(await page.isVisible('Waiting'))
+
+	await fetch('http://localhost:9000/api/games/delete', {
+		method: 'DELETE',
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify({
+			code: apiSecret,
+		}),
+	})
+
+	expect(await page.isVisible('Public Game'))
 })
-
