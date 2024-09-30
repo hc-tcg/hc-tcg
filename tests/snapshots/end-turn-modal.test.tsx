408432cd6f271a7642ff3790abf013e953bdc21b
Lunarmagpie <bambolambo0@gmail.com>
2024-09-30 00:38:07 -0400

    I HATE RADIX

--- tests/snapshots/end-turn-modal.test.tsx
+++ tests/snapshots/end-turn-modal.test.tsx
@@ -1,56 +1,68 @@
 import {describe, expect, test} from '@jest/globals'
-
+import * as ReactTestRenderer from 'react-test-renderer'
 import {EndTurnModalInner} from 'client/app/game/modals/end-turn-modal'
 
 describe('Test End Turn Modal', () => {
 	test('No Actions', () => {
-		expect(<EndTurnModalInner availableActions={[]} />).toMatchSnapshot()
-	})
-	test('All play card actions', () => {
 		expect(
-			<EndTurnModalInner
-				availableActions={[
-					'PLAY_HERMIT_CARD',
-					'PLAY_ITEM_CARD',
-					'PLAY_SINGLE_USE_CARD',
-					'PLAY_EFFECT_CARD',
-					'CHANGE_ACTIVE_HERMIT',
-				]}
-			/>,
+			ReactTestRenderer.create(
+				<EndTurnModalInner availableActions={[]} />,
+			).toJSON(),
 		).toMatchSnapshot()
 	})
 	test('All play card actions', () => {
 		expect(
-			<EndTurnModalInner
-				availableActions={[
-					'PRIMARY_ATTACK',
-					'SECONDARY_ATTACK',
-					'SINGLE_USE_ATTACK',
-				]}
-			/>,
+			ReactTestRenderer.create(
+				<EndTurnModalInner
+					availableActions={[
+						'PLAY_HERMIT_CARD',
+						'PLAY_ITEM_CARD',
+						'PLAY_SINGLE_USE_CARD',
+						'PLAY_EFFECT_CARD',
+						'CHANGE_ACTIVE_HERMIT',
+					]}
+				/>,
+			).toJSON(),
+		).toMatchSnapshot()
+	})
+	test('All play card actions', () => {
+		expect(
+			ReactTestRenderer.create(
+				<EndTurnModalInner
+					availableActions={[
+						'PRIMARY_ATTACK',
+						'SECONDARY_ATTACK',
+						'SINGLE_USE_ATTACK',
+					]}
+				/>,
+			).toJSON(),
 		).toMatchSnapshot()
 	})
 	test('All play card actions and attacks', () => {
 		expect(
-			<EndTurnModalInner
-				availableActions={[
-					'PLAY_HERMIT_CARD',
-					'PLAY_ITEM_CARD',
-					'PLAY_SINGLE_USE_CARD',
-					'PLAY_EFFECT_CARD',
-					'CHANGE_ACTIVE_HERMIT',
-					'PRIMARY_ATTACK',
-					'SECONDARY_ATTACK',
-					'SINGLE_USE_ATTACK',
-				]}
-			/>,
+			ReactTestRenderer.create(
+				<EndTurnModalInner
+					availableActions={[
+						'PLAY_HERMIT_CARD',
+						'PLAY_ITEM_CARD',
+						'PLAY_SINGLE_USE_CARD',
+						'PLAY_EFFECT_CARD',
+						'CHANGE_ACTIVE_HERMIT',
+						'PRIMARY_ATTACK',
+						'SECONDARY_ATTACK',
+						'SINGLE_USE_ATTACK',
+					]}
+				/>,
+			).toJSON(),
 		).toMatchSnapshot()
 	})
 	test('Waiting actions', () => {
 		expect(
-			<EndTurnModalInner
-				availableActions={['WAIT_FOR_TURN', 'WAIT_FOR_OPPONENT_ACTION']}
-			/>,
+			ReactTestRenderer.create(
+				<EndTurnModalInner
+					availableActions={['WAIT_FOR_TURN', 'WAIT_FOR_OPPONENT_ACTION']}
+				/>,
+			).toJSON(),
 		).toMatchSnapshot()
 	})
 })
