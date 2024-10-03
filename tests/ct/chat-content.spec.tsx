821c81a23794244911d927e4b98a48d678311c1d
Lunarmagpie <bambolambo0@gmail.com>
2024-10-02 20:46:52 -0400

    Update snapshots

--- tests/ct/chat-content.spec.tsx
+++ tests/ct/chat-content.spec.tsx
@@ -1,23 +1,21 @@
 import {expect, test} from '@playwright/experimental-ct-react'
 import {ChatContent, ChatMessageDisplay} from 'client/app/game/chat/chat'
-import {formatText} from 'common/utils/formatting'
+import {formatText, LineNode} from 'common/utils/formatting'
 
-test('Empty Chat Messages Display Properly', async ({mount}) => {
-	let messages: Array<ChatMessageDisplay> = []
-	const component = await mount(
-		<ChatContent
-			chatMessages={messages}
-			showLog={true}
-			isSpectating={false}
-			profanityFilterEnabled={false}
-			playerNames={['Player One', 'Player Two']}
-		/>,
-	)
-	await expect(component).toHaveScreenshot()
-})
-
-test('A message', async ({mount}) => {
+test('Players and spectators view messages correctly.', async ({mount}) => {
 	let messages = [
+		{
+			message: LineNode(),
+			isBattleLogMessage: false,
+			sender: 'playerOne',
+			createdAt: 0,
+		},
+		{
+			message: LineNode(),
+			isBattleLogMessage: false,
+			sender: 'playerTwo',
+			createdAt: 0,
+		},
 		{
 			message: formatText('$sSpectator$ Hello, I am a spectator.'),
 			isBattleLogMessage: false,
@@ -48,4 +46,17 @@ test('A message', async ({mount}) => {
 		/>,
 	)
 	await expect(component).toHaveScreenshot()
+	await component.unmount()
+
+	const component2 = await mount(
+		<ChatContent
+			chatMessages={messages}
+			showLog={true}
+			isSpectating={true}
+			profanityFilterEnabled={false}
+			playerNames={['Player One', 'Player Two']}
+		/>,
+	)
+	await expect(component2).toHaveScreenshot()
+	await component2.unmount()
 })
