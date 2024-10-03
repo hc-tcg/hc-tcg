2180fc180d29c90b251d9a8080bc1231a019fa4f
Lunarmagpie <bambolambo0@gmail.com>
2024-10-02 21:29:05 -0400

    Set consistent timezone

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -5,30 +5,18 @@ import {defineConfig, devices} from '@playwright/experimental-ct-react'
 // I am sorry
 const __dirname = execSync('git rev-parse --show-toplevel').toString().trim()
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
 	testDir: './tests/ct/',
-	/* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
 	snapshotDir: './tests/ct/__snapshots__',
-	/* Maximum time one test can run for. */
 	timeout: 10 * 1000,
-	/* Run tests in files in parallel */
 	fullyParallel: true,
-	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
-	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
-	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
-	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: 'html',
-	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
-		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: process.env.CI ? 'on-first-retry' : 'on',
-		/* Port to use for Playwright component endpoint. */
+		timezoneId: 'America/New_York',
 		ctPort: 3100,
 		ctViteConfig: {
 			publicDir: path.resolve(__dirname, 'client/public/'),
