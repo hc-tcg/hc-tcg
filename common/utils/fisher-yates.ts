2b4405cca7d927e5b04365395b6abad126b49a2c
Lunarmagpie <bambolambo0@gmail.com>
2024-10-08 15:42:21 -0400

    Fix it for real

--- common/utils/fisher-yates.ts
+++ common/utils/fisher-yates.ts
@@ -3,8 +3,8 @@ export function fisherYatesShuffle<T>(array: Array<T>) {
 	for (let i = array.length; i >= 1; i--) {
 		let j = Math.floor(Math.random() * i)
 		let tmp = array[j]
-		array[j] = array[i]
-		array[i] = tmp
+		array[j] = array[i - 1]
+		array[i - 1] = tmp
 	}
 	return array
 }
