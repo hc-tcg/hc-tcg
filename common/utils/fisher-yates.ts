081cc9b27276d9aca7f87f48817a8a5d5f0347c4
Lunarmagpie <bambolambo0@gmail.com>
2024-10-08 15:28:56 -0400

    fisher yates

---
+++ common/utils/fisher-yates.ts
@@ -0,0 +1,9 @@
+/** In place implementation of the Fisher-Yates shuffle */
+export function fisherYatesShuffle<T>(array: Array<T>) {
+	for (let i = array.length; i >= 1; i--) {
+		let j = Math.floor(Math.random() * (i + 1))
+		let tmp = array[j]
+		array[j] = array[i]
+		array[i] = tmp
+	}
+}
