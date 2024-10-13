e11703cfe1a6abc7efd31210811cd46510cf8b6e
Lunarmagpie <bambolambo0@gmail.com>
2024-10-12 23:21:29 -0400

    Updating tests

--- tests/ct/end-game-overlay.spec.tsx
+++ tests/ct/end-game-overlay.spec.tsx
@@ -2,7 +2,7 @@ import {expect, test} from '@playwright/experimental-ct-react'
 import EndGameOverlay from 'client/app/game/end-game-overlay'
 import {PlayerEntity} from 'common/entities'
 
-test('You won by killing all hermits!', async ({mount, page}) => {
+test('You won by killing all hermits!', async ({mount}) => {
 	let entity = 'playerOne' as PlayerEntity
 
 	const component = await mount(
@@ -18,6 +18,46 @@ test('You won by killing all hermits!', async ({mount, page}) => {
 			}}
 		/>,
 	)
-	
+
+	await expect(component).toHaveScreenshot()
+})
+
+test('You lost because your hermits were killed.', async ({mount}) => {
+	let playerOneEntity = 'playerOne' as PlayerEntity
+	let playerTwoEntity = 'playerTwo' as PlayerEntity
+
+	const component = await mount(
+		<EndGameOverlay
+			nameOfWinner={null}
+			outcome={{
+				winner: playerOneEntity,
+				victoryReason: 'no-hermits-on-board',
+			}}
+			viewer={{
+				type: 'player',
+				entity: playerTwoEntity,
+			}}
+		/>,
+	)
+
+	await expect(component).toHaveScreenshot()
+})
+
+test('Viewing as spectator shows victory.', async ({mount}) => {
+	let playerOneEntity = 'playerOne' as PlayerEntity
+
+	const component = await mount(
+		<EndGameOverlay
+			nameOfWinner={null}
+			outcome={{
+				winner: playerOneEntity,
+				victoryReason: 'no-hermits-on-board',
+			}}
+			viewer={{
+				type: 'spectator',
+			}}
+		/>,
+	)
+
 	await expect(component).toHaveScreenshot()
 })
