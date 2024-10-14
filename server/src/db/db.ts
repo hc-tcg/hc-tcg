f1b5e65cf7f6fc05c58819b1f8f8ba5645547630
Lunarmagpie <bambolambo0@gmail.com>
2024-10-14 10:40:06 -0400

    Get it basically working

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -3,6 +3,7 @@ import {Card} from 'common/cards/base/types'
 import {GameEndOutcomeT} from 'common/types/game-state'
 import {Tag} from 'common/types/deck'
 const {Pool} = pg
+import {Deck, User, UserWithoutSecret, Stats} from 'common/types/database'
 
 export type DatabaseResult<T = undefined> =
 	| {
@@ -13,36 +14,6 @@ export type DatabaseResult<T = undefined> =
 			type: 'failure'
 			reason?: string
 	  }
-
-export type User = {
-	uuid: string
-	secret: string
-	username: string
-	minecraftName: string | null
-}
-
-export type UserWithoutSecret = {
-	uuid: string
-	username: string
-	minecraftName: string | null
-}
-
-export type Deck = {
-	code: string
-	name: string
-	icon: string
-	tags: Array<string>
-	cards: Array<Card>
-}
-
-export type Stats = {
-	gamesPlayed: number
-	wins: number
-	losses: number
-	ties: number
-	forfeitWins: number
-	forfeitLosses: number
-}
 
 export class Database {
 	public pool: pg.Pool
