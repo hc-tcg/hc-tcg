a03d0710adf9a66627b0b9c19b801a9d0d6eb8f0
Lunarmagpie <Bambolabmo0@gmail.com>
2024-10-23 09:40:23 -0400

    Move things around

--- common/utils/instanced-value.ts
+++ common/utils/instanced-value.ts
@@ -1,17 +1,21 @@
 import {CardComponent} from '../components'
+import {StatusEffectComponent} from '../components'
 import {DefaultDictionary} from '../types/game-state'
 
 /** Type that allows multiple functions in a card to share values. */
-export class InstancedValue<T> extends DefaultDictionary<CardComponent, T> {
-	public set(component: CardComponent, value: T) {
+export class InstancedValue<T> extends DefaultDictionary<
+	CardComponent | StatusEffectComponent,
+	T
+> {
+	public set(component: CardComponent | StatusEffectComponent, value: T) {
 		this.setValue(component.entity, value)
 	}
 
-	public get(component: CardComponent): T {
+	public get(component: CardComponent | StatusEffectComponent): T {
 		return this.getValue(component.entity)
 	}
 
-	public clear(component: CardComponent) {
+	public clear(component: CardComponent | StatusEffectComponent) {
 		this.clearValue(component.entity)
 	}
 }
