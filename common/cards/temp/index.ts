df3d4af155fac95389dc2623231629f44b4dda3b
Rvtar <106639908+Rvtar@users.noreply.github.com>
2024-10-26 00:31:02 -0400

    Create index.ts

---
+++ common/cards/temp/index.ts
@@ -0,0 +1,8 @@
+import { Card } from '../base/types'
+import TinFoilChefMythic from './tinfoilchef-mythic'
+
+const hermitCardClasses: Array<Card> = [
+	TinFoilChefMythic,
+]
+
+export default hermitCardClasses
