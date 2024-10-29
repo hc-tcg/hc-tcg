24c86b957cec1625c2f234eb547910f6ef9a9e29
Sense101 <personal.sense101@gmail.com>
2024-10-29 13:07:50 +0000

    Add new features and component-style structure to modal

--- client/src/components/modal/modal.tsx
+++ client/src/components/modal/modal.tsx
@@ -1,23 +1,39 @@
-import cn from 'classnames'
-import React, {ReactNode, useEffect, useRef} from 'react'
+import {ReactNode, useEffect, useRef} from 'react'
 import css from './modal.module.scss'
+import cn from 'classnames'
 
 type Props = {
-	children: React.ReactNode
+	children: ReactNode
 	closeModal: () => void
-	title?: string
-	centered?: boolean
-	showCloseButton?: boolean
+	title: string
+	/** If true, the modal will not have a close button in the corner that can be clicked. */
+	disableCloseButton?: boolean
+	/** If true, the modal will not close when the ESC button is pressed. */
+	disableCloseOnEsc?: boolean
+	/** If true, the modal will not close when the overlay around it is clicked. */
+	disableCloseOnOverlayClick?: boolean
+	/** If true, The close button will be hidden and the user will not be able to close the modal in any way. Overrides `disableCloseButton`, `disableCloseOnEsc`, and `disableCloseOnOverlayClick` to `true`. */
+	disableUserClose?: boolean
 }
 
 function Modal({
 	children,
 	closeModal,
 	title,
-	centered,
-	showCloseButton = true,
+	disableCloseButton = false,
+	disableCloseOnEsc = false,
+	disableCloseOnOverlayClick = false,
+	disableUserClose = false,
 }: Props) {
 	const childrenRef = useRef(null)
+
+	if (disableUserClose) {
+		disableCloseButton = true
+		disableCloseOnEsc = true
+		disableCloseOnOverlayClick = true
+	}
+
+	// FOCUS TRAPPING
 
 	let focusableModalElements: any = []
 	let firstElement: any = null
@@ -35,13 +51,15 @@ function Modal({
 		lastElement = focusableModalElements[focusableModalElements.length - 1]
 	}, [childrenRef])
 
-	function handleKeys(e: any) {
-		if (e.key === 'Escape') {
+	// KEY HANDLING
+
+	function handleKeys(ev: KeyboardEvent) {
+		if (!disableCloseOnEsc && ev.key === 'Escape') {
 			closeModal()
 		}
 
 		// https://dev.to/mohitkyadav/how-to-trap-focus-in-react-3in8
-		if (e.key === 'Tab') {
+		if (ev.key === 'Tab') {
 			if (
 				![...focusableModalElements.values()].includes(
 					document.activeElement,
@@ -50,19 +68,19 @@ function Modal({
 			) {
 				lastElement.focus()
 			} else if (
-				!e.shiftKey &&
+				!ev.shiftKey &&
 				document.activeElement === lastElement &&
 				firstElement
 			) {
 				firstElement.focus()
-				e.preventDefault()
+				ev.preventDefault()
 			} else if (
-				e.shiftKey &&
+				ev.shiftKey &&
 				document.activeElement === firstElement &&
 				lastElement
 			) {
 				lastElement.focus()
-				e.preventDefault()
+				ev.preventDefault()
 			}
 		}
 	}
@@ -76,12 +94,15 @@ function Modal({
 
 	return (
 		<>
-			<div className={css.overlay} />
-			<div className={cn(css.modal, {[css.center]: centered})}>
+			<div
+				className={css.overlay}
+				onClick={!disableCloseOnOverlayClick ? closeModal : undefined}
+			/>
+			<div className={css.modal}>
 				<div className={css.title}>
 					{title && <span>{title}</span>}
 					{/* When tabbing around it is more convient to click the buttons only */}
-					{showCloseButton && (
+					{!disableCloseButton && (
 						<button className={css.close} tabIndex={-1} onClick={closeModal}>
 							<img src="/images/CloseX.svg" alt="close" />
 						</button>
@@ -93,19 +114,51 @@ function Modal({
 	)
 }
 
-type ETNProps = {
-	icon: '!' | 'i'
+type ComponentProps = {
+	className?: string
 	children: ReactNode
 }
 
-export const ModalNotice = ({icon, children}: ETNProps) => {
+type NoticeProps = ComponentProps & {
+	icon: '!' | 'i'
+}
+
+/** Pre-styled notice component for modals */
+Modal.Notice = ({className, children, icon}: NoticeProps) => {
+	const childrenRef = useRef(null)
 	return (
 		<div className={css.notice}>
-			<span className={css.noticeIcon}>{icon}</span>
-			<p>{children}</p>
+			<span className={cn(css.noticeIcon, className)}>{icon}</span>
+			<p ref={childrenRef}>{children}</p>
+		</div>
+	)
+}
+/** Pre-styled description component for modals */
+Modal.Description = ({className, children}: ComponentProps) => {
+	const childrenRef = useRef(null)
+	return (
+		<div className={cn(css.description, className)} ref={childrenRef}>
+			{children}
 		</div>
 	)
 }
 
-Modal.Notice = ModalNotice
+type OptionsProps = ComponentProps & {
+	/** Stretch buttons to fill available width. If this is not enabled each button is limited to 50% width. */
+	fillSpace?: boolean
+}
+
+/** Pre-styled options component for modals */
+// @TODO Make this a proper component with no need for children
+Modal.Options = ({className, children, fillSpace}: OptionsProps) => {
+	const childrenRef = useRef(null)
+	return (
+		<div
+			className={cn(css.options, fillSpace && css.fillSpace, className)}
+			ref={childrenRef}
+		>
+			{children}
+		</div>
+	)
+}
 export default Modal
