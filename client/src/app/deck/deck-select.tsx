f78a4f3b4edba44a5032ab4620630723c6372251
Lunarmagpie <Bambolabmo0@gmail.com>
2024-11-01 12:13:26 -0400

    format

--- client/src/app/deck/deck-select.tsx
+++ client/src/app/deck/deck-select.tsx
@@ -529,7 +529,8 @@ function SelectDeck({
 										{loadedDeck.cards.length}/{CONFIG.limits.maxCards}
 									</div>
 									<div className={classNames(css.mobileDeckStat, css.tokens)}>
-										{getDeckCost(loadedDeck.cards.map(card => card.props))}/{CONFIG.limits.maxDeckCost}
+										{getDeckCost(loadedDeck.cards.map((card) => card.props))}/
+										{CONFIG.limits.maxDeckCost}
 									</div>
 								</div>
 							</div>
