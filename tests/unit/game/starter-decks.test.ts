3ad45719f7ffa3c54986970f1a61d0c72b5f0ea6
Lunarmagpie <Bambolabmo0@gmail.com>
2024-11-01 13:37:52 -0400

    Test->Verify

--- tests/unit/game/starter-decks.test.ts
+++ tests/unit/game/starter-decks.test.ts
@@ -3,7 +3,7 @@ import {STARTER_DECKS} from 'common/cards/starter-decks'
 import {validateDeck} from 'common/utils/validation'
 
 describe('Test starter decks', () => {
-	test('Test starter decks are valid.', () => {
+	test('Verify starter decks are valid.', () => {
 		for (const deck of STARTER_DECKS) {
 			expect(validateDeck(deck)).toStrictEqual({valid: true})
 		}
