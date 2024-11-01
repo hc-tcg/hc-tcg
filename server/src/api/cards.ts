a1a1111e6bac4a192039032343f263486130b22c
Lunarmagpie <Bambolabmo0@gmail.com>
2024-11-01 13:07:59 -0400

    Format

--- server/src/api/cards.ts
+++ server/src/api/cards.ts
@@ -1,7 +1,6 @@
 import {CARDS, CARDS_LIST} from 'common/cards'
 import {getCardImage, getHermitBackground} from 'common/cards/card'
 import {Card, isAttach, isHermit, isItem, isSingleUse} from 'common/cards/types'
-import {toLocalCardInstance} from 'common/utils/cards'
 import {getDeckFromHash} from 'common/utils/import-export'
 import {getCardVisualTokenCost, getDeckCost} from 'common/utils/ranks'
 import root from 'serverRoot'
--- tests/unit/game/starter-decks.test.ts
+++ tests/unit/game/starter-decks.test.ts
@@ -5,7 +5,7 @@ import {validateDeck} from 'common/utils/validation'
 describe('Test starter decks', () => {
 	test('Test starter decks are valid.', () => {
 		for (const deck of STARTER_DECKS) {
-			expect(validateDeck(deck)).toHaveProperty("valid", true)
+			expect(validateDeck(deck)).toHaveProperty('valid', true)
 		}
 	})
 })
