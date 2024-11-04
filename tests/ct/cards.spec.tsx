0cd13edf8f7584e23fc0b69e062b0a707f9888c2
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-04 18:57:52 -0500

    changes

--- tests/ct/cards.spec.tsx
+++ tests/ct/cards.spec.tsx
@@ -8,39 +8,51 @@ import {
 	itemCardClasses,
 	singleUseCardClasses,
 } from 'common/cards'
-import EthosLabCommon from 'common/cards/hermits/ethoslab-common'
-import BuilderDoubleItem from 'common/cards/items/builder-rare'
-import GoldenAxe from 'common/cards/single-use/golden-axe'
 
 test.use({viewport: {width: 400, height: 400}})
 test.setTimeout(1_000_000)
 
 test('Hermit card SVG', async ({mount}) => {
 	for (const card of hermitCardClasses) {
-		const component = await mount(
-			<HermitCard card={card as any} displayTokenCost={true} />,
+		let component = await mount(
+			<HermitCard card={card as any} displayTokenCost={false} />,
 		)
 		await expect(component).toHaveScreenshot(`${card.id}.png`)
+		await component.unmount()
+		component = await mount(
+			<HermitCard card={card as any} displayTokenCost={true} />,
+		)
+		await expect(component).toHaveScreenshot(`${card.id}-with-tokens.png`)
 		await component.unmount()
 	}
 })
 
 test('Item card SVG', async ({mount}) => {
 	for (const card of itemCardClasses) {
-		const component = await mount(
-			<ItemCard card={card as any} displayTokenCost={true} />,
+		let component = await mount(
+			<ItemCard card={card as any} displayTokenCost={false} />,
 		)
 		await expect(component).toHaveScreenshot(`${card.id}.png`)
+		await component.unmount()
+		component = await mount(
+			<ItemCard card={card as any} displayTokenCost={true} />,
+		)
+		await expect(component).toHaveScreenshot(`${card.id}-with-tokens.png`)
 		await component.unmount()
 	}
 })
 
 test('Effect card SVG', async ({mount}) => {
 	for (const card of [...attachCardClasses, ...singleUseCardClasses]) {
-		const component = await mount(
-			<EffectCard card={card as any} displayTokenCost={true} />,
+		let component = await mount(
+			<EffectCard card={card as any} displayTokenCost={false} />,
 		)
 		await expect(component).toHaveScreenshot(`${card.id}.png`)
+		await component.unmount()
+		component = await mount(
+			<EffectCard card={card as any} displayTokenCost={true} />,
+		)
+		await expect(component).toHaveScreenshot(`${card.id}-with-tokens.png`)
 		await component.unmount()
 	}
 })
