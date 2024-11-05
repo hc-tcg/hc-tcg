4956ea2b51449d8ad7eb133bcf2f7d5070407ae3
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-05 10:59:11 -0500

    Make it look good

--- common/status-effects/mining-fatigue.ts
+++ common/status-effects/mining-fatigue.ts
@@ -1,4 +1,5 @@
-import {CardComponent} from '../components'
+import {CardComponent, StatusEffectComponent} from '../components'
+import query from '../components/query'
 import {Counter, StatusEffect, statusEffect} from './status-effect'
 
 export const MiningFatigueEffect: Counter<CardComponent> = {
@@ -32,7 +33,16 @@ export const SingleTurnMiningFatigueEffect: StatusEffect<CardComponent> = {
 	name: 'Mining Fatigue',
 	description:
 		"This Hermit's attacks cost an additional item card of their type.",
-	onApply(_game, effect, target, observer) {
+	onApply(game, effect, target, observer) {
+		let components = game.components.filter(
+			StatusEffectComponent,
+			query.effect.is(SingleTurnMiningFatigueEffect),
+			query.effect.targetEntity(target.entity),
+		)
+		if (components.length >= 2) {
+			components[0].remove()
+		}
+
 		let counter = 2
 
 		const {player} = target
