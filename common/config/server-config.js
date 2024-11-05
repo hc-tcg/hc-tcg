91666ecebbd0730cfa145bdd8895744fb9332ca7
Rvtar <106639908+Rvtar@users.noreply.github.com>
2024-11-05 15:07:18 -0500

    Change scuffed port
    This is probably equivalent to a mod. There will be conflicts with base game stuff.

--- common/config/server-config.js
+++ common/config/server-config.js
@@ -1,9 +1,9 @@
 export default {
 	port: 9000,
-	clientDevPort: 3002,
+	clientDevPort: 4002,
 	clientPath: 'client/dist',
 	cors: [
-		'http://localhost:3002',
+		'http://localhost:4002',
 		'https://hc-tcg-beta.fly.dev',
 		'https://hc-tcg-testing.fly.dev',
 		'https://hc-tcg.online',
