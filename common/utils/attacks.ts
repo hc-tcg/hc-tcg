f28afb4bd67ced56c76398c3126642d1bb1d7b81
Rvtar <106639908+Rvtar@users.noreply.github.com>
2024-11-06 23:25:44 -0500

    Remove master diff

--- common/utils/attacks.ts
+++ common/utils/attacks.ts
@@ -11,7 +11,7 @@ import {AttackModel} from '../models/attack-model'
 import {GameModel} from '../models/game-model'
 import WeaknessEffect from '../status-effects/weakness'
 import {TypeT} from '../types/cards'
-import {afterAttack, onTurnEnd} from '../types/priorities'//
+import {afterAttack, onTurnEnd} from '../types/priorities'
 
 /**
  * Call before attack hooks for each attack that has an attacker
