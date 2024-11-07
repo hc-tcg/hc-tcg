54384f4f5ab53e5ac855227ce287953189c4de89
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-07 13:29:15 -0500

    Format

--- client/src/components/card/card.tsx
+++ client/src/components/card/card.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames'
 import {Card as CardObject} from 'common/cards/types'
+import {DEBUG} from 'common/config'
 import {WithoutFunctions} from 'common/types/server-requests'
 import Tooltip from 'components/tooltip'
 import CardInstanceTooltip from './card-tooltip'
@@ -7,7 +8,6 @@ import css from './card.module.scss'
 import EffectCardModule, {EffectCardProps} from './effect-card-svg'
 import HermitCardModule, {HermitCardProps} from './hermit-card-svg'
 import ItemCardModule, {ItemCardProps} from './item-card-svg'
-import {DEBUG} from 'common/config'
 
 interface CardReactProps
 	extends React.DetailedHTMLProps<
