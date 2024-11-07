4dc1e21cf4c0bcc4b23f34e774443d35af9e69e9
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-07 13:24:52 -0500

    ct update

--- tests/ct/cards.spec.tsx
+++ tests/ct/cards.spec.tsx
@@ -2,57 +2,29 @@ import {expect, test} from '@playwright/experimental-ct-react'
 import EffectCard from 'client/components/card/effect-card-svg'
 import HermitCard from 'client/components/card/hermit-card-svg'
 import ItemCard from 'client/components/card/item-card-svg'
-import {
-	attachCardClasses,
-	hermitCardClasses,
-	itemCardClasses,
-	singleUseCardClasses,
-} from 'common/cards'
+import EthosLabCommon from 'common/cards/hermits/ethoslab-common'
+import BuilderDoubleItem from 'common/cards/items/builder-rare'
+import GoldenAxe from 'common/cards/single-use/golden-axe'
 
 test.use({viewport: {width: 400, height: 400}})
-test.setTimeout(1_000_000)
 
 test('Hermit card SVG', async ({mount}) => {
-	for (const card of hermitCardClasses) {
-		let component = await mount(
-			<HermitCard card={card as any} displayTokenCost={false} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}.png`)
-		await component.unmount()
-		component = await mount(
-			<HermitCard card={card as any} displayTokenCost={true} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}-with-tokens.png`)
-		await component.unmount()
-	}
+	const component = await mount(
+		<HermitCard card={EthosLabCommon} displayTokenCost={true} />,
+	)
+	await expect(component).toHaveScreenshot()
 })
 
 test('Item card SVG', async ({mount}) => {
-	for (const card of itemCardClasses) {
-		let component = await mount(
-			<ItemCard card={card as any} displayTokenCost={false} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}.png`)
-		await component.unmount()
-		component = await mount(
-			<ItemCard card={card as any} displayTokenCost={true} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}-with-tokens.png`)
-		await component.unmount()
-	}
+	const component = await mount(
+		<ItemCard card={BuilderDoubleItem} displayTokenCost={true} />,
+	)
+	await expect(component).toHaveScreenshot()
 })
 
 test('Effect card SVG', async ({mount}) => {
-	for (const card of [...attachCardClasses, ...singleUseCardClasses]) {
-		let component = await mount(
-			<EffectCard card={card as any} displayTokenCost={false} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}.png`)
-		await component.unmount()
-		component = await mount(
-			<EffectCard card={card as any} displayTokenCost={true} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}-with-tokens.png`)
-		await component.unmount()
-	}
+	const component = await mount(
+		<EffectCard card={GoldenAxe} displayTokenCost={true} />,
+	)
+	await expect(component).toHaveScreenshot()
 })
