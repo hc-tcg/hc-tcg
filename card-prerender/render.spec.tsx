a8647d62067ddc4fb28275f4e7002ad632c85110
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-08 17:49:36 -0500

    more workers

--- card-prerender/render.spec.tsx
+++ card-prerender/render.spec.tsx
@@ -2,68 +2,81 @@ import {test} from '@playwright/experimental-ct-react'
 import EffectCard from 'client/components/card/effect-card-svg'
 import HermitCard from 'client/components/card/hermit-card-svg'
 import ItemCard from 'client/components/card/item-card-svg'
-import {CARDS_LIST, hermitCardClasses} from 'common/cards'
+import {CARDS_LIST} from 'common/cards'
 
-test('Render Cards To PNG', async ({page, mount}) => {
-	let components = []
+const SECTION_SIZE = 75
+const MAX = Math.floor(CARDS_LIST.length / SECTION_SIZE)
 
-	let divStyle = {width: '400px', height: '400px'}
-
-	for (const card of [...CARDS_LIST]) {
-		if (card.category === 'hermit') {
-			components.push([
-				<div id={`${card.id}`} style={divStyle}>
-					<HermitCard card={card as any} displayTokenCost={false} />,
-				</div>,
-			])
-			components.push([
-				<div id={`${card.id}-with-tokens`} style={divStyle}>
-					<HermitCard card={card as any} displayTokenCost={false} />,
-				</div>,
-			])
-		} else if (card.category === 'item') {
-			components.push([
-				<div id={`${card.id}`} style={divStyle}>
-					<ItemCard card={card as any} displayTokenCost={false} />,
-				</div>,
-			])
-			components.push([
-				<div id={`${card.id}-with-tokens`} style={divStyle}>
-					<ItemCard card={card as any} displayTokenCost={false} />,
-				</div>,
-			])
-		} else {
-			components.push([
-				<div id={`${card.id}`} style={divStyle}>
-					<EffectCard card={card as any} displayTokenCost={false} />,
-				</div>,
-			])
-			components.push([
-				<div id={`${card.id}-with-tokens`} style={divStyle}>
-					<EffectCard card={card as any} displayTokenCost={false} />,
-				</div>,
-			])
-		}
-	}
-
-	console.log('Mounting cards...')
-	await mount(<div>{components}</div>)
-
-	const total = CARDS_LIST.length * 2
-	let completed = 0
-
-	for (const card of CARDS_LIST) {
-		await page
-			.locator(`id=${card.id}`)
-			.screenshot({path: `card-prerender/render/${card.id}.png`})
-		completed += 1
-		console.log(`[${completed}/${total}] Screenshotted \`${card.id}\``)
-		await page.locator(`id=${card.id}-with-tokens`).screenshot({
-			path: `card-prerender/render/${card.id}_with_tokens.png`,
-		})
-		completed += 1
-		console.log(
-			`[${completed}/${total}] Screenshotted \`${card.id}\` with tokens`,
+for (let i = 1; i <= MAX + 1; i++) {
+	const number = i
+	test(`Render Cards To PNG (${i}/${MAX}) `, async ({page, mount}) => {
+		let section = CARDS_LIST.slice(
+			(number - 1) * SECTION_SIZE,
+			Math.min(number * SECTION_SIZE, CARDS_LIST.length),
 		)
-	}
-})
+
+		let components = []
+
+		let divStyle = {width: '400px', height: '400px'}
+
+		for (const card of [...section]) {
+			if (card.category === 'hermit') {
+				components.push([
+					<div id={`${card.id}`} style={divStyle}>
+						<HermitCard card={card as any} displayTokenCost={false} />,
+					</div>,
+				])
+				components.push([
+					<div id={`${card.id}-with-tokens`} style={divStyle}>
+						<HermitCard card={card as any} displayTokenCost={false} />,
+					</div>,
+				])
+			} else if (card.category === 'item') {
+				components.push([
+					<div id={`${card.id}`} style={divStyle}>
+						<ItemCard card={card as any} displayTokenCost={false} />,
+					</div>,
+				])
+				components.push([
+					<div id={`${card.id}-with-tokens`} style={divStyle}>
+						<ItemCard card={card as any} displayTokenCost={false} />,
+					</div>,
+				])
+			} else {
+				components.push([
+					<div id={`${card.id}`} style={divStyle}>
+						<EffectCard card={card as any} displayTokenCost={false} />,
+					</div>,
+				])
+				components.push([
+					<div id={`${card.id}-with-tokens`} style={divStyle}>
+						<EffectCard card={card as any} displayTokenCost={false} />,
+					</div>,
+				])
+			}
+		}
+
+		console.log('Mounting cards...')
+		await mount(<div>{components}</div>)
+
+		const total = section.length * 2
+		let completed = 0
+
+		for (const card of section) {
+			await page
+				.locator(`id=${card.id}`)
+				.screenshot({path: `card-prerender/render/${card.id}.png`})
+			completed += 1
+			console.log(
+				`(${number}) [${completed}/${total}] Screenshotted \`${card.id}\``,
+			)
+			await page.locator(`id=${card.id}-with-tokens`).screenshot({
+				path: `card-prerender/render/${card.id}_with_tokens.png`,
+			})
+			completed += 1
+			console.log(
+				`(${number}) [${completed}/${total}] Screenshotted \`${card.id}\` with tokens`,
+			)
+		}
+	})
+}
