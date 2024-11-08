fc515689b1c2b70961c14397ee37762fd0d6615e
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-07 22:25:20 -0500

    mkdir

--- card-prerender/render.sh
+++ card-prerender/render.sh
@@ -8,6 +8,7 @@
 npx playwright test -c playwright-render.config.ts -u
 
 echo 'Writing JSON files to `client/public/images/cards/`...'
+mkdir client/public/images/cards || true
 find card-prerender/render/ -type f -print0 | xargs '-I{}' -n1 -0 sh -c 'convert {} client/public/images/cards/$(basename --suffix=.png {}).jpg'
 
 echo "DONE!"
