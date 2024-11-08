68cee1f098cbd0410c7212360662f86c544e4d54
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-08 17:07:40 -0500

    Change screenshot method

--- card-prerender/render.spec.tsx
+++ card-prerender/render.spec.tsx
@@ -1,57 +1,67 @@
-import {expect, test} from '@playwright/experimental-ct-react'
+import {test} from '@playwright/experimental-ct-react'
 import EffectCard from 'client/components/card/effect-card-svg'
 import HermitCard from 'client/components/card/hermit-card-svg'
 import ItemCard from 'client/components/card/item-card-svg'
-import {
-	attachCardClasses,
-	hermitCardClasses,
-	itemCardClasses,
-	singleUseCardClasses,
-} from 'common/cards'
+import {CARDS_LIST, hermitCardClasses} from 'common/cards'
 
-test.use({viewport: {width: 400, height: 400}})
+test('Render Cards To PNG', async ({page, mount}) => {
+	let components = []
 
-test('Hermit card SVG', async ({mount}) => {
-	for (const card of hermitCardClasses) {
-		let component = await mount(
-			<HermitCard card={card as any} displayTokenCost={false} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}.png`)
-		await component.unmount()
-		component = await mount(
-			<HermitCard card={card as any} displayTokenCost={true} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}-with-tokens.png`)
-		await component.unmount()
+	let divStyle = {width: '400px', height: '400px'}
+
+	for (const card of [...CARDS_LIST]) {
+		if (card.category === 'hermit') {
+			components.push([
+				<div id={`${card.id}`} style={divStyle}>
+					<HermitCard card={card as any} displayTokenCost={false} />,
+				</div>,
+			])
+			components.push([
+				<div id={`${card.id}-with-tokens`} style={divStyle}>
+					<HermitCard card={card as any} displayTokenCost={false} />,
+				</div>,
+			])
+		} else if (card.category === 'item') {
+			components.push([
+				<div id={`${card.id}`} style={divStyle}>
+					<ItemCard card={card as any} displayTokenCost={false} />,
+				</div>,
+			])
+			components.push([
+				<div id={`${card.id}-with-tokens`} style={divStyle}>
+					<ItemCard card={card as any} displayTokenCost={false} />,
+				</div>,
+			])
+		} else {
+			components.push([
+				<div id={`${card.id}`} style={divStyle}>
+					<EffectCard card={card as any} displayTokenCost={false} />,
+				</div>,
+			])
+			components.push([
+				<div id={`${card.id}-with-tokens`} style={divStyle}>
+					<EffectCard card={card as any} displayTokenCost={false} />,
+				</div>,
+			])
+		}
 	}
-})
 
-test('Item card SVG', async ({mount}) => {
-	for (const card of itemCardClasses) {
-		let component = await mount(
-			<ItemCard card={card as any} displayTokenCost={false} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}.png`)
-		await component.unmount()
-		component = await mount(
-			<ItemCard card={card as any} displayTokenCost={true} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}-with-tokens.png`)
-		await component.unmount()
-	}
-})
+	await mount(<div>{components}</div>)
 
-test('Effect card SVG', async ({mount}) => {
-	for (const card of [...attachCardClasses, ...singleUseCardClasses]) {
-		let component = await mount(
-			<EffectCard card={card as any} displayTokenCost={false} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}.png`)
-		await component.unmount()
-		component = await mount(
-			<EffectCard card={card as any} displayTokenCost={true} />,
-		)
-		await expect(component).toHaveScreenshot(`${card.id}-with-tokens.png`)
-		await component.unmount()
-	}
+	await Promise.all(
+		CARDS_LIST.flatMap((card) => [
+			(async () => {
+				await page
+					.locator(`id=${card.id}`)
+					.screenshot({path: `card-prerender/render/${card.id}.png`})
+				console.log(`screenshotted \`${card.id}\``)
+			})(),
+			(async () => {
+				await page.locator(`id=${card.id}-with-tokens`).screenshot({
+					path: `card-prerender/render/${card.id}-with-tokens.png`,
+				})
+				console.log(`screenshotted \`${card.id}\` with tokens`)
+			})(),
+		]),
+	)
 })
