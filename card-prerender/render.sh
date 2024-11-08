eba8ab89a9bb6affcf1676171174e5925be6b75f
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-07 22:13:59 -0500

    changes

--- card-prerender/render.sh
+++ card-prerender/render.sh
@@ -8,7 +8,7 @@
 npx playwright test -c playwright-render.config.ts -u
 
 echo 'Writing JSON files to `client/public/images/cards/`...'
-find card-prerender/render/ -type f -print0 | xargs '-I{}' -n1 -0 sh -c 'magick {} client/public/images/cards/$(basename --suffix=.png {}).jpg'
+find card-prerender/render/ -type f -print0 | xargs '-I{}' -n1 -0 sh -c 'convert {} client/public/images/cards/$(basename --suffix=.png {}).jpg'
 
 echo "DONE!"
 
