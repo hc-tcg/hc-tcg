164b78baa97254e56e20596f95d5adda820ab0de
Alice <cheezyanimations@gmail.com>
2024-11-10 02:12:40 -0500

    Fix

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -310,11 +310,13 @@ export class Database {
 					key: row['tag_id'],
 				}
 				const cardId: number | null = row['card_id']
-				const cards: Array<Card> = [
-					...Array(row['copies']).fill(
-						this.allCards.find((card) => card.numericId === cardId),
-					),
-				]
+				const cards: Array<Card> = cardId
+					? [
+							...Array(row['copies']).fill(
+								this.allCards.find((card) => card.numericId === cardId),
+							),
+						]
+					: []
 
 				const foundDeck = allDecks.find((deck) => deck.code === code)
 
