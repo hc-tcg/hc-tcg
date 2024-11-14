2d2d08f8420e722ee133f7f3b9cd5ba9c47997a7
Andreas Kjellqvist <andreas.kjellqvist@timeedit.com>
2024-11-14 23:41:24 +0100

    Refactored redstone-torch.ts

--- common/cards/advent-of-tcg/single-use/redstone-torch.ts
+++ common/cards/advent-of-tcg/single-use/redstone-torch.ts
@@ -1,12 +1,19 @@
 import assert from 'assert'
-import {CardComponent, ObserverComponent} from '../../../components'
+import {
+	CardComponent,
+	type ObserverComponent,
+	type PlayerComponent,
+} from '../../../components'
 import query from '../../../components/query'
-import {GameModel} from '../../../models/game-model'
+import {type CardEntity} from '../../../entities'
+import {type GameModel} from '../../../models/game-model'
+import type ComponentTable from '../../../types/ecs'
+import {type SelectCards} from '../../../types/modal-requests'
 import {executeExtraAttacks} from '../../../utils/attacks'
 import {fisherYatesShuffle} from '../../../utils/fisher-yates'
 import {singleUse} from '../../defaults'
 import TNT from '../../single-use/tnt'
-import {SingleUse} from '../../types'
+import {type SingleUse} from '../../types'
 import MinecartWithTNT from './tnt-minecart'
 
 const RedstoneTorch: SingleUse = {
@@ -26,90 +33,141 @@ const RedstoneTorch: SingleUse = {
 		(_game, pos) => pos.player.getDeck().length > 0,
 	),
 	log: (values) => values.defaultLog,
-	onAttach(
+	onAttach: (
 		game: GameModel,
 		component: CardComponent,
 		observer: ObserverComponent,
-	) {
-		const {player, opponentPlayer} = component
-		observer.subscribe(player.hooks.onApply, () => {
-			const tntCardEntities = game.components.filterEntities(
-				CardComponent,
-				query.card.slot(query.slot.deck, query.slot.currentPlayer),
-				query.card.is(TNT, MinecartWithTNT),
-			)
-
-			const playerDeck = player.getDeck().sort(CardComponent.compareOrder)
-
-			const tntAdjacentCards = playerDeck.reduce(
-				(reducer: Array<CardComponent>, card, i) => {
-					if (
-						(i < playerDeck.length - 1 &&
-							tntCardEntities.includes(playerDeck[i + 1].entity)) ||
-						(i > 0 && tntCardEntities.includes(playerDeck[i - 1].entity)) ||
-						tntCardEntities.includes(card.entity)
-					) {
-						return [...reducer, card]
-					}
-					return reducer
-				},
-				[],
-			)
-
-			game.addModalRequest({
-				player: player.entity,
-				modal: {
-					type: 'selectCards',
-					name: 'Redstone Torch',
-					description: 'These cards are going to be discarded.',
-					cards: tntAdjacentCards.map((card) => card.entity),
-					selectionSize: 0,
-					primaryButton: {
-						text: 'Detonate',
-						variant: 'error',
-					},
-					cancelable: false,
-				},
-				onResult(modalResult) {
-					if (!modalResult) return
-
-					tntAdjacentCards.forEach((card) => card.discard())
-
-					const tntAttack = game
-						.newAttack({
-							attacker: component.entity,
-							player: player.entity,
-							target: opponentPlayer.activeRowEntity,
-							type: 'effect',
-							log: (values) =>
-								`${values.player} detonated their ${tntCardEntities.length} $e${TNT.name}$ with $e${RedstoneTorch.name}$ to attack ${values.target} for ${values.damage} damage `,
-						})
-						.addDamage(component.entity, 40 * tntCardEntities.length)
-
-					executeExtraAttacks(game, [tntAttack])
-
-					const deckCards = player.getDeck()
-					const newOrder = fisherYatesShuffle(
-						deckCards.map((card) => {
-							assert(card.slot.inDeck())
-							return card.slot.order
-						}),
-						game.rng,
-					)
-					deckCards.forEach((card, i) => {
-						assert(card.slot.inDeck())
-						card.slot.order = newOrder[i]
-					})
-					deckCards.forEach((card) => card.hooks.onChangeSlot.call(card.slot))
-
-					return 'SUCCESS'
-				},
-				onTimeout() {
-					// Do nothing
-				},
-			})
-		})
+	) => {
+		const {player} = component
+		observer.subscribe(player.hooks.onApply, handleApply({game, component}))
 	},
+}
+
+type handleApplyProps = {
+	game: GameModel
+	component: CardComponent
+}
+function handleApply({game, component}: handleApplyProps): () => void {
+	return () => {
+		const {player, opponentPlayer, entity} = component
+		const tntCardEntities = getTntCardEntities(game.components)
+		const tntAdjacentCards = getTntAdjacentCards(
+			player.getDeck(),
+			tntCardEntities,
+		)
+
+		game.addModalRequest({
+			player: player.entity,
+			modal: {
+				type: 'selectCards',
+				name: 'Redstone Torch',
+				description: 'These cards are going to be discarded.',
+				cards: tntAdjacentCards.map((card) => card.entity),
+				selectionSize: 0,
+				primaryButton: {
+					text: 'Detonate',
+					variant: 'error',
+				},
+				cancelable: false,
+			},
+
+			onResult: handleResult({
+				tntAdjacentCards,
+				opponentPlayer,
+				componentEntity: entity,
+				tntCardEntities,
+				player,
+				game,
+			}),
+			onTimeout() {
+				// Do nothing
+			},
+		})
+	}
+	function getTntCardEntities(components: ComponentTable) {
+		return components.filterEntities(
+			CardComponent,
+			query.card.slot(query.slot.deck, query.slot.currentPlayer),
+			query.card.is(TNT, MinecartWithTNT),
+		)
+	}
+	function getTntAdjacentCards(
+		playerDeck: CardComponent[],
+		tntCardEntities: CardEntity[],
+	) {
+		playerDeck.sort(CardComponent.compareOrder)
+
+		return playerDeck.filter((card, i) => {
+			const followingCardIsTnt =
+				i < playerDeck.length - 1 &&
+				tntCardEntities.includes(playerDeck[i + 1].entity)
+
+			const previousCardIsTnt =
+				i > 0 && tntCardEntities.includes(playerDeck[i - 1].entity)
+
+			return (
+				followingCardIsTnt ||
+				previousCardIsTnt ||
+				tntCardEntities.includes(card.entity)
+			)
+		})
+	}
+}
+
+type handleResultProps = {
+	tntAdjacentCards: CardComponent[]
+	opponentPlayer: PlayerComponent
+	player: PlayerComponent
+	componentEntity: CardEntity
+	game: GameModel
+	tntCardEntities: CardEntity[]
+}
+function handleResult({
+	tntAdjacentCards,
+	game,
+	componentEntity,
+	player,
+	opponentPlayer,
+	tntCardEntities,
+}: handleResultProps) {
+	return (modalResult: SelectCards.Result) => {
+		if (!modalResult) return
+
+		tntAdjacentCards.forEach((card) => card.discard())
+		const tntAttack = createTntAttack()
+		executeExtraAttacks(game, [tntAttack])
+		shuffleCards(player.getDeck())
+
+		return 'SUCCESS'
+	}
+
+	function createTntAttack() {
+		return game
+			.newAttack({
+				attacker: componentEntity,
+				player: player.entity,
+				target: opponentPlayer.activeRowEntity,
+				type: 'effect',
+				log: (values) =>
+					`${values.player} detonated their ${tntCardEntities.length} $e${TNT.name}$ with $e${RedstoneTorch.name}$ to attack ${values.target} for ${values.damage} damage `,
+			})
+			.addDamage(componentEntity, 40 * tntCardEntities.length)
+	}
+
+	function shuffleCards(deckCards: CardComponent[]) {
+		const newOrder = fisherYatesShuffle(
+			deckCards.map((card) => {
+				assert(card.slot.inDeck())
+				return card.slot.order
+			}),
+			game.rng,
+		)
+		deckCards.forEach((card, i) => {
+			assert(card.slot.inDeck())
+			card.slot.order = newOrder[i]
+		})
+		deckCards.forEach((card) => card.hooks.onChangeSlot.call(card.slot))
+	}
 }
 
 export default RedstoneTorch
