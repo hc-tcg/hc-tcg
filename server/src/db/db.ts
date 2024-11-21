0782f9d3723ffe3d2e9ba4475114ac0461ad39b5
Alice <cheezyanimations@gmail.com>
2024-11-21 09:59:48 -0500

    fix

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -939,6 +939,7 @@ export class Database {
 							LEFT JOIN deck_cards ON deck_cards.deck_code = decks.deck_code
 							LEFT JOIN games ON games.winner_deck_code = decks.deck_code OR games.loser_deck_code = decks.deck_code
 							WHERE deck_cards.card_id >= 49 AND deck_cards.card_id <= 68
+                            AND (games.winner_deck_code = decks.deck_code OR games.loser_deck_code = decks.deck_code)
 							AND ($1::bigint IS NULL OR games.completion_time > to_timestamp($1::bigint))
 							AND ($2::bigint IS NULL OR games.completion_time <= to_timestamp($2::bigint))
 						)
