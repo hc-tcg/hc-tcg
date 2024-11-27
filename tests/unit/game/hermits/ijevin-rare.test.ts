368749370560a5fa673705e505cc903fc3b35401
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-27 14:09:17 -0500

    Move jevin stuff behind verbose logging

--- tests/unit/game/hermits/ijevin-rare.test.ts
+++ tests/unit/game/hermits/ijevin-rare.test.ts
@@ -2,6 +2,7 @@ import {describe, expect, test} from '@jest/globals'
 import EthosLabCommon from 'common/cards/hermits/ethoslab-common'
 import IJevinRare from 'common/cards/hermits/ijevin-rare'
 import Iskall85Common from 'common/cards/hermits/iskall85-common'
+import {printBoardState} from 'server/utils'
 import {attack, endTurn, playCardFromHand, testGame} from '../utils'
 
 describe('Test iJevin Peace Out', () => {
@@ -26,7 +27,10 @@ describe('Test iJevin Peace Out', () => {
 
 					// Manually timeout request
 					game.removePickRequest(0, true)
-					console.info(`${game.logHeader} Manually timed out pick request 0`)
+					if (game.settings.verboseLogging) {
+						console.info(`${game.logHeader} Manually timed out pick request 0`)
+						printBoardState(game)
+					}
 
 					expect(game.opponentPlayer.activeRow?.index).toBe(2)
 					expect(game.opponentPlayer.activeRow?.playerId).toBe(
