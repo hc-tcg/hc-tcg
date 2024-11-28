a261ab9a31eda1ba04547e55956ef4d53e549743
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-28 00:29:07 -0500

    Don't show forfeit action

--- client/src/logic/game/tasks/action-modals-saga.ts
+++ client/src/logic/game/tasks/action-modals-saga.ts
@@ -45,6 +45,7 @@ export const ActionMap: Record<TurnAction, string | null> = {
 	WAIT_FOR_TURN: null,
 	MODAL_REQUEST: null,
 	DELAY: null,
+	FORFEIT: null,
 }
 
 function* endTurnActionSaga(): SagaIterator {
