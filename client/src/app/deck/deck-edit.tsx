dd571ceb43c0ef6a507941a3ab24f4538038dbac
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-30 01:52:18 -0500

    Format

--- client/src/app/deck/deck-edit.tsx
+++ client/src/app/deck/deck-edit.tsx
@@ -408,7 +408,7 @@ function EditDeck({
 		setLoadedDeck({...loadedDeck, cards: []})
 	}
 	const addCard = (card: LocalCardInstance) => {
-		console.log("Card: ", card.props.id)
+		console.log('Card: ', card.props.id)
 		setLoadedDeck((loadedDeck) => ({
 			...loadedDeck,
 			cards: [
