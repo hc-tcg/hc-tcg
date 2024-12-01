53deda861bd2f2d58cc994444853eb64f46e76ab
Lunarmagpie <Bambolambo0@gmail.com>
2024-11-30 20:35:24 -0500

    Fix incorrect images for cards being generated

--- card-prerender/render.spec.tsx
+++ card-prerender/render.spec.tsx
@@ -28,7 +28,6 @@ for (let i = 1; i <= MAX; i++) {
 				])
 				components.push([
 					<div id={`${card.id}-with-tokens`} style={divStyle}>
-						<HermitCard card={card as any} displayTokenCost={false} />,
 						<HermitCard card={card as any} displayTokenCost={true} />,
 					</div>,
 				])
@@ -40,7 +39,6 @@ for (let i = 1; i <= MAX; i++) {
 				])
 				components.push([
 					<div id={`${card.id}-with-tokens`} style={divStyle}>
-						<ItemCard card={card as any} displayTokenCost={false} />,
 						<ItemCard card={card as any} displayTokenCost={true} />,
 					</div>,
 				])
@@ -52,7 +50,6 @@ for (let i = 1; i <= MAX; i++) {
 				])
 				components.push([
 					<div id={`${card.id}-with-tokens`} style={divStyle}>
-						<EffectCard card={card as any} displayTokenCost={false} />,
 						<EffectCard card={card as any} displayTokenCost={true} />,
 					</div>,
 				])
