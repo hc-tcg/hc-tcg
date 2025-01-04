5028ad72a9e85818fed94e34089efe0fe295fa9e
Benji42 <cheezyanimations@gmail.com>
2025-01-04 18:59:08 -0500

    lint

--- tests/unit/game/starter-decks.test.ts
+++ tests/unit/game/starter-decks.test.ts
@@ -1,8 +1,8 @@
 import assert from 'assert'
 import {describe, test} from '@jest/globals'
 import {STARTER_DECKS} from 'common/cards/starter-decks'
-import {validateDeck} from 'common/utils/validation'
 import {getDeckCost} from 'common/utils/ranks'
+import {validateDeck} from 'common/utils/validation'
 
 describe('Test starter decks', () => {
 	test('Verify starter decks are valid.', () => {
