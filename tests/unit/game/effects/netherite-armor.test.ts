eea34a09330e5c26966beccbb4461964df3c0269
Lunarmagpie <Bambolambo0@gmail.com>
2025-01-03 20:29:38 -0500

    start netherite armor tests

---
+++ tests/unit/game/effects/netherite-armor.test.ts
@@ -0,0 +1,17 @@
+import {NetheriteArmor} from 'common/cards/attach/armor'
+import {
+	applyEffect,
+	attack,
+	changeActiveHermit,
+	endTurn,
+	finishModalRequest,
+	pick,
+	playCardFromHand,
+	testGame,
+} from '../../utils'
+
+describe('Test Netherite Armor', () => {
+	test('Netherite Armor prevents damage', () => {})
+	test('Netherite Armor prevents knockback', () => {})
+	test('Netherite Armor prevents damage from effects', () => {})
+})
