7e45662f11bdcec05be06ef8990ce67ea178d2ba
Benji42 <cheezyanimations@gmail.com>
2025-01-03 22:37:50 -0500

    refactor starter decks test to show which decks are not valid

--- tests/unit/game/starter-decks.test.ts
+++ tests/unit/game/starter-decks.test.ts
@@ -1,11 +1,15 @@
-import {describe, expect, test} from '@jest/globals'
+import assert from 'assert'
+import {describe, test} from '@jest/globals'
 import {STARTER_DECKS} from 'common/cards/starter-decks'
 import {validateDeck} from 'common/utils/validation'
 
 describe('Test starter decks', () => {
 	test('Verify starter decks are valid.', () => {
 		for (const deck of STARTER_DECKS) {
-			expect(validateDeck(deck)).toStrictEqual({valid: true})
+			assert(
+				validateDeck(deck.cards).valid === true,
+				`The deck "${deck.name}" is not valid`,
+			)
 		}
 	})
 })
