a9469695f120c37626053ecaf9df007608d35ddb
Benji42 <cheezyanimations@gmail.com>
2025-01-03 22:38:45 -0500

    improve test

--- tests/unit/game/starter-decks.test.ts
+++ tests/unit/game/starter-decks.test.ts
@@ -2,13 +2,14 @@ import assert from 'assert'
 import {describe, test} from '@jest/globals'
 import {STARTER_DECKS} from 'common/cards/starter-decks'
 import {validateDeck} from 'common/utils/validation'
+import {getDeckCost} from 'common/utils/ranks'
 
 describe('Test starter decks', () => {
 	test('Verify starter decks are valid.', () => {
 		for (const deck of STARTER_DECKS) {
 			assert(
 				validateDeck(deck.cards).valid === true,
-				`The deck "${deck.name}" is not valid`,
+				`The deck "${deck.name}" is not valid. Current token count: ${getDeckCost(deck.cards)}`,
 			)
 		}
 	})
