919eece6728f54198794de56a24f91e821f97fdf
Lunarmagpie <Bambolambo0@gmail.com>
2025-01-05 18:10:52 -0500

    test fixes

--- tests/unit/game/effects/netherite-armor.test.ts
+++ tests/unit/game/effects/netherite-armor.test.ts
@@ -5,7 +5,15 @@ import Egg from 'common/cards/single-use/egg'
 import TargetBlock from 'common/cards/single-use/target-block'
 import {RowComponent} from 'common/components'
 import query from 'common/components/query'
-import {attack, endTurn, pick, playCardFromHand, testGame} from '../utils'
+import {
+	applyEffect,
+	attack,
+	endTurn,
+	pick,
+	playCardFromHand,
+	testGame,
+} from '../utils'
+import {DiamondSword, IronSword} from 'common/cards/single-use/sword'
 
 describe('Test Netherite Armor', () => {
 	test('Netherite Armor prevents damage', () => {
@@ -62,6 +70,36 @@ describe('Test Netherite Armor', () => {
 	test('Netherite Armor prevents damage from effects', () => {
 		testGame(
 			{
+				playerOneDeck: [EthosLabCommon, NetheriteArmor],
+				playerTwoDeck: [EthosLabCommon, DiamondSword],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, NetheriteArmor, 'attach', 0)
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, DiamondSword, 'single_use')
+					yield* attack(game, 'primary')
+
+					expect(
+						game.components.find(
+							RowComponent,
+							query.row.index(0),
+							query.row.opponentPlayer,
+						)?.health,
+					).toBe(
+						EthosLabCommon.health -
+							EthosLabCommon.primary.damage -
+							40 /* Diamond Sword */,
+					)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+	test('Netherite Armor does not protect against redirects', () => {
+		testGame(
+			{
 				playerOneDeck: [EthosLabCommon, EthosLabCommon, NetheriteArmor],
 				playerTwoDeck: [EthosLabCommon, TargetBlock],
 				saga: function* (game) {
@@ -94,7 +132,7 @@ describe('Test Netherite Armor', () => {
 							query.row.index(1),
 							query.row.opponentPlayer,
 						)?.health,
-					).toBe(EthosLabCommon.health)
+					).toBe(EthosLabCommon.health - EthosLabCommon.primary.damage)
 				},
 			},
 			{startWithAllCards: true, noItemRequirements: true},
