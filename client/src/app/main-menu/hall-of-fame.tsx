2e87bd0c032b2036566196ecd539517483280852
Tyrannicodin <tyrannicodin@gmail.com>
2025-01-12 21:11:11 +0000

    Add chart.js charts to the hall of fame (#1143)
    Co-authored-by: Lunarmagpie <Bambolambo0@gmail.com>

--- client/src/app/main-menu/hall-of-fame.tsx
+++ client/src/app/main-menu/hall-of-fame.tsx
@@ -2,6 +2,7 @@ import {CARDS} from 'common/cards'
 import {Card as CardType, isHermit, isItem} from 'common/cards/types'
 import serverConfig from 'common/config/server-config'
 import {EXPANSIONS} from 'common/const/expansions'
+import {TypeT} from 'common/types/cards'
 import {WithoutFunctions} from 'common/types/server-requests'
 import Button from 'components/button'
 import Card from 'components/card'
@@ -11,13 +12,46 @@ import {ScreenshotDeckModal} from 'components/import-export'
 import MenuLayout from 'components/menu-layout'
 import Spinner from 'components/spinner'
 import {useRef, useState} from 'react'
+import {Bar} from 'react-chartjs-2'
 import css from './main-menu.module.scss'
+
+const TYPE_COLORS: Record<TypeT, Array<number>> = {
+	farm: [124, 204, 12],
+	redstone: [185, 33, 42],
+	prankster: [116, 55, 168],
+	explorer: [103, 138, 190],
+	balanced: [101, 124, 50],
+	builder: [184, 162, 154],
+	pvp: [85, 202, 194],
+	speedrunner: [223, 226, 36],
+	terraform: [217, 119, 147],
+	miner: [110, 105, 108],
+	any: [0, 0, 0],
+}
+
+const getTypeColor = (types: Array<string>) => {
+	let r = 0
+	let g = 0
+	let b = 0
+	types.forEach((type) => {
+		const color = TYPE_COLORS[type as TypeT]
+		r += color[0]
+		g += color[1]
+		b += color[2]
+	})
+	return (
+		'#' +
+		Math.round(r / types.length).toString(16) +
+		Math.round(g / types.length).toString(16) +
+		Math.round(b / types.length).toString(16)
+	)
+}
 
 type Props = {
 	setMenuSection: (section: string) => void
 }
 
-type Endpoints = 'decks' | 'cards' | 'game'
+type Endpoints = 'decks' | 'cards' | 'game' | 'types'
 
 const cardOrderByOptions = {
 	winrate: 'Winrate',
@@ -61,6 +95,7 @@ function HallOfFame({setMenuSection}: Props) {
 	const [selectedEndpoint, setSelectedEndpoint] = useState<Endpoints>('decks')
 	const [showDisabled, setShowAdvent] = useState<boolean>(false)
 	const [dataRetrieved, setDataRetrieved] = useState<boolean>(false)
+	const [sortBy, setSortBy] = useState<'winrate' | 'frequency'>('winrate')
 
 	/** Endpoint Options */
 	const beforeRef = useRef<any>()
@@ -105,7 +140,18 @@ function HallOfFame({setMenuSection}: Props) {
 			if (endpointAfter !== null) {
 				return `games?after=${endpointAfter}`
 			}
+
 			return 'games'
+		},
+		types: () => {
+			let url = 'type-distribution'
+			if (endpointBefore !== null) {
+				url += `&before=${endpointBefore}`
+			}
+			if (endpointAfter !== null) {
+				url += `&after=${endpointAfter}`
+			}
+			return url
 		},
 	}
 
@@ -315,6 +361,61 @@ function HallOfFame({setMenuSection}: Props) {
 		)
 	}
 
+	const parseTypes = (
+		types: Record<string, number | Array<Record<string, any>>>,
+	) => {
+		const typeList = types.types as Array<Record<string, any>>
+		typeList.sort((a, b) => b[sortBy] - a[sortBy])
+
+		return (
+			<Bar
+				title={'Types sorted by ' + sortBy}
+				className={css.typeGraph}
+				data={{
+					// @TODO: This is pretty hacky, it extends the bottom of the chart to ensure the images fit
+					labels: typeList.map((_type) => '                '),
+					datasets: [
+						{
+							label: 'Types sorted by ' + sortBy,
+							data: typeList.map(
+								(type) => Math.round(type[sortBy] * 10000) / 100,
+							),
+							backgroundColor: typeList.map((value) =>
+								getTypeColor(value.type),
+							),
+						},
+					],
+				}}
+				options={{}}
+				plugins={[
+					{
+						id: 'iconDrawer',
+						afterDatasetsDraw: (chart) => {
+							const ctx = chart.ctx
+							const xAxis = chart.scales.x
+							const offset =
+								(xAxis.getPixelForTick(1) - xAxis.getPixelForTick(0)) / 2
+							xAxis.ticks.forEach((_value, index: number) => {
+								const x = xAxis.getPixelForTick(index) - offset + 10
+								typeList[index].type.forEach((type: string, index: number) => {
+									const image = new Image()
+									image.src = `/images/types/type-${type}.png`
+									ctx.drawImage(
+										image,
+										x,
+										chart.scales.y.bottom + 5 + index * 20,
+										20,
+										20,
+									)
+								})
+							})
+						},
+					},
+				]}
+			/>
+		)
+	}
+
 	const getTable = () => {
 		if (!data) {
 			return <></>
@@ -324,6 +425,8 @@ function HallOfFame({setMenuSection}: Props) {
 			return parseCards(data)
 		} else if (selectedEndpoint === 'game') {
 			return parseGame(data)
+		} else if (selectedEndpoint === 'types') {
+			return parseTypes(data)
 		}
 	}
 
@@ -357,7 +460,12 @@ function HallOfFame({setMenuSection}: Props) {
 										<DropDownButton>{title(selectedEndpoint)}</DropDownButton>
 									}
 									label="Selected statistic"
-									options={[{name: 'Decks'}, {name: 'Cards'}, {name: 'Game'}]}
+									options={[
+										{name: 'Decks'},
+										{name: 'Cards'},
+										{name: 'Game'},
+										{name: 'Types'},
+									]}
 									showNames={true}
 									action={(option) => {
 										if (option === selectedEndpoint) return
@@ -453,6 +561,15 @@ function HallOfFame({setMenuSection}: Props) {
 											/>
 										</div>
 									</>
+								)}
+								{selectedEndpoint === 'types' && (
+									<Button
+										onClick={() =>
+											setSortBy(sortBy === 'winrate' ? 'frequency' : 'winrate')
+										}
+									>
+										Sort by: {sortBy.charAt(0).toUpperCase() + sortBy.slice(1)}
+									</Button>
 								)}
 							</div>
 						</div>
