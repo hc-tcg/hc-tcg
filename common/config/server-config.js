827430557028ffb5c3d86860dbde772e3bdccba4
Benji42 <cheezyanimations@gmail.com>
2025-01-20 21:46:24 -0500

    Fix server config

--- common/config/server-config.js
+++ common/config/server-config.js
@@ -1,15 +1,15 @@
 export default {
 	port: 9000,
 	clientDevPort: 3002,
-	clientPath: 'client/dist',
+	clientPath: "client/dist",
 	cors: [
-		'http://localhost:3002',
-		'https://hc-tcg-beta.fly.dev',
-		'https://hc-tcg-testing.fly.dev',
-		'https://hc-tcg.online',
-		'https://testing.hc-tcg.online',
+		"http://localhost:3002",
+		"https://hc-tcg-beta.fly.dev",
+		"https://hc-tcg-testing.fly.dev",
+		"https://hc-tcg.online",
+		"https://testing.hc-tcg.online",
 	],
-	world: 'LTF42',
+	world: "LTF42",
 	limits: {
 		maxTurnTime: 90,
 		extraActionTime: 30,
@@ -17,13 +17,8 @@ export default {
 		maxCards: 42,
 		maxDuplicates: 3,
 		maxDeckCost: 42,
-		bannedCards: ['evilxisuma_boss', 'feather', 'item_any_rare'],
-		disabledCards: [
-			'iskall85_common',
-			'iskall85_rare',
-			'iskallman_common',
-			'iskallman_rare',
-		],
+		bannedCards: ["evilxisuma_boss", "feather", "item_any_rare"],
+		disabledCards: ["iskallman_common", "iskallman_rare"],
 	},
-	logoSubText: 'We nerfed Mumbo!',
-}
+	logoSubText: "We nerfed Mumbo!",
+};
