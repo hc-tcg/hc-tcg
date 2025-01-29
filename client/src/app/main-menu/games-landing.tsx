e679c3c38c13aa873943fd503d0d6940f063388e
Benji42 <cheezyanimations@gmail.com>
2025-01-28 20:16:38 -0500

    Add working deck selector

--- client/src/app/main-menu/games-landing.tsx
+++ client/src/app/main-menu/games-landing.tsx
@@ -5,10 +5,13 @@ import MenuLayout from 'components/menu-layout'
 import {getLocalDatabaseInfo} from 'logic/game/database/database-selectors'
 import {localMessages, useMessageDispatch} from 'logic/messages'
 import {getSession} from 'logic/session/session-selectors'
-import {useState} from 'react'
+import {useRef, useState} from 'react'
 import {useSelector} from 'react-redux'
 import css from './games-landing.module.scss'
-import {Deck} from 'common/types/deck'
+import {Deck, Tag} from 'common/types/deck'
+import classNames from 'classnames'
+import {getIconPath} from 'common/utils/state-gen'
+import {setActiveDeck} from 'logic/saved-decks/saved-decks'
 
 type Props = {
 	setMenuSection: (section: string) => void
@@ -18,13 +21,16 @@ function GameLanding({setMenuSection}: Props) {
 	const dispatch = useMessageDispatch()
 	const {playerDeck} = useSelector(getSession)
 	const databaseInfo = useSelector(getLocalDatabaseInfo)
-	const deck = databaseInfo?.decks.find((deck) => deck.code === playerDeck)
-	const decks = databaseInfo?.decks
+	const [loadedDeck, setLoadedDeck] = useState<Deck | undefined>(
+		databaseInfo?.decks.find((deck) => deck.code === playerDeck),
+	)
 
+	const decks = databaseInfo?.decks
 	const [mode, setMode] = useState<string | null>(null)
+	const selectedDeckRef = useRef<HTMLLIElement>(null)
 
 	const checkForValidation = (): boolean => {
-		if (!playerDeck || !deck) {
+		if (!playerDeck || !loadedDeck) {
 			dispatch({
 				type: localMessages.TOAST_OPEN,
 				open: true,
@@ -34,14 +40,14 @@ function GameLanding({setMenuSection}: Props) {
 			})
 			return false
 		}
-		const validation = validateDeck(deck.cards.map((card) => card.props))
+		const validation = validateDeck(loadedDeck.cards.map((card) => card.props))
 		if (validation.valid) return true
 		dispatch({
 			type: localMessages.TOAST_OPEN,
 			open: true,
 			title: 'Your deck is not valid!',
-			description: `The deck "${deck.name}" does not meet validation requirements.`,
-			image: `/images/types/type-${deck.icon}.png`,
+			description: `The deck "${loadedDeck.name}" does not meet validation requirements.`,
+			image: `/images/types/type-${loadedDeck.icon}.png`,
 		})
 		return false
 	}
@@ -74,6 +80,56 @@ function GameLanding({setMenuSection}: Props) {
 		code: '111111',
 	}
 
+	const decksHaveTags =
+		decks.reduce((tags: Array<Tag>, decks) => {
+			return [...tags, ...decks.tags]
+		}, []).length > 0
+
+	const decksList = decks.map((deck, i) => {
+		return (
+			<li
+				className={classNames(
+					css.myDecksItem,
+					loadedDeck && deck.code === loadedDeck.code && css.selectedDeck,
+				)}
+				ref={
+					loadedDeck && deck.code === loadedDeck.code
+						? selectedDeckRef
+						: undefined
+				}
+				key={i}
+				onClick={() => {
+					setLoadedDeck(deck)
+					setActiveDeck(deck)
+					dispatch({type: localMessages.UPDATE_DECK, deck: deck})
+				}}
+			>
+				{deck.tags && deck.tags.length > 0 && (
+					<div className={css.multiColoredCircle}>
+						{deck.tags.map((tag, i) => (
+							<div
+								className={css.singleTag}
+								style={{backgroundColor: tag.color}}
+								key={i}
+							></div>
+						))}
+					</div>
+				)}
+				{decksHaveTags && deck.tags.length === 0 && (
+					<div className={css.multiColoredCircle}>
+						<div className={css.singleTag}></div>
+					</div>
+				)}
+				<div
+					className={classNames(css.deckImage, css.usesIcon, css[deck.icon])}
+				>
+					<img src={getIconPath(deck)} alt={'deck-icon'} />
+				</div>
+				{deck.name}
+			</li>
+		)
+	})
+
 	return (
 		<>
 			<MenuLayout
@@ -93,14 +149,11 @@ function GameLanding({setMenuSection}: Props) {
 								type="public"
 								selectedKey={mode}
 								setSelectedKey={setMode}
-								selectedDeck={deck}
+								selectedDeck={loadedDeck}
 							>
 								<div className={css.buttonMenu}>
-									<div>
-										{decks &&
-											decks.map((deck) => {
-												return <div>{deck.name}</div>
-											})}
+									<div className={css.decksContainer}>
+										<ul>{decksList}</ul>
 									</div>
 									<Button onClick={handeJoinQueue}>Join Queue</Button>
 								</div>
@@ -113,9 +166,14 @@ function GameLanding({setMenuSection}: Props) {
 								type="private"
 								selectedKey={mode}
 								setSelectedKey={setMode}
-								selectedDeck={deck}
+								selectedDeck={loadedDeck}
 							>
-								<div className={css.buttonMenu}>CHILD</div>
+								<div className={css.buttonMenu}>
+									<div className={css.decksContainer}>
+										<ul>{decksList}</ul>
+									</div>
+									<Button onClick={handlePrivateGame}>Join Queue</Button>
+								</div>
 							</HermitButton>
 							<HermitButton
 								image={'evilxisuma'}
@@ -125,9 +183,14 @@ function GameLanding({setMenuSection}: Props) {
 								type="boss"
 								selectedKey={mode}
 								setSelectedKey={setMode}
-								selectedDeck={deck}
+								selectedDeck={loadedDeck}
 							>
-								<div className={css.buttonMenu}>CHILD</div>
+								<div className={css.buttonMenu}>
+									<div className={css.decksContainer}>
+										<ul>{decksList}</ul>
+									</div>
+									<Button onClick={handleSoloGame}>Join Queue</Button>
+								</div>
 							</HermitButton>
 							<HermitButton
 								image={'geminitay'}
@@ -141,7 +204,15 @@ function GameLanding({setMenuSection}: Props) {
 								setSelectedKey={setMode}
 								selectedDeck={tutorialDeck}
 							>
-								<div className={css.buttonMenu}>CHILD</div>
+								<div className={css.buttonMenu}>
+									<div>
+										<p>
+											Join a game here to learn the rules of HC-TCG before
+											facing online opponents.
+										</p>
+									</div>
+									<Button onClick={handeJoinQueue}>Join Queue</Button>
+								</div>
 							</HermitButton>
 						</div>
 					</div>
