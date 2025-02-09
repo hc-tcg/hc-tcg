decff598d6f069cbd347d3ee7fca591d085c45d1
Lunarmagpie <Bambolambo0@gmail.com>
2025-02-09 18:01:10 -0500

    add jingler test

--- tests/unit/game/hermits/jingler-rare.test.ts
+++ tests/unit/game/hermits/jingler-rare.test.ts
@@ -29,7 +29,7 @@ import {
 import JinglerRare from 'common/cards/hermits/jingler-rare'
 
 describe('Test Jingler Rare', () => {
-	test('Test Jingler Forces Opponent To Discard Two Cards', () => {
+	test('Test Jingler forces opponent to discard one card', () => {
 		testGame(
 			{
 				playerOneDeck: [EthosLabCommon, EthosLabCommon],
@@ -42,6 +42,24 @@ describe('Test Jingler Rare', () => {
 					yield* attack(game, 'secondary')
 
 					yield* pick(game, query.slot.hand, query.slot.opponent)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+	test("Test Jingler does nothing when opponent's hand is empty", () => {
+		testGame(
+			{
+				playerOneDeck: [EthosLabCommon],
+				playerTwoDeck: [JinglerRare],
+				saga: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, JinglerRare, 'hermit', 0)
+					yield* attack(game, 'secondary')
+
+					expect(game.state.pickRequests).toHaveLength(0)
 				},
 			},
 			{startWithAllCards: true, noItemRequirements: true},
