4dbcc884920b94122278ee036ecd209ed2a323e9
ずんだアロー <Bambolambo0@gmail.com>
2025-02-19 16:32:48 -0500

    Certified Zombie Achievement (#1249)

---
+++ common/achievements/certified-zombie.ts
@@ -0,0 +1,37 @@
+import assert from 'assert'
+import ArmorStand from '../cards/attach/armor-stand'
+import {onTurnEnd} from '../types/priorities'
+import {achievement} from './defaults'
+import {Achievement} from './types'
+
+const CertifiedZombie: Achievement = {
+	...achievement,
+	id: 'certified_zombie',
+	numericId: 18,
+	name: 'Certified Zombie',
+	description:
+		'Survive with an armor stand as your active hermit for three rounds.',
+	steps: 3,
+	icon: 'certified_zombie',
+	onGameStart(game, playerEntity, component, observer) {
+		let player = game.components.get(playerEntity)
+		assert(player, 'Player should be in the ECS')
+
+		let rounds = 0
+
+		observer.subscribeWithPriority(
+			player.hooks.onTurnEnd,
+			onTurnEnd.ON_STATUS_EFFECT_TIMEOUT,
+			() => {
+				if (player.getActiveHermit()?.props.id === ArmorStand.id) {
+					rounds += 1
+					component.bestGoalProgress({goal: 0, progress: rounds})
+				} else {
+					rounds = 0
+				}
+			},
+		)
+	},
+}
+
+export default CertifiedZombie
