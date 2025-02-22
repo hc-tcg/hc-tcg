c89c8dbb5e6afcf4420af154d15618fe679ab11e
東方愛麗絲 <63879236+alicetouhou@users.noreply.github.com>
2025-02-21 21:22:41 -0500

    Mobile UI reworks (#1260)

---
+++ client/src/components/tabs/tabs.tsx
@@ -0,0 +1,35 @@
+import classNames from 'classnames'
+import css from './tabs.module.scss'
+
+function Tabs({
+	selected,
+	setSelected,
+	tabs,
+}: {
+	selected: string
+	setSelected: (str: any) => void
+	tabs: Array<string>
+}) {
+	return (
+		<div className={css.tabs}>
+			{tabs.map((tab) => {
+				return (
+					<div
+						className={classNames(
+							css.tab,
+							selected === tab ? css.selected : css.deselected,
+						)}
+						onClick={() => {
+							if (selected !== tab) setSelected(tab)
+						}}
+					>
+						{tab}
+					</div>
+				)
+			})}
+			<div className={css.afterTabs}></div>
+		</div>
+	)
+}
+
+export default Tabs
