911adaff27638d666aa6872a93e519d1a314ac1b
Lunarmagpie <Bambolambo0@gmail.com>
2025-02-22 16:09:36 -0500

    undo changes to goals

--- common/types/achievements.ts
+++ common/types/achievements.ts
@@ -1,6 +1,6 @@
 export type AchievementProgress = Record<number, ProgressionEntry>
 
 export type ProgressionEntry = {
-	goals: Record<number, Array<number>>
+	goals: Record<number, number>
 	completionTime?: Date
 }
