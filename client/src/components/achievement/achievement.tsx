23c10c4e92813e2554e3388fcf2049c03074eb97
東方愛麗絲 <63879236+alicetouhou@users.noreply.github.com>
2025-02-21 21:22:41 -0500

    Mobile UI reworks (#1260)

--- client/src/components/achievement/achievement.tsx
+++ client/src/components/achievement/achievement.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import {Achievement} from 'common/achievements/types'
 import {COSMETICS} from 'common/cosmetics'
 import {ProgressionEntry} from 'common/types/achievements'
@@ -25,31 +26,44 @@ export default function AchievementComponent({
 
 	return (
 		<div className={css.achievementContainer}>
-			<img src={icon_url} className={css.icon} />
-			<div>
+			<img
+				src={icon_url}
+				className={classNames(
+					css.icon,
+					iconCosmetic && iconCosmetic.type === 'background' && css.rounded,
+				)}
+			/>
+			<div className={css.meat}>
 				<div>
-					{achievement.name}
-					<div className={css.achievementDescription}>
-						{achievement.description}
+					<div>
+						{achievement.name}
+						<div className={css.achievementDescription}>
+							{achievement.description}
+						</div>
+					</div>
+					<div className={css.achievementPlayers}>
+						3.5% of players have this achievement
 					</div>
 				</div>
-				<div className={css.progressContainer}>
-					<progress
-						value={progress}
-						max={achievement.steps}
-						className={css.progressBar}
-					></progress>
-					<span>
-						{progress}/{achievement.steps}
-					</span>
+				<div>
+					<div className={css.progressContainer}>
+						<div>
+							{progress}/{achievement.steps}
+						</div>
+						<progress
+							value={progress}
+							max={achievement.steps}
+							className={css.progressBar}
+						></progress>
+					</div>
+					{completionTime ? (
+						<span>
+							Completed: {new Date(completionTime).toLocaleDateString()}
+						</span>
+					) : (
+						''
+					)}
 				</div>
-				{completionTime ? (
-					<span>
-						Completed: {new Date(completionTime).toLocaleDateString()}
-					</span>
-				) : (
-					''
-				)}
 			</div>
 		</div>
 	)
