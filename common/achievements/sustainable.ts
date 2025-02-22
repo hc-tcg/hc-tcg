19e8e06224ce07b4f77e59553db0dbb30f2cfcde
ずんだアロー <Bambolambo0@gmail.com>
2025-02-22 18:13:39 -0500

    SUStainable Achievement (#1264)
    Adds an achievement for discarding 100 cards with composter. My idea for
    the reward is "green" heart or "plant" heart but I would need help from
    someone like ChimeraDev for that, so i want approval of the idea first.

---
+++ common/achievements/sustainable.ts
@@ -0,0 +1,37 @@
+import assert from 'assert'
+import Composter from '../cards/single-use/composter'
+import {CardComponent} from '../components'
+import query from '../components/query'
+import {achievement} from './defaults'
+import {Achievement} from './types'
+
+const SUStainable: Achievement = {
+	...achievement,
+	numericId: 19,
+	id: 'sustainable',
+	name: 'SUStainable',
+	description: 'Compost 100 cards.',
+	steps: 100,
+	icon: 'sustainable',
+	onGameStart(game, playerEntity, component, observer) {
+		let player = game.components.get(playerEntity)!
+
+		observer.subscribe(player.hooks.afterApply, () => {
+			let su = game.components.find(
+				CardComponent,
+				query.card.slot(query.slot.singleUse),
+			)
+
+			assert(
+				su,
+				'There should be a single use card in the single use slot if a sigle use card is applied',
+			)
+
+			if (su.props.id !== Composter.id) return
+
+			component.incrementGoalProgress({goal: 0, amount: 2})
+		})
+	},
+}
+
+export default SUStainable
