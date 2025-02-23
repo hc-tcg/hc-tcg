97ef44b003d0ebbd0655173fbdc49e9e4d038f3c
Lunarmagpie <Bambolambo0@gmail.com>
2025-02-22 23:22:06 -0500

    Fix pesky-bird file name

---
+++ common/achievements/pesky-bird.ts
@@ -0,0 +1,35 @@
+import {CardComponent} from '../components'
+import {achievement} from './defaults'
+import {Achievement} from './types'
+
+const PeskyBird: Achievement = {
+	...achievement,
+	numericId: 10,
+	icon: '',
+	id: 'pesky_bird',
+	levels: [
+		{
+			name: 'Pesky Bird',
+			description: 'Force your opponent to discard 7 cards in one game.',
+			steps: 7,
+		},
+	],
+	onGameStart(game, playerEntity, component, observer) {
+		const player = game.components.get(playerEntity)
+		if (!player) return
+
+		let forcedDiscards = 0
+
+		// Let it be known that I hate this
+		game.components.filter(CardComponent).forEach((cardComponent) => {
+			observer.subscribe(cardComponent.hooks.onChangeSlot, (newSlot) => {
+				if (!newSlot.inDiscardPile()) return
+				if (game.currentPlayerEntity !== playerEntity) return
+				forcedDiscards += 1
+				component.bestGoalProgress({goal: 0, progress: forcedDiscards})
+			})
+		})
+	},
+}
+
+export default PeskyBird
