c27f2def2d9d6efb981a4feaa8a85bf765cb7646
Benji42 <cheezyanimations@gmail.com>
2025-02-22 23:23:21 -0500

    fix

--- common/achievements/wins.ts
+++ common/achievements/wins.ts
@@ -6,11 +6,11 @@ const Win: Achievement = {
 	numericId: 26,
 	id: 'wins',
 	levels: [
-		{name: 'Card Slinger', description: 'Win 1 Game', steps: 1},
-		{name: 'TCG Novice', description: 'Win 10 Games', steps: 10},
-		{name: 'TCG Apprentice', description: 'Win 100 Games', steps: 100},
-		{name: 'TCG Legend', description: 'Win 500 Games', steps: 500},
-		{name: 'TCG Champion', description: 'Win 1000 Games', steps: 1000},
+		{name: 'Card Slinger', description: 'Win 1 game.', steps: 1},
+		{name: 'TCG Novice', description: 'Win 10 games.', steps: 10},
+		{name: 'TCG Apprentice', description: 'Win 100 games.', steps: 100},
+		{name: 'TCG Legend', description: 'Win 500 games.', steps: 500},
+		{name: 'TCG Champion', description: 'Win 1000 games.', steps: 1000},
 	],
 	icon: '',
 	onGameEnd(_game, playerEntity, component, outcome) {
