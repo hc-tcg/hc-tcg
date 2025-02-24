3b20f09452a6ab42c7db04b1d0a7679943d2d569
ずんだアロー <Bambolambo0@gmail.com>
2025-02-24 16:08:45 -0500

    Add "Eye of the Spider" Achievement and Reward (#1275)

--- common/cosmetics/coins.ts
+++ common/cosmetics/coins.ts
@@ -1,3 +1,4 @@
+import EyeOfTheSpider from '../achievements/eye-of-the-spider'
 import NoDerpcoins from '../achievements/no-derpcoins'
 import PackOfWolves from '../achievements/pack-of-wolves'
 import ServerLag from '../achievements/server-lag'
@@ -17,6 +18,12 @@ const CoinDefinitions: Omit<Coin, 'type'>[] = [
 		name: 'Chicken',
 		borderColor: '#CD1414',
 		requires: undefined,
+	},
+	{
+		id: 'cave-spider',
+		name: 'Cave Spider',
+		borderColor: '#840000',
+		requires: {achievement: EyeOfTheSpider.id},
 	},
 	{
 		id: 'cod',
