f5dda70fe99c39992f4d03a3a9368dfabf0c7e30
東方愛麗絲 <63879236+alicetouhou@users.noreply.github.com>
2025-02-23 22:16:07 -0500

    Fix cosmetics not being set properly  (#1276)
    Co-authored-by: ずんだアロー <Bambolambo0@gmail.com>

--- client/src/logic/session/session-saga.ts
+++ client/src/logic/session/session-saga.ts
@@ -189,12 +189,15 @@ function* createUser(username: string): Generator<any, User> {
 export function* setupData(user: User) {
 	// Setup database info
 	const appearance: Appearance = {
-		title: TITLES[user.title || ''] || defaultAppearance.title,
-		coin: COINS[user.coin || ''] || defaultAppearance.coin,
-		heart: HEARTS[user.heart || ''] || defaultAppearance.heart,
-		background: BACKGROUNDS[user.title || ''] || defaultAppearance.background,
-		border: BORDERS[user.title || ''] || defaultAppearance.border,
+		title: TITLES[user.title || ''] ?? defaultAppearance.title,
+		coin: COINS[user.coin || ''] ?? defaultAppearance.coin,
+		heart: HEARTS[user.heart || ''] ?? defaultAppearance.heart,
+		background:
+			BACKGROUNDS[user.background || ''] ?? defaultAppearance.background,
+		border: BORDERS[user.border || ''] ?? defaultAppearance.border,
 	}
+
+	console.log(appearance)
 
 	yield* put<LocalMessage>({
 		type: localMessages.DATABASE_SET,
