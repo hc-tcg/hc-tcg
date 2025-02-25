e1b158773f8062bb0878cec0dd7d0797b1f8eb26
Lunarmagpie <Bambolambo0@gmail.com>
2025-02-25 11:25:57 -0500

    review

--- common/achievements/hurtin-hermits.ts
+++ common/achievements/hurtin-hermits.ts
@@ -4,8 +4,10 @@ import {Achievement} from './types'
 const HurtinHermits: Achievement = {
 	...achievement,
 	numericId: 41,
-	id: 'hertin-hermits',
-	levels: [{name: "Hurtin' Hermits", description: 'Win 1 game.', steps: 1}],
+	id: 'hertin_hermits',
+	levels: [
+		{name: "Hurtin' Hermits", description: 'Lose your first game.', steps: 1},
+	],
 	onGameEnd(_game, playerEntity, component, outcome) {
 		if (outcome.type !== 'player-won' || outcome.winner === playerEntity) return
 		component.incrementGoalProgress({goal: 0})
