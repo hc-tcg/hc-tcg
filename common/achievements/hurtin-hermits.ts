fe44fd8e05b1a59e2d2a059868e734882182286c
Lunarmagpie <Bambolambo0@gmail.com>
2025-02-24 23:01:01 -0500

    change

--- common/achievements/hurtin-hermits.ts
+++ common/achievements/hurtin-hermits.ts
@@ -7,7 +7,6 @@ const HurtinHermits: Achievement = {
 	id: 'hertin-hermits',
 	levels: [{name: "Hurtin' Hermits", description: 'Win 1 game.', steps: 1}],
 	onGameEnd(_game, playerEntity, component, outcome) {
-		console.log('Incrementing progress')
 		if (outcome.type !== 'player-won' || outcome.winner === playerEntity) return
 		component.incrementGoalProgress({goal: 0})
 	},
