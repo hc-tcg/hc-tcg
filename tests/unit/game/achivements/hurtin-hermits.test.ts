94685f34f62f0faff1a59d9d6473395a05e8c4a8
Lunarmagpie <Bambolambo0@gmail.com>
2025-02-24 23:05:50 -0500

    test passed

---
+++ tests/unit/game/achivements/hurtin-hermits.test.ts
@@ -0,0 +1,28 @@
+import {expect, test} from '@jest/globals'
+import EthosLabCommon from 'common/cards/hermits/ethoslab-common'
+import {attack, endTurn, playCardFromHand, testAchivement} from '../utils'
+import HurtinHermits from 'common/achievements/hurtin-hermits'
+
+test("Test Hurtin' Hermits achivement", () => {
+	testAchivement(
+		{
+			achievement: HurtinHermits,
+			playerOneDeck: [EthosLabCommon],
+			playerTwoDeck: [EthosLabCommon],
+			playGame: function* (game) {
+				yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+				yield* endTurn(game)
+				yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+				yield* endTurn(game)
+				yield* endTurn(game)
+				yield* attack(game, 'secondary')
+			},
+			checkAchivement(_game, achievement, _outcome) {
+				expect(
+					HurtinHermits.getProgress(achievement.goals),
+				).toBeGreaterThanOrEqual(HurtinHermits.levels[0].steps)
+			},
+		},
+		{oneShotMode: true, noItemRequirements: true},
+	)
+})
