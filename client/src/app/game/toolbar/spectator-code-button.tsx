0a462fe7b5cb8cfe554ca31f7cf4019b13db245c
Tyrannicodin <86689800+Tyrannicodin@users.noreply.github.com>
2025-03-04 00:41:28 +0000

    Make it look nicer

--- client/src/app/game/toolbar/spectator-code-button.tsx
+++ client/src/app/game/toolbar/spectator-code-button.tsx
@@ -1,17 +1,32 @@
 import {CopyIcon} from 'components/svgs'
 import css from './toolbar.module.scss'
+import { useDispatch } from 'react-redux'
+import { localMessages } from 'logic/messages'
 
 function SpectatorCodeButton({spectatorCode}: {spectatorCode?: string | null}) {
 	if (!spectatorCode) return null
+	const dispatch = useDispatch()
+
+	const handleCodeClick = () => {
+		navigator.clipboard.writeText(spectatorCode)
+
+		dispatch({
+			type: localMessages.TOAST_OPEN,
+			open: true,
+			title: 'Code copied!',
+			description: `Copied spectator code to clipboard.`,
+			image: 'copy',
+		})
+	}
+
 	return (
 		<button
 			className={css.item}
 			title="Copy Spectator Code"
-			onClick={() => {
-				navigator.clipboard.writeText(spectatorCode)
-			}}
+			onClick={handleCodeClick}
 		>
-			<div>
+			<div className={css.copy}>
+				{spectatorCode}
 				<CopyIcon />
 			</div>
 		</button>
