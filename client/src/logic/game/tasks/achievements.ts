2233d233ffeca1178ffeae1af357e161d04a6e01
Lunarmagpie <Bambolambo0@gmail.com>
2025-03-04 19:13:23 -0500

    fix tooltips not showing up

--- client/src/logic/game/tasks/achievements.ts
+++ client/src/logic/game/tasks/achievements.ts
@@ -1,5 +1,5 @@
-import { ACHIEVEMENTS } from 'common/achievements'
-import { ALL_COSMETICS } from 'common/cosmetics'
+import {ACHIEVEMENTS} from 'common/achievements'
+import {ALL_COSMETICS} from 'common/cosmetics'
 import {serverMessages} from 'common/socket-messages/server-messages'
 import {LocalMessage, localMessages} from 'logic/messages'
 import {receiveMsg} from 'logic/socket/socket-saga'
@@ -12,15 +12,15 @@ function* achievementSaga() {
 		const result = yield* call(
 			receiveMsg(socket, serverMessages.ACHIEVEMENT_COMPLETE),
 		)
-        const {achievementId, level, newProgress} = result.achievement
-		
-        const iconCosmetic = ALL_COSMETICS.find(
-            (x) =>
-                x.requires?.achievement === ACHIEVEMENTS[achievementId].id &&
-                (x.requires.level === level.index || !x.requires.level),
-        )
+		const {achievementId, level, newProgress} = result.achievement
 
-        let icon_url = undefined
+		const iconCosmetic = ALL_COSMETICS.find(
+			(x) =>
+				x.requires?.achievement === ACHIEVEMENTS[achievementId].id &&
+				(x.requires.level === level.index || !x.requires.level),
+		)
+
+		let icon_url = undefined
 
 		if (iconCosmetic && iconCosmetic.type === 'title') {
 			icon_url = '/images/cosmetics/title.png'
@@ -28,13 +28,13 @@ function* achievementSaga() {
 			icon_url = `/images/cosmetics/${iconCosmetic.type}/${iconCosmetic.type === 'background' && iconCosmetic.preview ? iconCosmetic.preview : iconCosmetic.id}.png`
 		}
 
-        yield* put<LocalMessage>({
+		yield* put<LocalMessage>({
 			type: localMessages.TOAST_OPEN,
-            open: true,
+			open: true,
 			title: 'Achievement Complete!',
-            description: `Completed ${level.name} (${newProgress}/${level.steps})`,
-            image: icon_url
-        })
+			description: `Completed ${level.name} (${newProgress}/${level.steps})`,
+			image: icon_url,
+		})
 	}
 }
 
