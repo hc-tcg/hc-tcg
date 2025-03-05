339193bb9ee65570e73d4a20dbf396a537f800a1
Lunarmagpie <Bambolambo0@gmail.com>
2025-03-05 11:56:40 -0500

    probably fix poe poe enforcer

--- common/achievements/poe-poe-enforcer.ts
+++ common/achievements/poe-poe-enforcer.ts
@@ -1,5 +1,4 @@
 import query from '../components/query'
-import {CardEntity} from '../entities'
 import CurseOfBindingEffect from '../status-effects/curse-of-binding'
 import {afterAttack} from '../types/priorities'
 import {achievement} from './defaults'
@@ -13,23 +12,11 @@ const PoePoeEnforcer: Achievement = {
 		{
 			name: 'Poe Poe Enforcer',
 			description:
-				'Knock out a hermit with the Curse of Binding status effect.',
+				'Knock out a hermit while your opponent has the Curse of Binding status effect.',
 			steps: 1,
 		},
 	],
 	onGameStart(game, player, component, observer) {
-		let bindedHermits: Set<CardEntity> = new Set()
-
-		observer.subscribe(player.hooks.onTurnStart, () => {
-			for (const hermitEntity in Object.values(bindedHermits)) {
-				let hermit = game.components.get(hermitEntity as CardEntity)!
-
-				if (!query.card.hasStatusEffect(CurseOfBindingEffect)(game, hermit)) {
-					bindedHermits.delete(hermit.entity)
-				}
-			}
-		})
-
 		observer.subscribeWithPriority(
 			game.hooks.afterAttack,
 			afterAttack.ACHIEVEMENTS,
@@ -37,17 +24,8 @@ const PoePoeEnforcer: Achievement = {
 				if (!attack.target) return
 				let targetHermit = attack.target?.getHermit()
 				if (!targetHermit) return
-
-				if (
-					query.card.hasStatusEffect(CurseOfBindingEffect)(game, targetHermit)
-				) {
-					bindedHermits.add(targetHermit.entity)
-				}
-
 				if (attack.target.health) return
-
-				if (bindedHermits.has(targetHermit.entity)) {
-					bindedHermits.delete(targetHermit.entity)
+				if (!query.player.hasStatusEffect(CurseOfBindingEffect)(game, player.opponentPlayer)) {
 					component.incrementGoalProgress({goal: 0})
 				}
 			},
