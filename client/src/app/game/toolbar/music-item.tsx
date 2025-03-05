823a814434498d3a307678b07632e6e427743c41
東方愛麗絲 <63879236+alicetouhou@users.noreply.github.com>
2025-03-04 23:45:02 -0500

    New audio icons on toolbar, courtesy of Chimera Dev (#1349)
    Co-authored-by: Lunarmagpie <Bambolambo0@gmail.com>

--- client/src/app/game/toolbar/music-item.tsx
+++ client/src/app/game/toolbar/music-item.tsx
@@ -1,4 +1,3 @@
-import {SpeakerIcon} from 'components/svgs'
 import {getSettings} from 'logic/local-settings/local-settings-selectors'
 import {localMessages, useMessageDispatch} from 'logic/messages'
 import {useSelector} from 'react-redux'
@@ -18,9 +17,14 @@ function MusicItem() {
 		})
 	}
 
+	const enable = settings.musicMuted ? 'disable' : 'enable'
+
 	return (
 		<button className={css.item} title="Mute Music" onClick={handleSoundChange}>
-			<SpeakerIcon level={settings.musicMuted ? 0 : 100} />
+			<img
+				className={css.audioIcon}
+				src={`/images/icons/music_${enable}.png`}
+			></img>
 		</button>
 	)
 }
