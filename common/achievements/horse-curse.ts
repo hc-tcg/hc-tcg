43e06839c817953943244d8e2e2de0e8ab9043a4
Lunarmagpie <Bambolambo0@gmail.com>
2025-03-06 12:47:40 -0500

    implement horse curse

--- common/achievements/horse-curse.ts
+++ common/achievements/horse-curse.ts
@@ -1,13 +1,5 @@
-import {
-	DiamondArmor,
-	GoldArmor,
-	IronArmor,
-	NetheriteArmor,
-} from '../cards/attach/armor'
-import Emerald from '../cards/single-use/emerald'
-import {SlotComponent} from '../components'
 import query from '../components/query'
-import {afterApply} from '../types/priorities'
+import SleepingEffect from '../status-effects/sleeping'
 import {achievement} from './defaults'
 import {Achievement} from './types'
 
@@ -18,12 +10,20 @@ const HorseCurse: Achievement = {
 	levels: [
 		{
 			name: 'Horse Curse',
-			description:
-				"KO a Hermit while your active Hermit is sleeping.",
+			description: 'KO a Hermit while your active Hermit is sleeping.',
 			steps: 1,
 		},
 	],
 	onGameStart(game, player, component, observer) {
+		game.components
+			.filter(RowComponent, query.row.player(player.opponentPlayer.entity))
+			.forEach((row) => {
+				observer.subscribe(row.hooks.onKnockOut, () => {
+					if (player.activeRow.getHermit().getStatusEffect(SleepingEffect)) {
+						component.incrementGoalProgress({goal: 0})
+					}
+				})
+			})
 	},
 }
 
