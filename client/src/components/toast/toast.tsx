296034d108d1b28069d663d149b3f0f0fafa6497
Tyrannicodin <86689800+Tyrannicodin@users.noreply.github.com>
2025-03-08 20:08:04 +0000

    Fix toasts

--- client/src/components/toast/toast.tsx
+++ client/src/components/toast/toast.tsx
@@ -186,7 +186,7 @@ const ToastContainer = ({children}: ContainerProps) => {
 }
 
 export const Toaster = () => {
-	const toastMessage = useSelector(getToast, (a, b) => a.length !== b.length)
+	const toastMessage = useSelector(getToast)
 	return (
 		<ToastContainer>
 			{toastMessage.map((toast, i) => {
