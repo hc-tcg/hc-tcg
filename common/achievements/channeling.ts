dceef7843df10b28457f57a6777ff3a83bbe6de8
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2025-03-08 23:51:25 -0500

    Initial improvements

--- common/achievements/channeling.ts
+++ common/achievements/channeling.ts
@@ -1,6 +1,6 @@
 import LightningRod from '../cards/attach/lightning-rod'
 import {CardComponent} from '../components'
-import {afterAttack} from '../types/priorities'
+import {afterAttack, beforeAttack} from '../types/priorities'
 import {achievement} from './defaults'
 import {Achievement} from './types'
 
@@ -17,29 +17,38 @@ const Channeling: Achievement = {
 		},
 	],
 	onGameStart(game, player, component, observer) {
+		const {opponentPlayer} = player
+
+		let damageRedirected = 0
+
+		observer.subscribe(opponentPlayer.hooks.onTurnStart, () => {
+			damageRedirected = 0
+		})
+
+		observer.subscribeWithPriority(
+			game.hooks.beforeAttack,
+			beforeAttack.RESOLVE_AFTER_MODIFIERS,
+			(attack) => {
+				if (attack.player.entity !== opponentPlayer.entity) return
+				const history = attack.getHistory('redirect').find((history) => {
+					let creator = game.components.get(history.source as any)
+					if (!(creator instanceof CardComponent)) return false
+					return creator.props.id === LightningRod.id
+				})
+				if (!history || history.value.from !== player.activeRowEntity) return
+				damageRedirected += attack.calculateDamage()
+			},
+		)
+
 		observer.subscribeWithPriority(
 			game.hooks.afterAttack,
 			afterAttack.ACHIEVEMENTS,
-			(attack) => {
-				if (attack.player.entity !== player.opponentPlayer.entity) return
-				const target = attack.target
-				if (target === player.activeRowEntity) return
-				if (!target || target.health) return
-				if (
-					!attack.getHistory('redirect').find((history) => {
-						let creator = game.components.get(history.source as any)
-						if (!(creator instanceof CardComponent)) return
-						return creator.props.id === LightningRod.id
-					})
-				)
-					return
+			(_attack) => {
+				if (!player.activeRow?.health) return
+				if (damageRedirected < player.activeRow.health) return
 
-				if (
-					player.activeRow?.health &&
-					attack.getDamage() > player.activeRow.health
-				) {
-					component.incrementGoalProgress({goal: 0})
-				}
+				component.incrementGoalProgress({goal: 0})
+				damageRedirected = 0
 			},
 		)
 	},
--- tests/unit/game/achievements/channeling.test.ts
+++ tests/unit/game/achievements/channeling.test.ts
@@ -3,15 +3,14 @@ import Channeling from 'common/achievements/channeling'
 import LightningRod from 'common/cards/attach/lightning-rod'
 import EthosLabCommon from 'common/cards/hermits/ethoslab-common'
 import GoatfatherRare from 'common/cards/hermits/goatfather-rare'
-import {RowComponent} from 'common/components'
 import {
 	attack,
-	changeActiveHermit,
 	endTurn,
 	forfeit,
 	playCardFromHand,
 	testAchivement,
 } from '../utils'
+import Anvil from 'common/cards/single-use/anvil'
 
 describe('Test Channeling achievement', () => {
 	test('"Channeling" increases properly', () => {
@@ -19,7 +18,7 @@ describe('Test Channeling achievement', () => {
 			{
 				achievement: Channeling,
 				playerOneDeck: [EthosLabCommon, EthosLabCommon, LightningRod],
-				playerTwoDeck: [GoatfatherRare],
+				playerTwoDeck: [GoatfatherRare, Anvil],
 				playGame: function* (game) {
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
@@ -27,10 +26,8 @@ describe('Test Channeling achievement', () => {
 					yield* endTurn(game)
 
 					yield* playCardFromHand(game, GoatfatherRare, 'hermit', 0)
-
-					for (const hermit of game.components.filter(RowComponent)) {
-						if (hermit.health) hermit.health = 10
-					}
+					yield* playCardFromHand(game, Anvil, 'single_use')
+					game.opponentPlayer.activeRow!.health = 140
 
 					yield* attack(game, 'secondary')
 
@@ -48,15 +45,16 @@ describe('Test Channeling achievement', () => {
 			{
 				achievement: Channeling,
 				playerOneDeck: [EthosLabCommon, EthosLabCommon, LightningRod],
-				playerTwoDeck: [GoatfatherRare],
+				playerTwoDeck: [GoatfatherRare, Anvil],
 				playGame: function* (game) {
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
 					yield* playCardFromHand(game, LightningRod, 'attach', 1)
 					yield* endTurn(game)
 
-					yield* playCardFromHand(game, GoatfatherRare, 'hermit', 0)
-
+					yield* playCardFromHand(game, GoatfatherRare, 'hermit', 1)
+					yield* playCardFromHand(game, Anvil, 'single_use')
+					game.opponentPlayer.activeRow!.health = 120
 					yield* attack(game, 'secondary')
 
 					yield* forfeit(game.opponentPlayer.entity)
@@ -77,19 +75,11 @@ describe('Test Channeling achievement', () => {
 				playGame: function* (game) {
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
-					yield* playCardFromHand(game, LightningRod, 'attach', 1)
-
+					yield* playCardFromHand(game, LightningRod, 'attach', 0)
 					yield* endTurn(game)
+
 					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
-					yield* endTurn(game)
-
-					yield* changeActiveHermit(game, 1)
-					yield* endTurn(game)
-
-					for (const hermit of game.components.filter(RowComponent)) {
-						if (hermit.health) hermit.health = 10
-					}
-
+					game.opponentPlayer.activeRow!.health = 10
 					yield* attack(game, 'secondary')
 
 					yield* forfeit(game.opponentPlayer.entity)
