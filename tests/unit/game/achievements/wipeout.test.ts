d89c6879a3a420b4bbc5ec83f07bbf5ee3017c85
Lunarmagpie <Bambolambo0@gmail.com>
2025-03-09 17:08:21 -0400

    lava bucket

--- tests/unit/game/achievements/wipeout.test.ts
+++ tests/unit/game/achievements/wipeout.test.ts
@@ -5,12 +5,16 @@ import Anvil from 'common/cards/single-use/anvil'
 import {RowComponent} from 'common/components'
 import query from 'common/components/query'
 import {
+	applyEffect,
 	attack,
+	changeActiveHermit,
 	endTurn,
 	forfeit,
 	playCardFromHand,
 	testAchivement,
 } from '../utils'
+import LavaBucket from 'common/cards/single-use/lava-bucket'
+import {applySingleUse} from 'common/utils/board'
 
 describe('Test "Wipeout" achievement', () => {
 	test('Works when game ends when all Hermits are knocked out and game ends', () => {
@@ -62,6 +66,42 @@ describe('Test "Wipeout" achievement', () => {
 					yield* playCardFromHand(game, Anvil, 'single_use')
 					yield* attack(game, 'secondary')
 
+					yield* endTurn(game)
+
+					yield* forfeit(game.currentPlayer.entity)
+				},
+				checkAchivement(_game, achievement, _outcome) {
+					expect(Wipeout.getProgress(achievement.goals)).toEqual(2)
+				},
+			},
+			{noItemRequirements: true},
+		)
+	})
+	test('Works when killed by Lava Bucket', () => {
+		testAchivement(
+			{
+				achievement: Wipeout,
+				playerOneDeck: [EthosLabCommon, LavaBucket],
+				playerTwoDeck: [EthosLabCommon, EthosLabCommon, EthosLabCommon],
+				playGame: function* (game) {
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* endTurn(game)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 0)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 1)
+					yield* playCardFromHand(game, EthosLabCommon, 'hermit', 2)
+					yield* endTurn(game)
+
+					yield* playCardFromHand(game, LavaBucket, 'single_use')
+					yield* applyEffect(game)
+					yield* endTurn(game)
+
+					yield* changeActiveHermit(game, 1)
+					yield* endTurn(game)
+
+					game.components
+						.filter(RowComponent, query.row.hasHermit)
+						.forEach((row) => (row.health = 10))
+					yield* attack(game, 'secondary')
 					yield* endTurn(game)
 
 					yield* forfeit(game.currentPlayer.entity)
