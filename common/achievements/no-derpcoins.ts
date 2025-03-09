14dc07622c34e5494b65d12f01983c4a39d039fe
Lunarmagpie <Bambolambo0@gmail.com>
2025-03-08 22:58:19 -0500

    fix

--- common/achievements/no-derpcoins.ts
+++ common/achievements/no-derpcoins.ts
@@ -8,7 +8,7 @@ const NoDerpcoins: Achievement = {
 	...achievement,
 	numericId: 7,
 	id: 'no_derpcoins',
-	progressInBossGame: true,
+	evilXAchievement: true,
 	levels: [
 		{
 			name: 'No Derpcoins Required',
@@ -17,12 +17,11 @@ const NoDerpcoins: Achievement = {
 		},
 	],
 	onGameStart(game, player, component, observer) {
-		if (!game.state.isEvilXBossGame) return
 		const cost = getDeckCost(
-			game.components
-				.filter(CardComponent, query.card.player(player.entity))
+			player.getDeck()
 				.map((card) => card.props),
 		)
+		console.log(cost)
 		if (cost > 0) return
 		observer.subscribe(game.hooks.onGameEnd, (outcome) => {
 			if (outcome.type !== 'player-won') return
