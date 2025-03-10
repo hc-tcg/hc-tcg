0e5000d097f5acc577a2b574844c272e8b91e412
Benji42 <cheezyanimations@gmail.com>
2025-03-09 21:47:53 -0400

    lint

--- tests/unit/game/achievements/use-like-a-hermit.test.ts
+++ tests/unit/game/achievements/use-like-a-hermit.test.ts
@@ -1,8 +1,8 @@
 import {describe, expect, test} from '@jest/globals'
+import UseLikeAHermit from 'common/achievements/use-like-a-hermit'
 import ArmorStand from 'common/cards/attach/armor-stand'
 import EthosLabCommon from 'common/cards/hermits/ethoslab-common'
 import {endTurn, forfeit, playCardFromHand, testAchivement} from '../utils'
-import UseLikeAHermit from 'common/achievements/use-like-a-hermit'
 
 describe('Test Use Like a Hermit achievement', () => {
 	test('Positive Case', () => {
