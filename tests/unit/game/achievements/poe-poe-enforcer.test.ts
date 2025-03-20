a3cdbb15a6bfc553bc3acbad8e337bddb96569dd
Rvtar <shinboon.boon@gmail.com>
2025-03-19 21:50:14 -0400

    Update poe-poe-enforcer.test.ts

--- tests/unit/game/achievements/poe-poe-enforcer.test.ts
+++ tests/unit/game/achievements/poe-poe-enforcer.test.ts
@@ -1,7 +1,7 @@
 import {describe, expect, test} from '@jest/globals'
 import PoePoeEnforcer from 'common/achievements/poe-poe-enforcer'
 import ImpulseSVCommon from 'common/cards/hermits/impulsesv-common'
-import GrianCommon from 'common/cards/hermits/grian-common'
+import JoeHillsCommon from 'common/cards/hermits/joehills-common'
 import CurseOfBinding from 'common/cards/single-use/curse-of-binding'
 import {
 	applyEffect,
@@ -41,12 +41,12 @@ describe('Test Poe Poe Enforcer Achievement', () => {
 			{
 				achievement: PoePoeEnforcer,
 				playerOneDeck: [ImpulseSVCommon, CurseOfBinding],
-				playerTwoDeck: [GrianCommon, ImpulseSVCommon],
+				playerTwoDeck: [JoeHillsCommon, ImpulseSVCommon],
 				playGame: function* (game) {
 					yield* playCardFromHand(game, ImpulseSVCommon, 'hermit', 0)
 					yield* endTurn(game)
 
-					yield* playCardFromHand(game, GrianCommon, 'hermit', 0)
+					yield* playCardFromHand(game, JoeHillsCommon, 'hermit', 0)
 					yield* endTurn(game)
 
 					yield* playCardFromHand(game, CurseOfBinding, 'single_use')
