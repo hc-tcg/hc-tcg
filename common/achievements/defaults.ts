acf1baa501c1ad725480df5014393c2494b0506e
Lunarmagpie <Bambolambo0@gmail.com>
2025-03-23 00:29:17 -0400

    omit progression method

--- common/achievements/defaults.ts
+++ common/achievements/defaults.ts
@@ -2,12 +2,11 @@ import {Achievement} from './types'
 
 export const achievement: Omit<
 	Achievement,
-	'id' | 'numericId' | 'levels' | 'icon'
+	'id' | 'numericId' | 'levels' | 'icon' | 'progressionMethod'
 > = {
 	getProgress(goals: Record<number, number>) {
 		return goals[0]
 	},
 	onGameStart() {},
 	onGameEnd() {},
-	progressionMethod: 'best',
 }
