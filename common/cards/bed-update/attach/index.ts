83700d297d2a918aea601758f998c34532cd7ee9
Rvtar <shinboon.boon@gmail.com>
2025-03-27 23:26:38 -0400

    Bed

---
+++ common/cards/bed-update/attach/index.ts
@@ -0,0 +1,12 @@
+import {Card} from '../../types'
+import HealthyBed from './healthy-bed'
+import PowerBed from './power-bed'
+import ReallySleepyBed from './really-sleepy-bed'
+
+const effectCardClasses: Array<Card> = [
+    PowerBed,
+    ReallySleepyBed,
+    HealthyBed
+]
+
+export default effectCardClasses
