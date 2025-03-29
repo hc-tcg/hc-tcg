4c59e2ea54de7c7a8e28009f708beda9eacb5071
Tyrannicodin <86689800+Tyrannicodin@users.noreply.github.com>
2025-03-29 21:53:25 +0000

    format

--- common/cards/bed-update/single-use/exile-bed.ts
+++ common/cards/bed-update/single-use/exile-bed.ts
@@ -1,6 +1,10 @@
-import {CardComponent, SlotComponent, StatusEffectComponent} from '../../../components'
+import {
+	CardComponent,
+	SlotComponent,
+	StatusEffectComponent,
+} from '../../../components'
 import query from '../../../components/query'
-import Exiled from '../../../status-effects/exiled'
+import ExiledEffect from '../../../status-effects/exiled'
 import {singleUse} from '../../defaults'
 import {SingleUse} from '../../types'
 
@@ -21,15 +25,15 @@ const ExileBed: SingleUse = {
 	],
 	attachCondition: query.every(
 		singleUse.attachCondition,
-        query.exists(
-            SlotComponent,
-            query.slot.opponent,
+		query.exists(
+			SlotComponent,
+			query.slot.opponent,
 			query.slot.hermit,
 			query.not(query.slot.active),
 			query.not(query.slot.empty),
 			query.not(query.slot.frozen),
 			query.slot.canBecomeActive,
-        )
+		),
 	),
 	showConfirmationModal: true,
 	onAttach(game, component, observer) {
@@ -39,53 +43,52 @@ const ExileBed: SingleUse = {
 			const row = opponentPlayer.activeRow
 			if (!row) return
 
-            const discardRow = () => {
-                const toDiscard: (CardComponent | null)[] = [
-				    ...row.getItems(),
-				    row.hermitSlot.card,
-				    row.attachSlot.card,
-			    ]
-			    toDiscard.forEach((card) => {
-			    	if (!card) return
-			    	card.discard()
-			    	if (card.props.category !== 'hermit') return
-			    	const status = game.components.new(
-			    		StatusEffectComponent,
-			    		Exiled,
-			    		entity,
-			    	)
-			    	status.counter = row.index
-			    	status.apply(card.entity)
-			    })
-                row.health = null
-            }
+			const discardRow = () => {
+				const toDiscard: (CardComponent | null)[] = [
+					...row.getItems(),
+					row.hermitSlot.card,
+					row.attachSlot.card,
+				]
+				toDiscard.forEach((card) => {
+					if (!card) return
+					card.discard()
+					if (card.props.category !== 'hermit') return
+					const status = game.components.new(
+						StatusEffectComponent,
+						ExiledEffect,
+						entity,
+					)
+					status.counter = row.index
+					status.apply(card.entity)
+				})
+				row.health = null
+			}
 
-            let knockbackPickRequest =
+			let knockbackPickRequest =
 				opponentPlayer.getKnockbackPickRequest(component)
 			if (!knockbackPickRequest) return
-            knockbackPickRequest.onResult = (pickedSlot) => {
+			knockbackPickRequest.onResult = (pickedSlot) => {
 				if (!pickedSlot.inRow()) return
 				opponentPlayer.knockback(pickedSlot.row)
-                discardRow()
-            }
-            knockbackPickRequest.onTimeout = () => {
+				discardRow()
+			}
+			knockbackPickRequest.onTimeout = () => {
 				const slot = component.game.components.find(
 					SlotComponent,
 					query.every(
-                        query.slot.player(opponentPlayer.entity),
-                        query.slot.hermit,
-                        query.not(query.slot.active),
-                        query.not(query.slot.empty),
-                        query.not(query.slot.frozen),
-                        query.slot.canBecomeActive,
-                    ),
+						query.slot.player(opponentPlayer.entity),
+						query.slot.hermit,
+						query.not(query.slot.active),
+						query.not(query.slot.empty),
+						query.not(query.slot.frozen),
+						query.slot.canBecomeActive,
+					),
 				)
 				if (!slot?.inRow()) return
 				opponentPlayer.knockback(slot.row)
-                discardRow()
-            }
-            game.addPickRequest(knockbackPickRequest)
-            
+				discardRow()
+			}
+			game.addPickRequest(knockbackPickRequest)
 		})
 	},
 }
--- common/status-effects/exiled.ts
+++ common/status-effects/exiled.ts
@@ -1,5 +1,5 @@
 import EnderPearl from '../cards/single-use/ender-pearl'
-import { Hermit } from '../cards/types'
+import {Hermit} from '../cards/types'
 import {
 	CardComponent,
 	ObserverComponent,
@@ -13,7 +13,6 @@ import {hiddenStatusEffect, StatusEffect} from './status-effect'
 const ExiledEffect: StatusEffect<CardComponent> = {
 	...hiddenStatusEffect,
 	id: 'exiled',
-	icon: 'exiled',
 	name: 'Exiled',
 	description:
 		'This hermit and any attached cards are discarded without losing a life.\nThe hermit can be recovered by using a bed card or ender pearl if the slot they were discarded from is empty.',
@@ -29,32 +28,33 @@ const ExiledEffect: StatusEffect<CardComponent> = {
 			const returnSlot = game.components.find(
 				SlotComponent,
 				query.slot.rowIndex(effect.counter),
-                query.slot.player(target.player.entity),
-                query.slot.hermit,
+				query.slot.player(target.player.entity),
+				query.slot.hermit,
 				query.slot.empty,
 			)
-            if (!returnSlot || !returnSlot.inRow()) return
-            returnSlot.row.health = (target.props as Hermit).health
-            target.attach(returnSlot)
+			if (!returnSlot || !returnSlot.inRow()) return
+			returnSlot.row.health = (target.props as Hermit).health
+			target.attach(returnSlot)
 		}
 
 		observer.subscribe(player.hooks.onApply, () => {
-            const id = game.components.find(SlotComponent, query.slot.singleUse)?.card?.props?.id
-            if (!id) return
-            if (!(id.includes('bed') || id === EnderPearl.id)) return
+			const id = game.components.find(SlotComponent, query.slot.singleUse)?.card
+				?.props?.id
+			if (!id) return
+			if (!(id.includes('bed') || id === EnderPearl.id)) return
 
-            recoverCard()
-            effect.remove()
+			recoverCard()
+			effect.remove()
 		})
 
-        observer.subscribe(player.hooks.onAttach, (card) => {
-            const {id} = card.props
-            if (!id) return
-            if (!(id.includes('bed') || id === EnderPearl.id)) return
+		observer.subscribe(player.hooks.onAttach, (card) => {
+			const {id} = card.props
+			if (!id) return
+			if (!(id.includes('bed') || id === EnderPearl.id)) return
 
-            recoverCard()
-            effect.remove()
-        })
+			recoverCard()
+			effect.remove()
+		})
 	},
 }
 
