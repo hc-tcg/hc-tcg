72eb61e9903fb4aa95391dd7c544086d54068cc8
Rvtar <shinboon.boon@gmail.com>
2025-03-29 19:45:24 -0400

    Update disappointing-bed.ts

--- common/cards/bed-update/single-use/disappointing-bed.ts
+++ common/cards/bed-update/single-use/disappointing-bed.ts
@@ -1,10 +1,9 @@
 import {
 	CardComponent,
 	ObserverComponent,
-	StatusEffectComponent,
 } from '../../../components'
 import {GameModel} from '../../../models/game-model'
-import DisappointingSwapEffect from '../../../status-effects/disappointing-swap'
+import {beforeAttack, onTurnEnd} from '../../../types/priorities'
 import {singleUse} from '../../defaults'
 import {SingleUse} from '../../types'
 
@@ -27,21 +26,44 @@ const DisappointingBed: SingleUse = {
 	) {
 		const {player, opponentPlayer} = component
 
+		let playerActiveRow = player.activeRow
+		let opponentActiveRow = opponentPlayer.activeRow
+
+		let swappedBack = false
+
 		observer.subscribe(player.hooks.onApply, () => {
-			if (!player.activeRow || !player.activeRow.health || !opponentPlayer.activeRow || !opponentPlayer.activeRow.health) return
-			let placeholder = player.activeRow.health
-			player.activeRow.health = opponentPlayer.activeRow.health
-			opponentPlayer.activeRow.health = placeholder
-
-			game.components
-				.new(StatusEffectComponent, DisappointingSwapEffect, component.entity)
-				.apply(player.entity)
-
-			if (component.slot.onBoard()) component.discard()
-			// Remove playing a single use from completed actions so it can be done again
-			game.removeCompletedActions('PLAY_SINGLE_USE_CARD')
-			player.singleUseCardUsed = false
+			if (!playerActiveRow || !opponentActiveRow) return
+			if (!playerActiveRow.health || !opponentActiveRow.health) return
+			let placeholder = playerActiveRow.health
+			playerActiveRow.health = opponentActiveRow.health
+			opponentActiveRow.health = placeholder
 		})
+
+		observer.subscribeWithPriority(
+			game.hooks.beforeAttack,
+			beforeAttack.REACT_TO_DAMAGE,
+			() => {
+				if (!playerActiveRow || !opponentActiveRow) return
+				if (!playerActiveRow.health || !opponentActiveRow.health) return
+				let placeholder = playerActiveRow.health
+				playerActiveRow.health = opponentActiveRow.health
+				opponentActiveRow.health = placeholder
+				swappedBack = true
+			}
+		)
+
+		observer.subscribeWithPriority(
+			player.hooks.onTurnEnd,
+			onTurnEnd.ON_STATUS_EFFECT_TIMEOUT,
+			() => {
+				if (swappedBack) return
+				if (!playerActiveRow || !opponentActiveRow) return
+				if (!playerActiveRow.health || !opponentActiveRow.health) return
+				let placeholder = playerActiveRow.health
+				playerActiveRow.health = opponentActiveRow.health
+				opponentActiveRow.health = placeholder
+			}
+		)
 	},
 }
 
