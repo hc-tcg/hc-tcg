b32df063242a63cdd80a8ed0584cfed74e39ebce
Rvtar <106639908+Rvtar@users.noreply.github.com>
2025-03-30 12:45:28 -0400

    Some SU Beds (#1474)

---
+++ common/cards/bed-update/single-use/index.ts
@@ -0,0 +1,12 @@
+import {Card} from '../../types'
+import BlockingBed from './blocking-bed'
+import CowardsBed from "./coward's-bed"
+import DisappointingBed from './disappointing-bed'
+
+const effectCardClasses: Array<Card> = [
+	CowardsBed,
+	BlockingBed,
+	DisappointingBed,
+]
+
+export default effectCardClasses
