1c433e9df97200aeea33c9fa8db9727ac3bc1dd5
Lunarmagpie <Bambolambo0@gmail.com>
2025-07-20 11:37:45 -0400

    tests for saving and loading bow

--- tests/unit/game/saveload/saveload.test.ts
+++ tests/unit/game/saveload/saveload.test.ts
@@ -6,7 +6,7 @@ import query from 'common/components/query'
 import {testGame} from '../utils'
 
 describe('Test Saving and Loading', () => {
-	test('Test bow', async () => {
+	test('Test save and load bow when on game field', async () => {
 		await testGame(
 			{
 				playerOneDeck: [EthosLabCommon, Bow],
@@ -29,6 +29,48 @@ describe('Test Saving and Loading', () => {
 					let bowData = bowCard.save()
 					game.components.delete(bowCard.entity)
 					game.components.load(CardComponent, bowData)
+
+					await test.attack('single-use')
+					await test.pick(
+						query.slot.rowIndex(1),
+						query.slot.hermit,
+						query.slot.opponent,
+					)
+
+					expect(
+						game.components.find(
+							RowComponent,
+							query.row.index(1),
+							query.row.opponentPlayer,
+						)?.health,
+					).toBe(EthosLabCommon.health - 40)
+				},
+			},
+			{startWithAllCards: true, noItemRequirements: true},
+		)
+	})
+	test('Test save and load bow when in hand', async () => {
+		await testGame(
+			{
+				playerOneDeck: [EthosLabCommon, Bow],
+				playerTwoDeck: [EthosLabCommon, EthosLabCommon],
+				testGame: async (test, game) => {
+					await test.playCardFromHand(EthosLabCommon, 'hermit', 0)
+					await test.endTurn()
+
+					const bowCard = game.components.find(
+						CardComponent,
+						(_g, c) => c.props.id === Bow.id,
+					)!
+					let bowData = bowCard.save()
+					game.components.delete(bowCard.entity)
+					game.components.load(CardComponent, bowData)
+
+					await test.playCardFromHand(EthosLabCommon, 'hermit', 0)
+					await test.playCardFromHand(EthosLabCommon, 'hermit', 1)
+					await test.endTurn()
+
+					await test.playCardFromHand(Bow, 'single_use')
 
 					await test.attack('single-use')
 					await test.pick(
