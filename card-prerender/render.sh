b7a6f0cff267e771dc71d1cf002139949a258283
Zunda <mailme@jacobhaussman.online>
2025-09-13 05:09:11 -0400

    maybe fix

--- card-prerender/render.sh
+++ card-prerender/render.sh
@@ -10,7 +10,7 @@ npx playwright test -c playwright-render.config.ts -u
 
 echo 'Writing WEBP  and PNG files to `client/public/images/cards/`...'
 mkdir client/public/images/cards || true
-find card-prerender/render/ -type f -print0 | xargs '-I{}' -n1 -0 sh -c 'magick {} client/public/images/cards/$(basename --suffix=.png {}).webp'
+find card-prerender/render/ -type f -print0 | xargs '-I{}' -n1 -0 sh -c 'convert {} client/public/images/cards/$(basename --suffix=.png {}).webp'
 find card-prerender/render/ -type f -print0 | xargs '-I{}' -n1 -0 sh -c 'cp {} client/public/images/cards/$(basename {})'
 
 echo "DONE!"
