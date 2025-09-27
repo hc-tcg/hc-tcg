171342f84065b5ff5d3193d6b9fe9e7d76917114
Tyrannicodin <tyrannicodin@gmail.com>
2025-09-27 11:45:58 +0100

    Lint

--- common/achievements/space-race.ts
+++ common/achievements/space-race.ts
@@ -5,25 +5,24 @@ import {achievement} from './defaults'
 import {Achievement} from './types'
 
 const SpaceRace: Achievement = {
-    ...achievement,
-    numericId: 70,
-    id: 'space-race',
-    progressionMethod: 'best',
-    levels: [
-        {
-            name: "Space Race",
-            description:
-                "Knockout 2 Hermits using the same Renbob Rare card",
-            steps: 2,
-        },
-    ],
-    onGameStart(game, player, component, observer) {
-        const kos: Record<
-            CardEntity | StatusEffectEntity, // We will only track renbob, but for type safety...
-            number
-        > = {}
+	...achievement,
+	numericId: 70,
+	id: 'space-race',
+	progressionMethod: 'best',
+	levels: [
+		{
+			name: 'Space Race',
+			description: 'Knockout 2 Hermits using the same Renbob Rare card',
+			steps: 2,
+		},
+	],
+	onGameStart(game, player, component, observer) {
+		const kos: Record<
+			CardEntity | StatusEffectEntity, // We will only track renbob, but for type safety...
+			number
+		> = {}
 
-        observer.subscribeWithPriority(
+		observer.subscribeWithPriority(
 			game.hooks.afterAttack,
 			afterAttack.ACHIEVEMENTS,
 			(attack) => {
@@ -31,22 +30,22 @@ const SpaceRace: Achievement = {
 				if (!attack.target) return
 				let targetHermit = attack.target?.getHermit()
 				if (!targetHermit) return
-                if (!targetHermit.isHermit()) return // Should this also include berry bush, armor stand etc.?
+				if (!targetHermit.isHermit()) return // Should this also include berry bush, armor stand etc.?
 
-                const attacker = attack.attacker
-                if (!attacker) return
+				const attacker = attack.attacker
+				if (!attacker) return
 				if (attacker.props !== RenbobRare) return
 
 				if (attack.target.health) return
 
-                if (!kos[attacker.entity]) {
-                    kos[attacker.entity] = 0
-                }
-                kos[attacker.entity] += 1
+				if (!kos[attacker.entity]) {
+					kos[attacker.entity] = 0
+				}
+				kos[attacker.entity] += 1
 				component.updateGoalProgress({goal: 0, progress: kos[attacker.entity]})
 			},
 		)
-    }
+	},
 }
 
 export default SpaceRace
