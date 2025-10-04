f69d9e0cbbf5d0cd016ff2848f1e5a68f2a1a752
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2025-10-04 18:16:16 -0400

    Final tests, ready for review

--- common/achievements/turtle-master.ts
+++ common/achievements/turtle-master.ts
@@ -1,13 +1,17 @@
 import assert from 'assert'
 import TurtleShell from '../cards/attach/turtle-shell'
-import {CardComponent, StatusEffectComponent} from '../components'
+import Ladder from '../cards/single-use/ladder'
+import {
+	CardComponent,
+	PlayerComponent,
+	StatusEffectComponent,
+} from '../components'
 import query from '../components/query'
 import {CardEntity} from '../entities'
 import LooseShellEffect from '../status-effects/loose-shell'
 import {afterApply, onTurnEnd} from '../types/priorities'
 import {achievement} from './defaults'
 import {Achievement} from './types'
-import Ladder from '../cards/single-use/ladder'
 
 const TurtleMaster: Achievement = {
 	...achievement,
@@ -25,58 +29,68 @@ const TurtleMaster: Achievement = {
 	onGameStart(game, player, component, observer) {
 		const looseShells: Set<CardEntity> = new Set()
 
-		// Detects when Turtle Shell is attached to an already active hermit
-		observer.subscribe(player.hooks.onAttach, (card) => {
-			if (card.props.id !== TurtleShell.id) return
-			if (card.player.entity !== player.entity) return
+		game.components.filter(PlayerComponent).forEach((player) => {
+			// Detects when Turtle Shell is attached to an already active hermit
+			observer.subscribe(player.hooks.onAttach, (card) => {
+				if (card.props.id !== TurtleShell.id) return
+				if (card.player.entity !== player.entity) return
 
-			observer.subscribe(card.hooks.onChangeSlot, () => {
-				observer.unsubscribe(card.hooks.onChangeSlot)
-				if (
-					game.components.exists(
-						StatusEffectComponent,
-						query.effect.is(LooseShellEffect),
-						query.not(query.effect.targetEntity(null)),
-						(_game, value) => value.creatorEntity === card.entity,
+				observer.subscribe(card.hooks.onChangeSlot, () => {
+					observer.unsubscribe(card.hooks.onChangeSlot)
+					if (
+						game.components.exists(
+							StatusEffectComponent,
+							query.effect.is(LooseShellEffect),
+							query.not(query.effect.targetEntity(null)),
+							(_game, value) => value.creatorEntity === card.entity,
+						)
 					)
-				)
-					looseShells.add(card.entity)
+						looseShells.add(card.entity)
+				})
 			})
-		})
 
-		observer.subscribeWithPriority(
-			player.hooks.afterApply,
-			afterApply.CHECK_BOARD_STATE,
-			() => {
-				const su = game.components.find(
-					CardComponent,
-					query.card.slot(query.slot.singleUse),
-				)
+			// Detects when active Hermit changes rows from Ladder to row with Turtle Shell
+			observer.subscribeWithPriority(
+				player.hooks.afterApply,
+				afterApply.CHECK_BOARD_STATE,
+				() => {
+					const su = game.components.find(
+						CardComponent,
+						query.card.slot(query.slot.singleUse),
+					)
 
-				assert(
-					su,
-					'There should be a single use card in the single use slot if a sigle use card is applied',
-				)
+					assert(
+						su,
+						'There should be a single use card in the single use slot if a sigle use card is applied',
+					)
 
-				if (su.props.id !== Ladder.id) return
+					if (su.props.id !== Ladder.id) return
 
-				observer.subscribe(
-					player.hooks.onActiveRowChange,
-					(_oldHermit, newHermit) => {
-						observer.unsubscribe(player.hooks.onActiveRowChange)
+					observer.subscribe(
+						player.hooks.onActiveRowChange,
+						(_oldHermit, newHermit) => {
+							observer.unsubscribe(player.hooks.onActiveRowChange)
 
-						const looseShell = newHermit.getStatusEffect(LooseShellEffect)
-						if (!looseShell) return
+							const looseShell = newHermit.getStatusEffect(LooseShellEffect)
+							if (!looseShell) return
 
-						looseShells.add(looseShell.creatorEntity)
-					},
-				)
-			},
-		)
+							looseShells.add(looseShell.creatorEntity)
+						},
+					)
+				},
+			)
 
-		// If the Turtle Shell is removed from the board, or given to other player, it no longer counts
-		observer.subscribe(player.hooks.onDetach, (card) => {
-			looseShells.delete(card.entity)
+			// If the Turtle Shell is removed from the board, it no longer counts
+			observer.subscribe(player.hooks.onDetach, (card) => {
+				if (card.props.id !== TurtleShell.id) return
+
+				observer.subscribe(card.hooks.onChangeSlot, (newSlot) => {
+					if (newSlot.onBoard()) return
+
+					looseShells.delete(card.entity)
+					observer.unsubscribe(card.hooks.onChangeSlot)
+				})
+			})
 		})
 
 		observer.subscribeWithPriority(
