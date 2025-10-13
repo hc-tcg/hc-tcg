098fbb21a8912785420cfcb59312af1b74241fef
Zunda <mailme@jacobhaussman.online>
2025-10-14 01:32:30 +0900

    auth stuff

--- restart-server.py
+++ restart-server.py
@@ -1,25 +1,36 @@
+# Restart hermitcraft TCG remotely
+# Example:
+# curl localhost:8999/restart -H "auth:SECRET_KEY"
+# Reds config file "auth.json"
+
 from http.server import *
 from textwrap import dedent
+import json
 import os
 
-PORT = 8001
+PORT = 8999
+
+AUTH = json.load(open("auth.json"))
 
 class Server(BaseHTTPRequestHandler):
      def do_GET(self):
         self.send_response(200)
 
         if self.path == "/restart":
-	        restart_hctcg()
-	        self.wfile.write('ok'.encode())
+            if self.headers["auth"] != AUTH["auth"]:
+                print("Recieved bad key")
+                self.wfile.write('bad auth'.encode())
+                return
+            restart_hctcg()
+            self.wfile.write('ok'.encode())
 
 def restart_hctcg():
-	print("Restarting server")
+    print("Restarting server")
 
-
-	os.subprocess(dedent"""
-	docker compose pull
-	docker compose restart
-	""")
+    os.system(dedent("""
+    docker compose pull
+    docker compose restart
+    """))
 
 port = HTTPServer(('', PORT), Server)
 port.serve_forever()
