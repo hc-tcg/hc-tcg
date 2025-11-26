801c8c315e7cb06b7768955d30a28c227cfb9fba
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2025-11-26 14:00:30 -0500

    Update washed-up.ts
    Fix whitespace for linter

--- common/achievements/washed-up.ts
+++ common/achievements/washed-up.ts
@@ -1,7 +1,7 @@
-import WaterBucket from "../cards/attach/water-bucket";
-import EthosLabUltraRare from "../cards/hermits/ethoslab-ultra-rare";
-import { achievement } from "./defaults";
-import { Achievement } from "./types";
+import WaterBucket from '../cards/attach/water-bucket'
+import EthosLabUltraRare from '../cards/hermits/ethoslab-ultra-rare'
+import {achievement} from './defaults'
+import {Achievement} from './types'
 
 const WashedUp: Achievement = {
 	...achievement,
@@ -15,15 +15,15 @@ const WashedUp: Achievement = {
 			steps: 1,
 		},
 	],
-    onGameStart(_game, player, component, observer) {
-        observer.subscribe(player.hooks.onAttach, (card) => {
-            if (card.props !== WaterBucket) return
-            if (!card.slot.inRow()) return
-            if (card.slot.row.getHermit()?.props !== EthosLabUltraRare) return
+	onGameStart(_game, player, component, observer) {
+		observer.subscribe(player.hooks.onAttach, (card) => {
+			if (card.props !== WaterBucket) return
+			if (!card.slot.inRow()) return
+			if (card.slot.row.getHermit()?.props !== EthosLabUltraRare) return
 
-            component.updateGoalProgress({goal: 0})
-        })
-    },
+			component.updateGoalProgress({goal: 0})
+		})
+	},
 }
 
 export default WashedUp
