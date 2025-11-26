8000dfff795e1c8bf6c6ecabc92b9742282eef4c
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2025-11-26 13:13:01 -0500

    Fix whitespace for linter (#1582)

--- common/achievements/world-eater.ts
+++ common/achievements/world-eater.ts
@@ -10,24 +10,23 @@ const WorldEater: Achievement = {
 	levels: [
 		{
 			name: 'World Eater',
-			description:
-				'Draw 5 cards in one turn.',
+			description: 'Draw 5 cards in one turn.',
 			steps: 5,
 		},
 	],
 	onGameStart(game, player, component, observer) {
 		var cardsDrawn = 0
 
-        game.components.filter(CardComponent).forEach((card) => {
-            observer.subscribe(card.hooks.onChangeSlot, (newSlot, oldSlot) => {
-                if (!oldSlot.inDeck()) return
-                if (newSlot.player.entity !== player.entity) return
-                cardsDrawn += 1
-                component.updateGoalProgress({goal: 0, progress: cardsDrawn})
-            })
-        })
+		game.components.filter(CardComponent).forEach((card) => {
+			observer.subscribe(card.hooks.onChangeSlot, (newSlot, oldSlot) => {
+				if (!oldSlot.inDeck()) return
+				if (newSlot.player.entity !== player.entity) return
+				cardsDrawn += 1
+				component.updateGoalProgress({goal: 0, progress: cardsDrawn})
+			})
+		})
 
-        observer.subscribe(player.hooks.onTurnStart, () => cardsDrawn = 0)
+		observer.subscribe(player.hooks.onTurnStart, () => (cardsDrawn = 0))
 	},
 }
 
