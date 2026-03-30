49bb7ad148ecbb54d6fb0358b96d5cfc286f543b
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2026-03-30 11:48:08 -0400

    Test with real user goals

--- tests/db/database.test.ts
+++ tests/db/database.test.ts
@@ -679,6 +679,21 @@ describe('Test Database', () => {
 		expect(returnedDeck3.type).toBe('success')
 	})
 
+	const ALL_CARDS_GOALS = [
+		3, 10, 2, 34, 61, 34, 19, 68, 12, 1, 23, 8, 15, 39, 10, 11, 25, 8, 3, 4, 39,
+		22, 11, 160, 6, 22, 23, 33, 17, 11, 6, 0, 9, 1, 7, 0, 9, 0, 5, 1, 3, 10, 79,
+		9, 6, 6, 0, 0, 18, 1, 40, 31, 42, 37, 56, 45, 21, 12, 15, 10, 14, 10, 56, 9,
+		36, 31, 78, 21, 0, 17, 0, 4, 21, 18, 6, 13, 27, 4, 2, 2, 10, 19, 2, 0, 50,
+		0, 7, 11, 9, 5, 3, 6, 11, 2, 29, 1, 15, 14, 6, 11, 32, 1, 9, 11, 2, 10, 7,
+		30, 1, 5, 37, 28, 1, 19, 1, 7,
+	].reduce(
+		(r, val, key) => {
+			val && (r[key] = val)
+			return r
+		},
+		{} as Record<number, number>,
+	)
+
 	test('Test achievement progress is saved', async () => {
 		const user1 = await database.insertUser('Test User')
 		const user2 = await database.insertUser('Test User 2')
@@ -714,7 +729,7 @@ describe('Test Database', () => {
 					levels: [],
 				},
 				[AllCards.numericId]: {
-					goals: {0: AllCards.levels[0].steps},
+					goals: ALL_CARDS_GOALS,
 					levels: [],
 				},
 			},
@@ -742,9 +757,9 @@ describe('Test Database', () => {
 		])
 		expect(achievements[BalancedWins.numericId].goals).toStrictEqual({0: 10})
 		expect(achievements[BalancedWins.numericId].levels).toStrictEqual([{}, {}])
-		expect(achievements[AllCards.numericId].goals).toStrictEqual({
-			0: AllCards.levels[0].steps,
-		})
+		expect(achievements[AllCards.numericId].goals).toStrictEqual(
+			ALL_CARDS_GOALS,
+		)
 		expect(achievements[AllCards.numericId].levels).toStrictEqual([{}])
 
 		expect(
@@ -786,7 +801,10 @@ describe('Test Database', () => {
 			player2.uuid,
 			{
 				// Default Evil X achievement
-				6: {goals: {0: 1}, levels: [{completionTime: completionTime}]},
+				[DefeatEvilX.numericId]: {
+					goals: {0: 1},
+					levels: [{completionTime: completionTime}],
+				},
 			},
 			completionTime,
 		)
